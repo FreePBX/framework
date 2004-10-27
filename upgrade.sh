d97368c0d0c06f8c70efc94f010f28fd6e4a6834
Ryan Courtnage <ryan@courtnage.ca>
2004-10-27 22:18:16 +0000

    Copy in AMP config then update FOP config.

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+# copy in new config
+cp -rf /usr/src/AMP/amp_conf/* /
+/usr/src/AMP/chown_asterisk.sh
+
+# reload asterisk
+asterisk -rx reload
+
 # Update FOP config to latest format
 su - asterisk -c "cd /var/www/html/panel && /var/www/html/panel/convert_config_pre_14.pl"
 
 # apply the new configuration
-/usr/src/AMP/apply_conf.sh
+asterisk -rx reload
+su - asterisk -c "/var/www/html/admin/retrieve_op_conf_from_mysql.pl"
+su - asterisk -c "/var/www/html/admin/bounce_op.sh"
+
