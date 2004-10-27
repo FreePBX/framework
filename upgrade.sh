1fbc535ac8c257660efbb08a418b66226bbef5d1
Ryan Courtnage <ryan@courtnage.ca>
2004-10-27 18:46:57 +0000

    upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -3,5 +3,5 @@
 # Update FOP config to latest format
 su - asterisk -c "cd /var/www/html/panel && /var/www/html/panel/convert_config_pre_14.pl"
 
-apply the new configuration
+# apply the new configuration
 /usr/src/AMP/apply_conf.sh
