46ead84cb40b0bb1892d38772716ad6b348e9237
Ryan Courtnage <ryan@courtnage.ca>
2004-12-16 02:13:07 +0000

    added extension number checking logic

--- amp_conf/htdocs/admin/common/script.js
+++ amp_conf/htdocs/admin/common/script.js
@@ -21,17 +21,20 @@ function checkForm(theForm) {
 	
 	if ($account == "" || $secret == "" || $vmpwd == "" || $context == "" || $dtmfmode == "" || $host == "" || $type == "" || $mailbox == "" || $username == "" || $fullname == "") {
 		alert('Please fill out all forms.');
+	} else if (($account.indexOf('0') == 0) || ($account.indexOf('8') == 0)) {
+		alert('Extensions cannot begin with 0 or 8');
+	} else if ($account != parseInt($account)) {
+		alert('There is something wrong with your extension number - it must be in integer');
 	} else {
 	theForm.submit();
 	}
 }
 
 function checkGeneral(theForm) {
-	$DIAL_OUT = theForm.DIAL_OUT.value;
 	$RINGTIMER = theForm.RINGTIMER.value;
 	$FAX_RX = theForm.FAX_RX.value;
 
-	if ($DIAL_OUT == "" || $RINGTIMER == "" || $FAX_RX == "") {
+	if ($RINGTIMER == "" || $FAX_RX == "") {
 		alert('Please fill out all forms.');
 	} else {
 	theForm.submit();
@@ -61,6 +64,38 @@ function checkGRP(theForm) {
 	
 	if ($grplist == "") {
 		alert('Please enter an extension list.');
+	} else {
+	theForm.submit();
+	}
+}
+
+function checkDID(theForm) {
+	$account = theForm.account.value;
+
+	var whichitem = 0;
+	while (whichitem < theForm.goto_indicate.length) {
+		if (theForm.goto_indicate[whichitem].checked) {
+			theForm.goto0.value=theForm.goto_indicate[whichitem].value;
+		}
+		whichitem++;
+	}
+	
+	if ($account == "") {
+		alert('Please enter a DID number list.');
+	} else {
+	theForm.submit();
+	}
+}
+
+function checkTrunk(theForm) {
+	$dialprefix = theForm.dialprefix.value;
+	$channelid = theForm.channelid.value;
+	$usercontext = theForm.usercontext.value;
+
+	if ($dialprefix == "" || $channelid == "") {
+		alert('Please fill out all forms');
+	} else if ($channelid == $usercontext) {
+		alert('Trunk Name and User Context cannot be set to the same value');
 	} else {
 	theForm.submit();
 	}
