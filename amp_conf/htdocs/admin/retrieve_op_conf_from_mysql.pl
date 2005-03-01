1074142d1157c256f0bf9ef4db0e0f751496e93e
Ryan Courtnage <ryan@courtnage.ca>
2005-03-01 22:56:21 +0000

    sip_conf -> op_conf

--- amp_conf/htdocs/admin/retrieve_op_conf_from_mysql.pl
+++ amp_conf/htdocs/admin/retrieve_op_conf_from_mysql.pl
@@ -9,101 +9,86 @@
 use DBI;
 ################### BEGIN OF CONFIGURATION ####################
 
-######## STYLE INFO #########
-$extenpos="2-7,9-14";
-$trunkpos="23-28";
-$confepos="16-18";
-$queuepos="19-21";
-
-@zaplines=(); # zap channel, description
-#@zaplines=(@zaplines,[ "Zap/1","Zap 1" ]);
-#@zaplines=(@zaplines,[ "Zap/2","Zap 2" ]);
-#@zaplines=(@zaplines,[ "Zap/3","Zap 3" ]);
-@zaplines=(@zaplines,[ "Zap/4","PSTN" ]);
-
-@conferences=();   #### ext#, description
-#@conferences=(@conferences,[ "810","Conf.10" ]);
-#@conferences=(@conferences,[ "811","Conf.11" ]);
-
-@queues=();       ### queue name, queue description
-#@queues=(@queues, [ "SALES", "Sales Queue" ]);
-#@queues=(@queues, [ "TECH", "Tech Queue" ]);
-
-
+# the name of the extensions table
+$table_name = "sip";
+# the path to the extensions.conf fileop_conf
 
 # WARNING: this file will be substituted by the output of this program
-$op_conf = "/var/www/html/panel/op_buttons_additional.cfg";
+$op_conf = "AMPWEBROOT/panel/op_buttons_additional.cfg";
 # the name of the box the MySQL database is running on
 $hostname = "localhost";
 # the name of the database our tables are kept
 $database = "asterisk";
 # username to connect to the database
-$username = "asteriskuser";
+$username = "AMPDBUSER";
 # password to connect to the database
-$password = "amp109";
+$password = "AMPDBPASS";
 
 # Zap Channels = remove or add to this list as necessary
-#$additional = "[Zap/4]\nPosition=1\nLabel=\"Telefonica\"\nExtension=-1\nIcon=3\n";
-#$additional .= "[Zap/2]\nPosition=2\nLabel=\"External 2\"\nExtension=-1\nIcon=3\n";
-#$additional .= "[Zap/3]\nPosition=3\nLabel=\"External 3\"\nExtension=-1\nIcon=3\n";
-#$additional .= "[Zap/4]\nPosition=4\nLabel=\"External 4\"\nExtension=-1\nIcon=3\n";
+$additional = "[Zap/1]\nPosition=1\nLabel=\"External 1\"\nExtension=-1\nIcon=3\n";
+$additional .= "[Zap/2]\nPosition=2\nLabel=\"External 2\"\nExtension=-1\nIcon=3\n";
+$additional .= "[Zap/3]\nPosition=3\nLabel=\"External 3\"\nExtension=-1\nIcon=3\n";
+$additional .= "[Zap/4]\nPosition=4\nLabel=\"External 4\"\nExtension=-1\nIcon=3\n";
 # Button position to start regular extensions at
 $btn=10;
 
 ################### END OF CONFIGURATION #######################
 
 
-open EXTEN, ">$op_conf" || die "Cannot create/overwrite config file: $op_conf\n";
+open EXTEN, ">$op_conf" or die "Cannot create/overwrite config file: $op_conf\n";
 
-#print EXTEN "$additional";
+print EXTEN "$additional";
 
 $dbh = DBI->connect("dbi:mysql:dbname=$database;host=$hostname", "$username", "$password");
 
 
-#First, populate extensions
+$statement = "SELECT data,id from $table_name where keyword='account' and flags <> 1 group by data";
 
-@extensionlist=();
-
-foreach my $table ("sip","iax","zap") {
-	if (table_exists($dbh,$table)) {
-		$statement = "SELECT data,id,'$table' from $table where keyword='account' and flags <> 1 and id<9999 group by data order by id";
-		$result = $dbh->selectall_arrayref($statement);
-		@resultSet = @{$result};
-		if ( $#resultSet == -1 ) {
-	  		print "Notice: no $table accounts defined\n";
-		}
-		push(@extensionlist, @{ $result });
-	}
-	else { print "no existe $table\n"; }
+$result = $dbh->selectall_arrayref($statement);
+unless ($result) {
+  # check for errors after every single database call
+  print "dbh->selectall_arrayref($statement) failed!\n";
+  print "DBI::err=[$DBI::err]\n";
+  print "DBI::errstr=[$DBI::errstr]\n";
 }
 
-@extensionlist=sort {$a->[1] cmp $b->[1]}(@extensionlist);
-
-#Next, populate trunks (sip and iax)
-@trunklist=();
-foreach $table ("sip","iax") {
-	if (table_exists($dbh,$table)) {
-		$statement = "SELECT data,id,'$table' from $table where keyword='account' and flags <> 1 and id>9999 group by data order by id";
-		$result = $dbh->selectall_arrayref($statement);
-		@resultSet = @{$result};
-		if ( $#resultSet == -1 ) {
-	  		print "Notice: no $table trunks defined\n";
-		}
-		push(@trunklist, @{ $result });
-	}
+@resultSet = @{$result};
+if ( $#resultSet == -1 ) {
+  print "Notice: no sip accounts defined\n";
+  #exit;
 }
 
+@total_result = @{ $result };
 
-# WRITE EXTENSIONS
 
-$btn=0; 
-foreach my $row ( @extensionlist ) {
+
+if (table_exists($dbh,"iax")) {
+	$statement = "SELECT data,id from iax where keyword='account' and flags <> 1 group by data order by id";
+	$result = $dbh->selectall_arrayref($statement);
+	@resultSet = @{$result};
+	if ( $#resultSet == -1 ) {
+  		print "Notice: no iax accounts defined\n";
+	}
+	push(@total_result, @{ $result });
+}
+
+if (table_exists($dbh,"zap")) {
+	$statement = "SELECT data,id FROM zap WHERE keyword='account' and flags <> 1 group by data order by id";
+	$result = $dbh->selectall_arrayref($statement);
+	@resultSet = @{$result};
+	if ($#resultSet == -1 ) {
+		print "Notice: no zap accounts defined\n";
+	}
+	push(@total_result, @{ $result });
+}
+
+foreach my $row ( @total_result ) {
+	$btn++;
 	my $account = @{ $row }[0];
 	my $id = @{ $row }[1];
-	my $table = @{ $row }[2];
-#	next if ($account eq "");
-	$btn=get_next_btn($extenpos,$btn);
-	$statement = "SELECT keyword,data from $table where id=$id and keyword <> 'account' and flags <> 1 order by keyword";
+	#print EXTEN "[$account]\n";
+	$statement = "SELECT keyword,data from sip where id=$id and keyword <> 'account' and flags <> 1 order by keyword";
+	$tech="SIP";
 	my $result = $dbh->selectall_arrayref($statement);
 	unless ($result) {
 		# check for errors after every single database call
@@ -112,122 +97,46 @@ foreach my $row ( @extensionlist ) {
 		print "DBI::errstr=[$DBI::errstr]\n";
 		exit;
 	}
-	
-	$tech="SIP" if $table eq "sip";
-	$tech="IAX2" if $table eq "iax";
-	$tech="ZAP" if $table eq "zap";
 
-	#my @resSet = @{$result};
-	
+	my @resSet = @{$result};
+	if ( $#resSet == -1 ) {       	#if a result isn't in sip, look in iax   
+		#print "no results\n";
+			$statement = "SELECT keyword,data from iax where id=$id and keyword <> 'account' and flags <> 1 order by keyword";
+			$tech="IAX2";
+			$result = $dbh->selectall_arrayref($statement);
+			my @resSetIax = @{$result};
+			if ( $#resSetIax == -1 ) { #Maybe it's ZAP
+				$statement = "SELECT keyword,data from zap where id=$id and keyword <> 'account' and flags<>1 order by keyword";
+				$tech="ZAP";
+				$result = $dbh->selectall_arrayref($statement);
+			}
+
+	}
+	$zapchannel="PROBLEM"; #default to an invalid zap channel
 	$callerid = $account;  #default callerid to account
-	$user=$account;
-
-	foreach my $drow ( @{ $result } ) {
-		my @result = @{ $drow };
+	foreach my $row ( @{ $result } ) {
+		my @result = @{ $row };
 		if ( $result[0] eq "callerid" ) {
 			$callerid = $result[1];
-			@fields=split(/</,$callerid);
-			$callerid=$fields[1] ." ". $fields[0];
 			$callerid =~ tr/\"<>//d;
 		}
 		if ( $result[0] eq "channel" ) {
-			if ($tech eq "ZAP") { $user = $result[1]; }
+			$zapchannel = $result[1];
 		}
 	}
 	$icon='4';
-	print EXTEN "[$tech/$user]\nPosition=$btn\nLabel=\"$callerid\"\nExtension=$account\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n";
-}
-
-
-### NOW WRITE TRUNKS.. WE START WITH ZAP TRUNKS DEFINED ABOVE
-
-
-
-
-$btn=0; 
-foreach my $row ( @zaplines ) {
-	$btn=get_next_btn($trunkpos,$btn);
-	$zapdef=@{$row}[0];
-	$zapdesc=@{$row}[1];
-	$icon='3';
-	print EXTEN "[$zapdef]\nPosition=$btn\nLabel=\"$zapdesc\"\nExtension=-1\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n";
-}
-
-
-foreach my $row ( @trunklist ) {
-	my $account = @{ $row }[0];
-	my $id = @{ $row }[1];
-	my $table = @{ $row }[2];
-	next if ($account eq "");
-	$btn=get_next_btn($trunkpos,$btn);
-	$statement = "SELECT keyword,data from $table where id=$id and keyword <> 'account' and flags <> 1 order by keyword";
-	my $result = $dbh->selectall_arrayref($statement);
-	unless ($result) {
-		# check for errors after every single database call
-		print "dbh->selectall_arrayref($statement) failed!\n";
-		print "DBI::err=[$DBI::err]\n";
-		print "DBI::errstr=[$DBI::errstr]\n";
-		exit;
+	if ($callerid eq $account) {  #if this happens, we know it's a voip trunk, so change the icon
+		$icon='3';
 	}
-	
-	$tech="SIP" if $table eq "sip";
-	$tech="IAX2" if $table eq "iax";
-	#$tech="ZAP" if $table eq "zap"; #no zap trunks in db
-
-	#my @resSet = @{$result};
-	
-	$callerid = $account;  #default callerid to account
-
-	foreach my $drow ( @{ $result } ) {
-		my @result = @{ $drow };
-		if ( $result[0] eq "callerid" ) {
-			$callerid = $result[1];
-			@fields=split(/</,$callerid);
-			$callerid=$fields[1] ." ". $fields[0];
-			$callerid =~ tr/\"<>//d;
-		}
+	if ($tech eq "ZAP") {
+		print EXTEN "[Zap/$zapchannel]\nPosition=$btn\nLabel=\"$callerid\"\nExtension=$account\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n"
+	} else {
+		print EXTEN "[$tech/$account]\nPosition=$btn\nLabel=\"$callerid\"\nExtension=$account\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n";
 	}
-	$icon='3';
-	print EXTEN "[$tech/$account]\nPosition=$btn\nLabel=\"$callerid\"\nExtension=$account\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n";
 }
 
+exit 0;
 
-### Write conferences (meetme)
-
-
-$btn=0; 
-foreach my $row ( @conferences ) {
-	$btn=get_next_btn($confepos,$btn);
-	$confenum=@{$row}[0];
-	$confedesc=@{$row}[1];
-	$icon='6';
-	print EXTEN "[$confenum]\nPosition=$btn\nLabel=\"$confedesc\"\nExtension=$confenum\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n";
-}
-
-$btn=0; 
-foreach my $row ( @queues ) {
-	$btn=get_next_btn($queuepos,$btn);
-	$queuename=@{$row}[0];
-	$queuedesc=@{$row}[1];
-	$icon='5';
-	print EXTEN "[$queuename]\nPosition=$btn\nLabel=\"$queuedesc\"\nExtension=-1\nContext=from-internal\nIcon=$icon\nVoicemail_Context=default\n";
-}
-
-sub get_next_btn {
-	my $data = shift;
-	my $last = shift;
-
-	@rangelist=split(",",$data);
-
-	foreach $range (@rangelist) {
-		@rangeval=split("-",$range);
-		return $rangeval[0] if $last < $rangeval[0];
-		return $last+1 if $last < $rangeval[1];
-		#Need to try another range def...
-	}
-	#If we get here, we ran out of positions :(
-	return 0; #?????
-}
 #this sub checks for the existance of a table
 sub table_exists {
     my $db = shift;
@@ -236,7 +145,7 @@ sub table_exists {
     if (@tables) {
         for (@tables) {
             next unless $_;
-            $_ =~ s/`//g;
+			$_ =~ s/`//g;     #on suse 9.2 we need to get rid of quotes
             return 1 if $_ eq $table
         }
     }
@@ -250,5 +159,3 @@ sub table_exists {
     }
     return 0;
 }
-
-
