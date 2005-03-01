a1e932b17e079b549ca5310442554c67d8b1b9db
Greg MacLellan <greg@mtechsolutions.ca>
2005-03-01 19:33:14 +0000

    Now reads /etc/amportal.conf and applies ALL values

--- change_pass.sh
+++ change_pass.sh
@@ -1,73 +1,77 @@
 #!/bin/bash
 
-OLDPASS=amp109
-
-if [ -z "$1" ]
+if [ "$1" == "-h" ]
 then
 	echo "Usage: "
-	echo "   "$0" newpass [username]"
+	echo "   "$0" [config]"
 	echo
-	echo "the Username is only changed if specified"
+	echo "If config file is not specified, default is /etc/amportal.conf"
 	echo
 	exit
 fi
 
-NEWPASS=$1
-
-# check for username specified
-if [ -n "$2" ]
+if [ -n "$1" ]
 then
-	echo "Got new username $USERNAME"
-	USERNAME=$2
+	AMPCONFIG=$1
+else
+	AMPCONFIG=/etc/amportal.conf
 fi
 
+if [ ! -e $AMPCONFIG ]
+then
+	echo "Cannot find $AMPCONFIG"
+	exit
+fi
 
-echo "Changing passwords..."
+# include config file
+echo "Reading $AMPCONFIG"
+source $AMPCONFIG
+
+
+echo "Updating configuration..."
 
 echo "/etc/asterisk/cdr_mysql.conf"
-sed -r -i "s/password=[a-zA-Z0-9]+/password=$NEWPASS/" /etc/asterisk/cdr_mysql.conf
+sed -r -i "s/username=[a-zA-Z0-9]*/username=$AMPDBUSER/" /etc/asterisk/cdr_mysql.conf
+sed -r -i "s/password=[a-zA-Z0-9]*/password=$AMPDBPASS/" /etc/asterisk/cdr_mysql.conf
 
-echo "/var/www/html/admin/cdr/lib/defines.php"
-sed -r -i "s/define \(\"PASS\", \"[a-zA-Z0-9]+\"\);/define \(\"PASS\", \"$NEWPASS\"\);/" /var/www/html/admin/cdr/lib/defines.php
+echo $AMPWEBROOT"/admin/cdr/lib/defines.php"
+sed -r -i "s/define \(\"USER\", \"[a-zA-Z0-9]*\"\);/define \(\"USER\", \"$AMPDBUSER\"\);/" $AMPWEBROOT/admin/cdr/lib/defines.php
+sed -r -i "s/define \(\"PASS\", \"[a-zA-Z0-9]*\"\);/define \(\"PASS\", \"$AMPDBPASS\"\);/" $AMPWEBROOT/admin/cdr/lib/defines.php
+sed -r -i "s/define \(\"WEBROOT\", \"[a-zA-Z0-9_-\.\/\\]*\"\);/define \(\"WEBROOT\", \"http:\/\/$AMPWEBADDRESS\/admin\/cdr\/\"\);/" $AMPWEBROOT/admin/cdr/lib/defines.php
+sed -r -i "s!define \(\"FSROOT\", \"[a-zA-Z0-9_-\.\/\\]*\"\);!define \(\"FSROOT\", \"$AMPWEBROOT\/admin\/cdr\/\"\);!" $AMPWEBROOT/admin/cdr/lib/defines.php
 
 # do a bunch at once here
 find /var/www/html/admin/ -name retrieve\*.pl
-sed -r -i "s/password = \"[a-zA-Z0-9]+\";/password = \"$NEWPASS\";/" `find /var/www/html/admin/ -name retrieve\*.pl`
+sed -r -i "s/username = \"[a-zA-Z0-9]*\";/username = \"$AMPDBUSER\";/" `find /var/www/html/admin/ -name retrieve\*.pl`
+sed -r -i "s/password = \"[a-zA-Z0-9]*\";/password = \"$AMPDBPASS\";/" `find /var/www/html/admin/ -name retrieve\*.pl`
+sed -r -i "s!sip_conf = \"[a-zA-Z0-9_-\.\/\\]*\";!sip_conf = \"$AMPWEBROOT\/panel\/op_buttons_additional.cfg\";!" `find /var/www/html/admin/ -name retrieve\*.pl`
 
 echo "/var/www/html/admin/common/db_connect.php"
-sed -r -i "s/db_pass = '[a-zA-Z0-9]+';/db_pass = '$NEWPASS';/" /var/www/html/admin/common/db_connect.php
+sed -r -i "s/db_user = '[a-zA-Z0-9]*';/db_user = '$AMPDBUSER';/" /var/www/html/admin/common/db_connect.php
+sed -r -i "s/db_pass = '[a-zA-Z0-9]*';/db_pass = '$AMPDBPASS';/" /var/www/html/admin/common/db_connect.php
 
-if [ -n "$USERNAME" ]
-then
-	echo "Changing database usernames..."
-	
-	echo "/etc/asterisk/cdr_mysql.conf"
-	sed -r -i "s/username=[a-zA-Z0-9]+/username=$USERNAME/" /etc/asterisk/cdr_mysql.conf
+echo "/etc/asterisk/manager.conf"
+sed -r -i "s/secret = [a-zA-Z0-9]*/secret = $AMPMGRPASS/" /etc/asterisk/manager.conf
+sed -r -i "/\[general\]/!s/\[[a-zA-Z0-9]+\]/[$AMPMGRUSER]/" /etc/asterisk/manager.conf
 
-	echo "/var/www/html/admin/cdr/lib/defines.php"
-	sed -r -i "s/define \(\"USER\", \"[a-zA-Z0-9]+\"\);/define \(\"USER\", \"$USERNAME\"\);/" /var/www/html/admin/cdr/lib/defines.php
+echo "/var/lib/asterisk/agi-bin/dialparties.agi"
+sed -r -i "s/mgrUSERNAME='[a-zA-Z0-9]*';/mgrUSERNAME='$AMPDBUSER';/" /var/lib/asterisk/agi-bin/dialparties.agi
+sed -r -i "s/mgrSECRET='[a-zA-Z0-9]*';/mgrSECRET='$AMPDBPASS';/" /var/lib/asterisk/agi-bin/dialparties.agi
 
-	# do a bunch at once here
-	find /var/www/html/admin/ -name retrieve\*.pl
-	sed -r -i "s/username = \"[a-zA-Z0-9]+\";/username = \"$USERNAME\";/" `find /var/www/html/admin/ -name retrieve\*.pl`
+echo $AMPWEBROOT"/panel/op_server.cfg"
+sed -r -i "s/manager_user=[a-zA-Z0-9]*/manager_user=$AMPMGRUSER/" $AMPWEBROOT/panel/op_server.cfg
+sed -r -i "s/manager_secret=[a-zA-Z0-9]*/manager_secret=$AMPMGRPASS/" $AMPWEBROOT/panel/op_server.cfg
+sed -r -i "s/web_hostname=[a-zA-Z0-9_-\.]*/web_hostname=$AMPWEBADDRESS/" $AMPWEBROOT/panel/op_server.cfg
+sed -r -i "s/security_code=[a-zA-Z0-9]*/security_code=$FOPPASSWORD/" $AMPWEBROOT/panel/op_server.cfg
+sed -r -i "s!flash_dir=[a-zA-Z0-9_-\.\/\\]*!flash_dir=$AMPWEBROOT\/panel!" $AMPWEBROOT/panel/op_server.cfg
+sed -r -i "s!web_hostname=[a-zA-Z0-9\.]*!web_hostname=$AMPWEBADDRESS!" $AMPWEBROOT/panel/op_server.cfg
+sed -r -i "s!web_hostname=[a-zA-Z0-9\.]*!web_hostname=$AMPWEBADDRESS!" $AMPWEBROOT/panel/op_server.cfg
 
-	echo "/var/www/html/admin/common/db_connect.php"
-	sed -r -i "s/db_user = '[a-zA-Z0-9]+';/db_user = '$USERNAME';/" /var/www/html/admin/common/db_connect.php
-	
-fi
-
+echo "/etc/asterisk/vm_email.inc (may require manual check)"
+sed -i -e "s/AMPWEBADDRESS/$AMPWEBADDRESS/g" /etc/asterisk/vm_email.inc
 
 echo "Done"
 echo
 
 exit
-
-# amp_conf/etc/asterisk/cdr_mysql.conf: password=amp109
-# amp_conf/var/www/html/admin/cdr/lib/defines.php.template:define ("PASS", "amp109");
-# amp_conf/var/www/html/admin/retrieve_extensions_from_mysql.pl:$password = "amp109";
-# amp_conf/var/www/html/admin/retrieve_iax_conf_from_mysql.pl:$password = "amp109";
-# amp_conf/var/www/html/admin/retrieve_meetme_conf_from_mysql.pl:$password = "amp109";
-# amp_conf/var/www/html/admin/retrieve_op_conf_from_mysql.pl:$password = "amp109";
-# amp_conf/var/www/html/admin/retrieve_sip_conf_from_mysql.pl:$password = "amp109";
-# amp_conf/var/www/html/admin/common/db_connect.php:$db_pass = 'amp109';
 
