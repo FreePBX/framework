c8d1dbded9a0d990a13f6123e5c581d55d399970
Ryan Courtnage <ryan@courtnage.ca>
2005-03-17 16:48:42 +0000

    fix quote prob on line 4

--- amp_conf/htdocs/admin/cdr/export_pdf.php
+++ amp_conf/htdocs/admin/cdr/export_pdf.php
@@ -2,7 +2,7 @@
 include_once(dirname(__FILE__) . "/lib/defines.php");
 include_once(dirname(__FILE__) . "/lib/Class.Table.php");
 define(FPDF_FONTPATH,dirname(__FILE__).'/lib/font/');
-include_once(dirname(__FILE__) . "/lib/fpdf.php');
+include_once(dirname(__FILE__) . "/lib/fpdf.php");
 
 session_start();
 
