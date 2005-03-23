d6b7941a04587e46de4af631ff483307e8fe95b6
Ryan Courtnage <ryan@courtnage.ca>
2005-03-23 05:35:21 +0000

    remove obsolete scripts

--- bounce_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/usr/src/AMP/chown_asterisk.sh
-asterisk -rx "stop when convenient"
-echo PLEASE WAIT
-sleep 10
-/sbin/modprobe -r wcfxs
-/sbin/modprobe wcfxs
-su - asterisk -c "export PATH=$PATH:/usr/sbin && export LD_ASSUME_KERNEL=2.4.1 && export LD_LIBRARY_PATH=/usr/local/lib && /usr/sbin/safe_asterisk"
-echo COMPLETE
