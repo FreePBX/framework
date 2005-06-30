ea045e1a5c7072e71d675ee3b2f11d4f176b09f9
Ryan Courtnage <ryan@courtnage.ca>
2005-06-30 16:35:49 +0000

    Frnch translation

--- amp_conf/htdocs/admin/index.php
+++ amp_conf/htdocs/admin/index.php
@@ -57,8 +57,7 @@ switch($display) {
 	<select onchange="javascript:changeLang(this.value)">
         <option value="en_US" <? echo ($_COOKIE['lang']=="en_US" ? "selected" : "") ?> >English</option>
         <option value="it_IT" <? echo ($_COOKIE['lang']=="it_IT" ? "selected" : "") ?> >Italian</option>
-        <option value="es_ES" <? echo ($_COOKIE['lang']=="es_ES" ? "selected" : "") ?> >Espa&ntilde;ol</option>
-        <option value="en_GB" <? echo ($_COOKIE['lang']=="en_GB" ? "selected" : "") ?> >Test</option>
+        <option value="fr_FR" <? echo ($_COOKIE['lang']=="fr_FR" ? "selected" : "") ?> >French</option>
     </select>
 	<?php } ?>
     </p>
