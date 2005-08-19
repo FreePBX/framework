35747490730e0f9a2244146ede241968195597e7
Dan Littlejohn <dan@littlejohnconsulting.com>
2005-08-19 22:02:23 +0000

    Update for ARI.
    Adds Authentication and Voicemail support.
    
    New configuration options :
    - Navigate to /var/www/html/recordings/includes
    - Open the file main.conf
    - If you wish to only use the call monitor portion set $ari_no_login=1
    - If you wish for certain extensions to view all calls in the call
    monitor,
      set $callmonitor_admin_mailboxes equal to a comma delimited list of
    the
      extensions
    - If you wish for all users to view all calls in the call monitor,
      set $callmonitor_admin_mailboxes to "all"

--- amp_conf/htdocs/recordings/theme/page.tpl.php
+++ amp_conf/htdocs/recordings/theme/page.tpl.php
@@ -5,7 +5,10 @@
     <link rel="stylesheet" href="theme/style.css" type="text/css" />
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
   </head>
+  <body>
   <div id="page">
+  <div class="minwidth">
+  <div class="container">
     <div id="topheader">
       <div id="logo"><a href='<?=$_SERVER['PHP_SELF']?>' title="ARI">Asterisk Recording Interface</a></div>
     </div>
@@ -20,30 +23,62 @@
     </div> 
     <div id="headerspacer"><img src="theme/spacer.gif" alt=""></div> 
     <div id="main">
-      <div id="menu" class="nav">
-        <div class="navimg">
-          <img alt="" src="./theme/header_monitor.png">
-	</div>
-        <div class="navtext">
-          <p><small><small><a href='<?=$_SERVER['PHP_SELF']?>'>Recordings</a></small></small></p>
-        </div>
-      </div>
-      <div id="center">
-        <div id="content">
-          <!-- begin main content -->
-          <?php print($content) ?>
-          <!-- end main content -->
-          <!--begin footer-->
-          <div id="footer">
-            <p class="footer">
-              <small class="min">&copy; Copyright 2005 . All Rights Reserved.<br>
-              <a href="Mailto:">L</a></small>
-            </p>
+    <div class="minwidth">
+    <div class="container">
+      <div class="spacer"></div>
+      <span class="left">
+        <div id="menu">
+          <div><img height=4 src="theme/spacer.gif" alt=""></div> 
+          <div class="nav">
+            <b class='nav_b1'></b><b class='nav_b2'></b><b class='nav_b3'></b><b class='nav_b4'></b>
+            <div class='nav_items'>
+              <?php if ($ari_no_login =='') { ?>
+                <p><small><small><a href='<?=$_SERVER['PHP_SELF']?>?s=voicemail'>Voicemail</a></small></small></p>
+              <?php } ?>
+                <p><small><small><a href='<?=$_SERVER['PHP_SELF']?>?s=callmonitor'>Call Monitor</a></small></small></p>
+                <?php if ($logout !='') { ?>
+                  <br>
+                  <p><small><small><a href='<?=$_SERVER['PHP_SELF']?>?logout=1'>Logout</a></small></small></p>
+                <?php } ?>
+            </div>
+            <b class='nav_b4'></b><b class='nav_b3'></b><b class='nav_b2'></b><b class='nav_b1'></b>
           </div>
-          <!-- end footer -->
+          <div><img height=14 src="theme/spacer.gif" alt=""></div> 
+          <?php if ($navsub_menu !='') { ?>
+            <div class="subnav">
+              <div class="subnav_title">Folders:</div>
+              <b class='subnav_b1'></b><b class='subnav_b2'></b><b class='subnav_b3'></b><b class='subnav_b4'></b>
+              <div class='subnav_items'>
+                <? echo $navsub_menu; ?>
+              </div>
+              <b class='subnav_b4'></b><b class='subnav_b3'></b><b class='subnav_b2'></b><b class='subnav_b1'></b>
+            </div>
+          <?php } ?>
         </div>
-      </div>
+      </span>
+      <span class="right">
+        <div id="center">
+          <?php if ($login != "") { ?>
+            <?php print($login) ?>
+          <?php } ?>
+          <div id="content">
+            <!-- begin main content -->
+              <?php print($content) ?>
+            <!-- end main content -->
+          </div>
+        </div>
+      </span>
+      <div class="spacer"></div>
     </div>
+    </div>
+    </div>
+    <!--begin footer-->
+    <div id="footer">
+      <small>&middot;&nbsp;<a href="http://www.littlejohnconsulting.com">Littlejohn Consulting</a></small>
+    </div>
+    <!-- end footer -->
+  </div>
+  </div>
   </div>
   </body>
 </html>
