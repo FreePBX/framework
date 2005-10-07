82538b789d33ecb92f923cff5ab93919b9e01f79
Greg MacLellan <greg@mtechsolutions.ca>
2005-10-07 15:39:50 +0000

    Updating extensions class with 1.2-style "n" priorities and tags

--- amp_conf/htdocs/admin/extensions.class.php
+++ amp_conf/htdocs/admin/extensions.class.php
@@ -23,144 +23,47 @@ class extensions {
 	/** Add an entry to the extensions file
 	* @param $section    The section to be added to
 	* @param $extension  The extension used
-	* @param $value      The value to put at that extension
-	* @param $priority   The priority to use. False for next
+	* @param $tag        A tag to use (to reference with basetag), use false or '' if none
+	* @param $command    The command to execute
+	* @param $basetag    The tag to base this on. Only used in conjunction with $addpriority
 	*                    priority. Defaults to false.
-	* @param $insert     If we're allowed to insert priorities. If $priority is 
-	*			used up, everything higher will be incremented by 1
-	*			(including Goto's, If's, etc)
-	* @return The priority the extension was inserted at, or
-	*         false if there is already a value at that extension
+	* @param $addpriority  Finds the priority of the tag called $basetag, and adds this 
+	*			value to it to use as the priority for this command.
+	* @return 
 	*/
-	function add($section, $extension, $value, $basepriority = false, $addpriority = false, $insert = false) {
-		if ($basepriority || $addpriority) {
-			if (!is_int($basepriority) || !is_int($addpriority)) {
-				// not supported. for non-integers (for 'hint' priority, use addHint() )
-				trigger_error("Only integers are permitted for \$priority to extensions::add()", E_ERROR);
+	function add($section, $extension, $tag, $command, $basetag = false, $addpriority = false) {
+		
+		if ($basetag || $addpriority) {
+			if (!is_int($addpriority) || ($addpriority < 1)) {
+				trigger_error(E_ERROR, "\$addpriority must be >= 1 in extensions::add()");
 				return false;
 			}
-			$priority = $basepriority + $addpriority;
-		} else {
-			// no priority specified, pick the next one
-			$priority = $this->_lastpri[$section][$extension] + 1;
+			if (empty($basetag)) {
+				trigger_error(E_ERROR, "\$basetag is required with \$addpriority in extensions::add()");
+				return false;
+			}
 		}
 		
-		if (!is_subclass_of($value, "extension")) {
-			trigger_error(E_ERROR, "Only instances and subclasses of the 'extension' class can be passed as \$value to extensions::add()");
-			return false;
-		}
-		
-		echo "adding at [$section][$extension][$priority]";
-		
-		if (isset($this->_exts[$section][$extension][$priority])) {
-			// priority is already used
-			if ($insert) {
-				// we can insert, so increment everything before inserting
-				$this->increment($section, $extension, $priority);
+		if (empty($basetag)) {
+			// no basetag, we need to make one
+			
+			if (!isset($this->_exts[$section][$extension])) {
+				// first entry, use 1
+				$basetag = '1';
 			} else {
-				// we can't increment
-				return false;
+				// anything else just n
+				$basetag = 'n';
 			}
 		}
-
-		$this->_exts[$section][$extension][$priority] = $value;
-		if ($basepriority) {
-			// store a pointer relating this priority to the basepriority (for incrementing purposes)
-			$this->_related[$section][$extension][$priority] = array('base'=>$basepriority, 'add'=>$addpriority);
-		}
 		
-		// it may still be sorted, but thats a lot of work to figure out
-		$this->_sorted = false;
-		
-		$this->_lastpri[$section][$extension] = $priority;
-		return $priority;
-	}
-	
-	function add2($section, $extension, $value, $base = ".") {
-		
-		if (!is_subclass_of($value, "extension")) {
-			trigger_error(E_ERROR, "Only instances and subclasses of the 'extension' class can be passed as \$value to extensions::add()");
-			return false;
-		}
-		
-		echo "adding at [$section][$extension][$priority]";
-		if (isset($this->_exts[$section][$extension][$priority])) {
-			// priority is already used
-			if ($insert) {
-				// we can insert, so increment everything before inserting
-				//$this->increment($section, $extension, $priority);
-			} else {
-				// we can't increment
-				return false;
-			}
-		}
-
-		$this->_exts[$section][$extension][$priority] = array(
-			'tag' => $base,
-			'cmd' => $value,
+		$new = array(
+			'basetag' => $basetag,
+			'tag' => $tag,
+			'addpri' => $addpriority,
+			'cmd' => $command,
 		);
 		
-		// it may still be sorted, but thats a lot of work to figure out
-		$this->_sorted = false;
-		
-		$this->_lastpri[$section][$extension] = $priority;
-		return $priority;
-	}
-	
-	function increment($section, $extension, $start_priority, $increment = 1) {
-		if ($increment < 1) {
-			// not supported
-			return false;
-		}
-		
-		// sort this extension by priority
-		//not important with for loop!    ksort($this->_exts[$section][$extension]);  
-		
-		$num_incremented = 0;
-		
-		// iterate through sequential priorities, until the next one is not set
-		// at the end of the loop $i will be the next available priority
-		for ($i = $start_priority; isset($this->_exts[$section][$extension][$priority]); $i++) {
-			$this->_exts[$section][$extension][$priority]->increment($increment);
-			$reassign[$priority] = $priority+$increment;
-		}
-		
-		$do_increment = false;
-		$keys = array_keys($this->_exts[$section][$extension]);
-		
-		foreach ($keys as $priority) {
-		
-			if ($do_increment || ($priority == $start_priority)) {
-				$do_increment = true;
-				$num_incremented += 1;
-				// increment the object inside this priority
-				$this->_exts[$section][$extension][$priority]->increment($increment);
-				
-				// then move the actual priority. since we're going backwards, there's never going to be something bigger
-				echo "copy to [$section][$extension][$priority+$increment]";
-				echo "unset [$section][$extension][$priority]";
-				
-				$reassign[$priority] = $priority+$increment;
-			}
-		}
-		
-		krsort($reassign);
-		foreach ($reassign as $oldpri => $newpri) {
-			// we move variables around in the array with references. this works a lot like hard links in a *nix file system
-			
-			// reference each value first (this does not allocate any new memory)
-			$this->_exts[$section][$extension][$newpri] &= $this->_exts[$section][$extension][$oldpri];
-			// then unset the original, leaving only one reference to the value
-			unset($this->_exts[$section][$extension][$oldpri]);
-		}
-		
-		//array(1,2,3,101,102)
-		// array(1,2,4,101,102) oh FUCK!
-		
-				$this->_exts[$section][$extension][$priority+$increment] &= $this->_exts[$section][$extension][$priority];
-				unset($this->_exts[$section][$extension][$priority]);
-
-		return $num_incremented;
+		$this->_exts[$section][$extension][] = $new;
 	}
 	
 	/** Sort sections, extensions and priorities alphabetically
@@ -190,9 +93,11 @@ class extensions {
 	function generateConf() {
 		$output = "";
 		
+		/* sorting is not necessary anymore
 		if (!$this->_sorted) {
 			$this->sort();
 		}
+		*/
 		
 		var_dump($this->_exts);
 		
@@ -200,10 +105,82 @@ class extensions {
 			$output .= "[".$section."]\n";
 			
 			foreach (array_keys($this->_exts[$section]) as $extension) {
-				foreach (array_keys($this->_exts[$section][$extension]) as $priority) {
-					var_dump($this->_exts[$section][$extension][$priority]);
-					$output .= "exten => ".$extension.",".$priority.",".$this->_exts[$section][$extension][$priority]->output()."\n";
+				foreach (array_keys($this->_exts[$section][$extension]) as $idx) {
+				
+					$ext = $this->_exts[$section][$extension][$idx];
+					
+					var_dump($ext);
+					
+					$output .= "exten => ".$extension.",".
+						$ext['basetag'].
+						($ext['addpri'] ? '+'.$ext['addpri'] : '').
+						($ext['tag'] ? '('.$ext['tag'].')' : '').
+						",".$ext['cmd']->output()."\n";
 				}
+				if (isset($this->_hints[$section][$extension])) {
+					foreach ($this->_hints[$section][$extension] as $hint) {
+						$output .= "exten => ".$extension.",hint,".$hint;
+					}
+				}
+			}
+			
+			$output .= "\n; end of [".$section."]\n\n\n";
+		}
+		
+		return $output;
+	}
+
+	/** Generate the file
+	* @return A string containing the extensions.conf file
+	*/
+	function generateOldConf() {
+		$output = "";
+		
+		/* sorting is not necessary anymore
+		if (!$this->_sorted) {
+			$this->sort();
+		}
+		*/
+		
+		var_dump($this->_exts);
+		
+		foreach (array_keys($this->_exts) as $section) {
+			$output .= "[".$section."]\n";
+			
+			foreach (array_keys($this->_exts[$section]) as $extension) {
+				$priority = 0;
+				$prioritytable = array();
+				
+				foreach (array_keys($this->_exts[$section][$extension]) as $idx) {
+				
+					$ext = $this->_exts[$section][$extension][$idx];
+					
+					//var_dump($ext);
+					switch ($ext['basetag']) {
+						case '1': $priority = 1; break;
+						case 'n': $priority += 1; break;
+						default:
+							if (isset($prioritytable[$ext['basetag']])) {
+								$priority = $prioritytable[$ext['basetag']];
+							} else {
+								$priority = 'unknown!!!';
+							}
+						break;
+					}
+					
+					if ($ext['addpri']) {
+						$priority += $ext['addpri'];
+					}
+					
+					if ($ext['tag']) {
+						$prioritytable[$ext['tag']] = $priority;
+					}
+					
+					$output .= "exten => ".$extension.",".$priority.
+						",".$ext['cmd']->output()."\n";
+					
+				}
+				
 				if (isset($this->_hints[$section][$extension])) {
 					foreach ($this->_hints[$section][$extension] as $hint) {
 						$output .= "exten => ".$extension.",hint,".$hint;
@@ -219,15 +196,13 @@ class extensions {
 }
 
 class extension { 
-	var $_priadd;
-	
 	var $data;
 	
 	function extension($data) {
 		$this->data = $data;
 	}
 	
-	function increment($value) {
+	function incrementContents($value) {
 		return true;
 	}
 	
@@ -251,7 +226,7 @@ class ext_goto extends extension {
 		$this->context = $context;
 	}
 	
-	function increment($value) {
+	function incrementContents($value) {
 		$this->pri += $value;
 	}
 	
@@ -272,7 +247,7 @@ class ext_if extends extension {
 	function output() {
 		return 'If(' .$condition. '?' .$true_priority.($false_priority ? ':' .$false_priority : '' ). ')' ;
 	}
-	function increment($value) {
+	function incrementContents($value) {
 		$this->true_priority += $value;
 		$this->false_priority += $value;
 	}
@@ -314,46 +289,27 @@ class ext_setvar {
 
 $ext = new extensions;
 
-$dialpri = $ext->add('default','123',new ext_dial('ZAP/1234'));
-$ext->add('default','123',new ext_noop('test1'));
-$ext->add('default','123',new ext_noop('test2'));
-$ext->add('default','123',new ext_noop('test at +101'), $dialpri+101);
 
-$ext->add('default','100',new ext_noop('test100'));
-$ext->add('default','101',new ext_noop('test101'));
-$ext->add('default','102',new ext_noop('test102'));
-$ext->add('default','103',new ext_noop('test103'));
-
-$ext->add('testgoto','100',new ext_noop('test1'));
-$ext->add('testgoto','100',new ext_goto(3));
-$ext->add('testgoto','100',new ext_noop('test3'));
-
-$ext->add('testgoto','101',new ext_noop('test1'));
-$ext->add('testgoto','101',new ext_goto(3));
-$ext->add('testgoto','101',new ext_noop('test3'));
-
-$ext->add('testgoto','101',new ext_noop('test4'),2,true);
-
+$ext->add('default','123', 'dial1', new ext_dial('ZAP/1234'));
+$ext->add('default','123', '', new ext_noop('test1'));
+$ext->add('default','123', '', new ext_noop('test2'));
+$ext->add('default','123', '', new ext_noop('test at +101'), 'dial1', 101);
+$ext->add('default','123', '', new ext_noop('test at +102'));
 echo "<pre>";
 echo $ext->generateConf();
+echo $ext->generateOldConf();
+exit;
 
-
-[default][123][1] = dial
-[default][123][2] = noop test1
-[default][123][3] = noop test2
-[default][123][102] = noop test101
-[default][123][103] = noop test101
-
-related[default][123][101] = array(1,102)
-
-increment @ 1 --
-
-for ($i = 1; isset($ext[$i]); $i++)
-related
-
-foreach related {
-  if $rel[0] == 1    increment($key)
-}  
-
+/*
+exten => 123,1(dial1),Dial(ZAP/1234)
+exten => 123,n,noop(test1)
+exten => 123,n,noop(test2)
+exten => 123,dial1+101,noop(test at 101)
+exten => 123,n,noop(test at 102)
+*/
 
 ?>
+
+
+
+
