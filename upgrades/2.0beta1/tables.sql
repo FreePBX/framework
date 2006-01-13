2de2fcb3f038e3e5cdfc2b1ca241e29826f4a2b7
Ryan Courtnage <ryan@courtnage.ca>
2006-01-13 22:47:46 +0000

    preparing for 2.0beta1

--- upgrades/2.0beta1/tables.sql
+++ upgrades/2.0beta1/tables.sql
@@ -2,7 +2,7 @@
 -- Create module tables
 -- 
 
-CREATE TABLE `modules` (
+CREATE TABLE IF NOT EXISTS `modules` (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `modulename` VARCHAR( 50 ) NOT NULL ,
 `version` VARCHAR( 20 ) NOT NULL ,
