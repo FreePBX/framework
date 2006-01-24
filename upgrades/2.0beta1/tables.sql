ba3eca074d06e26d49d88def53a82574406056e8
Ryan Courtnage <ryan@courtnage.ca>
2006-01-24 16:55:12 +0000

    devices table mod for emergency cid.  If you are running cvs-head, use ./install_amp --force-version=1.10.010

--- upgrades/2.0beta1/tables.sql
+++ upgrades/2.0beta1/tables.sql
@@ -8,3 +8,11 @@ CREATE TABLE IF NOT EXISTS `modules` (
 `version` VARCHAR( 20 ) NOT NULL ,
 `enabled` TINYINT NOT NULL
 ) TYPE = MYISAM ;
+
+
+--
+-- Upgrade Devices table
+--
+
+ALTER TABLE `devices` ADD `emergency_cid` VARCHAR( 100 ) NULL
+
