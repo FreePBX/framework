3eb050092b10b0d513deaa5712f1d41509a7758a
Rob Thomas <xrobau@gmail.com>
2006-03-02 01:31:20 +0000

    Minor fix - this wouldn't work if it existed and was empty.

--- upgrades/2.0beta1/emergancycid.php
+++ upgrades/2.0beta1/emergancycid.php
@@ -1,13 +1,8 @@
 <?php
 
-$sql = "SELECT * from devices";
+$sql = "SELECT emergency_cid from devices";
 $devices = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 if(DB::IsError($devices)) {     
-	die($devices->getMessage()); 
-}
-
-
-if(!array_key_exists("emergency_cid",$devices)) {
 	$sql = "ALTER TABLE devices ADD emergency_cid VARCHAR( 100 ) NULL";
 	$result = $db->query($sql); 
 	if(DB::IsError($result)) {     
