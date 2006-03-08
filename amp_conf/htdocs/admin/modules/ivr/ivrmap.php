af2a7b0ef192e08b0260db82e77f73da6c462219
Ryan Courtnage <ryan@courtnage.ca>
2006-03-08 21:42:14 +0000

    add Conference module to IVR map

--- amp_conf/htdocs/admin/modules/ivr/ivrmap.php
+++ amp_conf/htdocs/admin/modules/ivr/ivrmap.php
@@ -93,6 +93,9 @@ if (count($unique_aas) > 0) {
 			elseif ($application == 'Goto' && !(strpos($args[0],'timeconditions') === false)) {
 				echo '<li>'._("dialing").' '.$extension.' <b>'._("goes to Time Condition ").$args[1].'</b>';
 			}
+			elseif ($application == 'Goto' && !(strpos($args[0],'ext-meetme') === false)) {
+				echo '<li>'._("dialing").' '.$extension.' <b>'._("goes to Conference ").$args[1].'</b>';
+			}
 		}
 ?>
 			</ul>
