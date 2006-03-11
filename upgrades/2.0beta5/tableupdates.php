5c753dc53e408d29f664c0945a94c5ee1c2bad4e
mheydon1973 <mheydon1973>
2006-03-11 21:34:28 +0000

    Added ALERT_INFO to Incoming Routing...patch supplied by Godsey

--- upgrades/2.0beta5/tableupdates.php
+++ upgrades/2.0beta5/tableupdates.php
@@ -3,7 +3,9 @@
 // Meetme table will only exist if Conferencing module installed
 $sql = "SELECT exten FROM meetme";
 $confs = $db->getRow($sql, DB_FETCHMODE_ASSOC);
-if (!DB::IsError($confs)) { // no error...module not installed
+if (!DB::IsError($confs)) { // no error...module installed
+
+	// Add 'joinmsg' field
 	$sql = "SELECT joinmsg FROM meetme";
 	$confjoin = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 	if(DB::IsError($confjoin)) { // error, new field doesn't exist
@@ -14,5 +16,35 @@ if (!DB::IsError($confs)) { // no error...module not installed
 		}
 	}
 }
+
+// Incoming table should exist, but you never know
+$sql = "SELECT destination FROM incoming";
+$incomes = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+if (!DB::IsError($incomes)) { // no error...module installed
+
+        // Add 'alertinfo' field
+        $sql = "SELECT alertinfo FROM incoming";
+        $incomealertinfo = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+        if(DB::IsError($incomealertinfo)) { // error, new field doesn't exist
+                $sql = "ALTER TABLE incoming ADD alertinfo VARCHAR( 32 ) NULL";
+                $result = $db->query($sql);
+                if(DB::IsError($result)) {
+                        die($result->getDebugInfo());
+                }
+        }
+
+	// Add 'adddialopts' field
+	/* $sql = "SELECT adddialopts FROM incoming";
+	$incomeadddialopts = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+	if(DB::IsError($incomeadddialopts)) { // error, new field doesn't exist
+		$sql = "ALTER TABLE incoming ADD adddialopts VARCHAR( 10 ) NULL";
+                $result = $db->query($sql);
+                if(DB::IsError($result)) {
+                        die($result->getDebugInfo());
+                }
+	} */
+
+}
+
 ?>
 
