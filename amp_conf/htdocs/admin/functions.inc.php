35eb0abeb17e96d6410d685815573e964d471b5c
Rob Thomas <xrobau@gmail.com>
2006-03-22 23:29:16 +0000

    Finish merging 1207

--- amp_conf/htdocs/admin/functions.inc.php
+++ amp_conf/htdocs/admin/functions.inc.php
@@ -15,7 +15,7 @@
 function parse_amportal_conf($filename) {
 	$file = file($filename);
 	foreach ($file as $line) {
-		if (preg_match("/^\s*([\w]+)\s*=\s*\"?([\w\/\:\.\%-]*)\"?\s*([;#].*)?/",$line,$matches)) {
+		if (preg_match("/^\s*([a-zA-Z0-9]+)=([a-zA-Z0-9 .&-@=_<>\"\']+)\s*$/",$line,$matches)) {
 			$conf[ $matches[1] ] = $matches[2];
 		}
 	}
