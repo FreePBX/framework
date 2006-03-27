753d9e8f63db8de17f2f012f58f050ff8461b45f
Rob Thomas <xrobau@gmail.com>
2006-03-27 20:58:11 +0000

    If you went into a destination, and just clicked save without doing anything else, gotoN wouldn't be set. Preloading gotoN

--- amp_conf/htdocs/admin/functions.inc.php
+++ amp_conf/htdocs/admin/functions.inc.php
@@ -618,7 +618,7 @@ function drawselects($goto,$i) {
 	
 	// This is purely to remove a warning. 
 	if (!isset($selectHtml)) { $selectHtml=''; }
-	$selectHtml .= '<tr><td colspan=2><input type="hidden" name="goto'.$i.'" value="">';
+	$selectHtml .= '<tr><td colspan=2>'; 
 	
 	//check for module-specific destination functions
 	foreach ($active_modules as $mod => $displayname) {
@@ -642,9 +642,10 @@ function drawselects($goto,$i) {
 			}
 			
 			$selectHtml .=	'<input type="radio" name="goto_indicate'.$i.'" value="'.$mod.'" onclick="javascript:this.form.goto'.$i.'.value=\''.$mod.'\';" onkeypress="javascript:if (event.keyCode == 0 || (document.all && event.keyCode == 13)) this.form.goto'.$i.'.value=\''.$mod.'\';" '.($checked? 'CHECKED=CHECKED' : '').' /> '._($displayname['displayName']).': ';
+			if ($checked) { $goto = $mod; }
 			$selectHtml .=	'<select name="'.$mod.$i.'"/>';
 			$selectHtml .= $options;	
-			$selectHtml .=	'</select><br>';
+			$selectHtml .=	"</select><br>\n";
 		}
 	}
 	/* --- MODULES END --- */
@@ -653,6 +654,7 @@ function drawselects($goto,$i) {
 	$selectHtml .= '<input type="radio" name="goto_indicate'.$i.'" value="custom" onclick="javascript:this.form.goto'.$i.'.value=\'custom\';" onkeypress="javascript:if (event.keyCode == 0 || (document.all && event.keyCode == 13)) this.form.goto'.$i.'.value=\'custom\';" '.(strpos($goto,'custom') === false ? '' : 'CHECKED=CHECKED').' />';
 	$selectHtml .= '<a href="#" class="info"> '._("Custom App<span><br>ADVANCED USERS ONLY<br><br>Uses Goto() to send caller to a custom context.<br><br>The context name <b>MUST</b> contain the word 'custom' and should be in the format custom-context , extension , priority. Example entry:<br><br><b>custom-myapp,s,1</b><br><br>The <b>[custom-myapp]</b> context would need to be created and included in extensions_custom.conf</span>").'</a>:';
 	$selectHtml .= '<input type="text" size="15" name="custom'.$i.'" value="'.(strpos($goto,'custom') === false ? '' : $goto).'" />';
+	$selectHtml .= "\n<input type='hidden' name='goto$i' value='$goto'>";
 
 	//close off our row
 	$selectHtml .= '</td></tr>';
