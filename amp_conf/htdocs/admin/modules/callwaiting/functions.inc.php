ce0613a1b18631bd5f9e451af76fcf5a47f4c1e3
mheydon1973 <mheydon1973>
2006-04-07 00:01:52 +0000

    ** DEVELOPMENT ONLY -- WILL NOT WORK ** Example of new featurecodes code.  Pending changes to module system to allow running of .php scripts when installing a new module.  To test until then, append &testfeaturecodes=yes to URL

---
+++ amp_conf/htdocs/admin/modules/callwaiting/functions.inc.php
@@ -0,0 +1,90 @@
+<?php
+
+// FOLLOWING FUNCTION WILL BE STRIPPED OUT TO A SEPERATE FILE ONCE RYAN
+// CHANGES THE MODULE INSTALL PROCESS TO RUN A .PHP FILE AS WELL AS A .SQL FILE
+if ($_REQUEST['testfeaturecodes'] == "yes") {
+	callwaiting_install();
+	?>
+	<script language="javascript">
+	alert("callwaiting_install ran!");
+	</script>
+	<?php
+}
+function callwaiting_install() {
+	// Register FeatureCodes
+	// Activate
+	$fcc = new featurecode('callwaiting', 'cwon');
+	$fcc->init();
+	$fcc->setDescription('Call Waiting - Activate');
+	$fcc->setDefault('*70');
+	$fcc->update();
+	unset($fcc);
+	// Deactivate
+	$fcc = new featurecode('callwaiting', 'cwoff');
+	$fcc->init();
+	$fcc->setDescription('Call Waiting - Deactivate');
+	$fcc->setDefault('*71');
+	$fcc->update();
+	unset($fcc);	
+}
+
+function callwaiting_get_config($engine) {
+	$modulename = 'callwaiting';
+	
+	// This generates the dialplan
+	global $ext;  
+	switch($engine) {
+		case "asterisk":
+			if (is_array($featurelist = featurecodes_getModuleFeatures('callwaiting'))) {
+				foreach($featurelist as $item) {
+					$featurename = $item['featurename'];
+					$fname = $modulename.'_'.$featurename;
+					if (function_exists($fname)) {
+						$fcc = new featurecode($modulename, $featurename);
+						$fcc->init();
+						$fc = $fcc->getCodeActive();
+						unset($fcc);
+						
+						if ($fc != '')
+							$fname($fc);
+					} else {
+						$ext->add('from-internal-additional', 'debug', '', new ext_noop("No func $fname"));
+						var_dump($item);
+					}	
+				}
+			}
+		break;
+	}
+}
+
+function callwaiting_cwon($c) {
+	global $ext;
+
+	$id = "app-callwaiting-cwon"; // The context to be included
+
+	$ext->addInclude('from-internal-additional', $id); // Add the include from from-internal
+
+	$ext->add($id, $c, '', new ext_answer('')); // $cmd,1,Answer
+	$ext->add($id, $c, '', new ext_wait('1')); // $cmd,n,Wait(1)
+	$ext->add($id, $c, '', new ext_macro('user-callerid')); // $cmd,n,Macro(user-callerid)
+	$ext->add($id, $c, '', new ext_setvar('DB(CW/${CALLERID(number)})', 'ENABLED')); 
+	$ext->add($id, $c, '', new ext_playback('call-waiting&activated')); // $cmd,n,Playback(...)
+	$ext->add($id, $c, '', new ext_macro('hangupcall')); // $cmd,n,Macro(user-callerid)
+}
+
+
+function callwaiting_cwoff($c) {
+	global $ext;
+
+	$id = "app-callwaiting-cwoff"; // The context to be included
+
+	$ext->addInclude('from-internal-additional', $id); // Add the include from from-internal
+
+	$ext->add($id, $c, '', new ext_answer('')); // $cmd,1,Answer
+	$ext->add($id, $c, '', new ext_wait('1')); // $cmd,n,Wait(1)
+	$ext->add($id, $c, '', new ext_macro('user-callerid')); // $cmd,n,Macro(user-callerid)
+	$ext->add($id, $c, '', new ext_dbdel('CW/${CALLERID(number)}')); 
+	$ext->add($id, $c, '', new ext_playback('call-waiting&de-activated')); // $cmd,n,Playback(...)
+	$ext->add($id, $c, '', new ext_macro('hangupcall')); // $cmd,n,Macro(user-callerid)
+}
+?>
