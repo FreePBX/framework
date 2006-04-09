667841eab4eef80255947c1a5a7b51de39edc1d9
mheydon1973 <mheydon1973>
2006-04-09 13:10:03 +0000

    User logon/off now using FeaturesCodes....macro should probably be removed but at the moment userlogon depends on logoff

---
+++ upgrades/2.1beta1/featurecodes.php
@@ -0,0 +1,22 @@
+<?php
+
+// Add FeatureCodes for 'core' module
+// note i'm not even checking if core is loaded but that doesn't matter
+// codes are only used where relevent so doesn't matter if a code is added
+// and that module isn't in use
+
+require_once($amp_conf["AMPWEBROOT"].'/admin/functions.inc.php');
+
+$fcc = new featurecode('core', 'userlogon');
+$fcc->setDescription('User Logon');
+$fcc->setDefault('*11');
+$fcc->update();
+unset($fcc);
+
+$fcc = new featurecode('core', 'userlogoff');
+$fcc->setDescription('User Logoff');
+$fcc->setDefault('*12');
+$fcc->update();
+unset($fcc);
+
+?>
