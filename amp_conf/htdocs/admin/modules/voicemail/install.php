fc50b359d442b4cca2c71c29cac938173ddcb809
mheydon1973 <mheydon1973>
2006-04-09 20:17:02 +0000

    Looks like somewhere along the line, the voicemail feature codes were typo'd......this should put them back to the original.
    Anybody using SVN-HEAD will have to un-install and re-install the Voicemail module to pick this up

--- amp_conf/htdocs/admin/modules/voicemail/install.php
+++ amp_conf/htdocs/admin/modules/voicemail/install.php
@@ -3,14 +3,14 @@
 // Register FeatureCode - Activate
 $fcc = new featurecode('voicemail', 'myvoicemail');
 $fcc->setDescription('My Voicemail');
-$fcc->setDefault('*98');
+$fcc->setDefault('*97');
 $fcc->update();
 unset($fcc);
 
 // Register FeatureCode - Deactivate
 $fcc = new featurecode('voicemail', 'dialvoicemail');
 $fcc->setDescription('Dial Voicemail');
-$fcc->setDefault('*96');
+$fcc->setDefault('*98');
 $fcc->update();
 unset($fcc);
 
