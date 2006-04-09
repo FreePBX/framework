4e073f6f58d2af836b2545f144fbca50df96eb0d
mheydon1973 <mheydon1973>
2006-04-09 23:33:16 +0000

    Install script wasn't running sql files first

---
+++ upgrades/2.1beta1/upgrade.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once($amp_conf["AMPWEBROOT"] . "/admin/functions.inc.php");
+
+$fcc = new featurecode('core', 'userlogon');
+$fcc->setDescription('User Logon');
+$fcc->setDefault('*11');
+$fcc->update();
+unset($fcc);
+
+$fcc = new featurecode('core', 'userlogoff');
+$fcc->setDescription('User Logoff');
+$fcc->setDefault('*12');
+$fcc->update();
+unset($fcc);
+
+$fcc = new featurecode('core', 'zapbarge');
+$fcc->setDescription('ZapBarge');
+$fcc->setDefault('888');
+$fcc->update();
+unset($fcc);
+
+$fcc = new featurecode('core', 'simu_pstn');
+$fcc->setDescription('Simulate Incoming Call');
+$fcc->setDefault('7777');
+$fcc->update();
+unset($fcc);
+
+$fcc = new featurecode('core', 'simu_fax');
+$fcc->setDescription('Simulate Incoming FAX Call');
+$fcc->setDefault('666');
+$fcc->update();
+unset($fcc);
+
+?>
