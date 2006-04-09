d392295d55d69da856d8d967f9b7f84a90d957b2
mheydon1973 <mheydon1973>
2006-04-09 14:58:22 +0000

    Typo by me

--- upgrades/2.1beta1/tables.php
+++ upgrades/2.1beta1/tables.php
@@ -1,11 +1,11 @@
 <?php
 
 // Meetme table will only exist if Conferencing module installed
-$sql = "SELECT value FROM globals WHERE variable = 'TIMEZONE' ";
+$sql = "SELECT value FROM globals WHERE variable = 'TONEZONE' ";
 $tz = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 if (!is_array($tz)) { // does not exist already
         // Default to 'us'
-        $sql = "INSERT INTO globals (variable, value) VALUES ('TIMEZONE', 'us') ";
+        $sql = "INSERT INTO globals (variable, value) VALUES ('TONEZONE', 'us') ";
         $result = $db->query($sql);
         if(DB::IsError($result)) {
                 die($result->getDebugInfo());
