17998a1de637e39512fe55e1f03dc5c9070b4a55
mheydon1973 <mheydon1973>
2006-04-11 22:38:06 +0000

    Module upgrade script should be in module's install.php file and NOT the freepbx upgrade folder

--- upgrades/2.1beta1/tables.php
+++ upgrades/2.1beta1/tables.php
@@ -23,30 +23,4 @@ if(DB::IsError($check)) {
         }
 }
 
-// New description on Ring Groups
-$sql = "SELECT description FROM ringgroups";
-$check = $db->getRow($sql, DB_FETCHMODE_ASSOC);
-if(DB::IsError($check)) {
-	// add new field
-    $sql = "ALTER TABLE ringgroups ADD description VARCHAR( 35 ) NULL ;";
-    $result = $db->query($sql);
-    if(DB::IsError($result)) {
-            die($result->getDebugInfo());
-    }
-
-    // update existing groups
-    $sql = "UPDATE ringgroups SET description = CONCAT('Ring Group ', grpnum) WHERE description IS NULL ;";
-    $result = $db->query($sql);
-    if(DB::IsError($result)) {
-            die($result->getDebugInfo());
-    }
-
-	// make new field required
-	$sql = "ALTER TABLE `ringgroups` CHANGE `description` `description` VARCHAR( 35 ) NOT NULL ;";
-    $result = $db->query($sql);
-    if(DB::IsError($result)) {
-            die($result->getDebugInfo());
-    }
-}
-
 ?>
