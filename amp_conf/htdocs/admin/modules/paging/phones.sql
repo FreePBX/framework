21532438a17d887c7ef8a577f71cf569ac3e4a32
Ryan Courtnage <ryan@courtnage.ca>
2006-04-12 21:31:51 +0000

    move paging dr brown

--- amp_conf/htdocs/admin/modules/paging/phones.sql
+++
@@ -1,8 +0,0 @@
-# These are the three most common ways of auto answering.
-INSERT INTO paging_phones VALUES ('GXP-2000', 1, 'Set(SIPADDHEADER="Call-Info: answer-after=0")')
-INSERT INTO paging_phones VALUES ('Polycom', 1, 'Set(ALERT_INFO="Ring Answer")')
-INSERT INTO paging_phones VALUES ('Snom', 1, 'Set(SIP_URI_OPTIONS="intercom=true")')
-
-
-
-
--- amp_conf/htdocs/admin/modules/paging/uninstall.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE paging_overview;
-DROP TABLE paging_groups;
-DROP TABLE paging_phones;
