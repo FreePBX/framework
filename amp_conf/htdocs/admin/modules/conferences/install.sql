6a786a374bfb0d4c57a3255073062381313f050e
Ryan Courtnage <ryan@courtnage.ca>
2006-04-12 21:28:18 +0000

    move module - make sure i don't move core

--- amp_conf/htdocs/admin/modules/conferences/install.sql
+++
@@ -1,4 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `meetme` ( `exten` VARCHAR( 50 ) NOT NULL , `options` VARCHAR( 15 ) , `userpin` VARCHAR( 50 ) , `adminpin` VARCHAR( 50 ) , `description` VARCHAR( 50 ) , `joinmsg` VARCHAR( 255 ) );
-
-
