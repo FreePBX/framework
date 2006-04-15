cf2cc26e66b51cd5b73735267cce6112482e85c0
mheydon1973 <mheydon1973>
2006-04-15 11:20:54 +0000

    Updated Rob's scripts -- I guess we won't be doing this long term thou ????

--- amp_conf/htdocs/admin/modules/import.sh
+++ amp_conf/htdocs/admin/modules/import.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests
 do
+  echo $modname
   svn co https://svn.sourceforge.net/svnroot/amportal/modules/branches/2.1/$modname
 done
--- amp_conf/htdocs/admin/modules/update.sh
+++ amp_conf/htdocs/admin/modules/update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests
 do
+  echo $modname
   cd $modname
   svn update
   cd ..
