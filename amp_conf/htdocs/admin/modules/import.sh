0b1047ba211286c89788611f34282a670275e4a0
mheydon1973 <mheydon1973>
2006-04-18 21:36:01 +0000

    Custom trunks weren't using DIAL_OPTIONS

--- amp_conf/htdocs/admin/modules/import.sh
+++ amp_conf/htdocs/admin/modules/import.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback
 do
   echo $modname
   svn co https://svn.sourceforge.net/svnroot/amportal/modules/branches/2.1/$modname
--- amp_conf/htdocs/admin/modules/status.sh
+++ amp_conf/htdocs/admin/modules/status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback
 do
   echo $modname
   cd $modname
