dc0c4b90f7382cb52e99ac361bd6b9e7f82232fd
mheydon1973 <mheydon1973>
2006-04-26 21:28:16 +0000

    Update module import/update/status scripts....There's a better way of doing this, right?

--- amp_conf/htdocs/admin/modules/import.sh
+++ amp_conf/htdocs/admin/modules/import.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow
 do
   echo $modname
   svn co https://svn.sourceforge.net/svnroot/amportal/modules/branches/2.1/$modname
--- amp_conf/htdocs/admin/modules/status.sh
+++ amp_conf/htdocs/admin/modules/status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow
 do
   echo $modname
   cd $modname
