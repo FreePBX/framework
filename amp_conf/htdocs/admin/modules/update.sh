2b10bbf9f3cd7e739f82445e4ae09572a0e8bbcb
Rob Thomas <xrobau@gmail.com>
2006-04-29 22:48:35 +0000

    Yes, I really need to do this a better way.

--- amp_conf/htdocs/admin/modules/update.sh
+++ amp_conf/htdocs/admin/modules/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow
+for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow javassh
 do
   echo $modname
   cd $modname
