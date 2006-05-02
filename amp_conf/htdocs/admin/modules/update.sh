83b90200a27a67ee8721decc35365313d5a5f828
Ryan Courtnage <ryan@courtnage.ca>
2006-05-02 19:21:02 +0000

    #748 (Tracking SVN head is not fun anymore...).  We no longer need maintain a list of modules to loop through.  Hope this helps.

--- amp_conf/htdocs/admin/modules/update.sh
+++ amp_conf/htdocs/admin/modules/update.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-for modname in asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow javassh logfiles
-do
-  echo $modname
-  cd $modname
-  svn update
-  cd ..
-done
+svn update
+
