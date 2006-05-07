5052b598767e66dc576cf91eaf147e124736d79d
mheydon1973 <mheydon1973>
2006-05-07 00:55:05 +0000

    * Ticket #743 (Bugs) -- Error in macro-vm
    Option for Default/Unavailable/Busy/None now on General Settings
    
    * Ticket #732 (Feature Requests) -- Give GUI/dialplan to voicemail gain adjustment
    
    Optional Gain setting on General Settings
    
    * General Settings Cleanup
    
    Re-arranged all Voicemail options under their own heading

---
+++ upgrades/2.1beta3/tables.php
@@ -0,0 +1,27 @@
+<?php
+
+// Add default VM_DDTYPE of '' if no VM_DDTYPE exists already
+$sql = "SELECT value FROM globals WHERE variable = 'VM_DDTYPE' ";
+$asa = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+if (!is_array($asa)) { // does not exist already
+        // Default to ''
+        $sql = "INSERT INTO globals (variable, value) VALUES ('VM_DDTYPE', '') ";
+        $result = $db->query($sql);
+        if(DB::IsError($result)) {
+                die($result->getDebugInfo());
+        }
+}
+
+// Add default VM_GAIN of '' if no VM_GAIN exists already
+$sql = "SELECT value FROM globals WHERE variable = 'VM_GAIN' ";
+$asb = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+if (!is_array($asb)) { // does not exist already
+        // Default to ''
+        $sql = "INSERT INTO globals (variable, value) VALUES ('VM_GAIN', '') ";
+        $result = $db->query($sql);
+        if(DB::IsError($result)) {
+                die($result->getDebugInfo());
+        }
+}
+
+?>
