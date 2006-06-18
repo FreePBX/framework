fa97805ea238eba6f59ee585f05ee71cc47fc86b
mheydon1973 <mheydon1973>
2006-06-18 20:01:48 +0000

    Updated to current 'official' module list

--- amp_conf/htdocs/admin/modules/modlist.sh
+++ amp_conf/htdocs/admin/modules/modlist.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 FREEPBX_MODBRANCH=2.2
-FREEPBX_MODLIST="asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow javassh logfiles manager pinsets gabcast"
+FREEPBX_MODLIST="asterisk-cli backup callforward callwaiting conferences disa donotdisturb featurecodeadmin infoservices irc ivr music paging queues recordings ringgroups timeconditions voicemail miscdests callback findmefollow javassh logfiles manager pinsets gabcast printextensions phpagiconf inventorydb"
