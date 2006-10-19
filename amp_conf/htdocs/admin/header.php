1eb3b1aa553d9c5d3d9e60e18c222eed19c067cd
Diego Iastrubni <diego.iastrubni@xorcom.com>
2006-10-19 08:42:12 +0000

    re-organized the header page. now the php code is much cleaner, and the html code is clearer, and contains less html errors.

--- amp_conf/htdocs/admin/header.php
+++ amp_conf/htdocs/admin/header.php
@@ -11,17 +11,51 @@
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 
-//get the current file name
-    $currentFile = $_SERVER["PHP_SELF"];
-    $parts = Explode('/', $currentFile);
-    //header('Content-type: text/html; charset=utf-8');
-    $currentFile = $parts[count($parts) - 1];
+// helper function, to draw the upper links
+function print_sub_tool( $name, $page, $is_current, $href=NULL, $new_window=false )
+{
+	if (!is_file($page))
+		return;
 
-    if (!extension_loaded('gettext')) {
-           function _($str) {
-                   return $str;
-           }
-    }
+	$html = "<a ";
+
+	if ($href == NULL)
+		$href .= $page;
+
+	if ($new_window != NULL)
+		$html .= " target=\"_blank\" ";
+	
+	if ($is_current)
+		$html .= "id=current ";
+	$html .= "href=\"$href\"> &#8226; <li>$name</li></a>";
+
+	print("\t\t$html\n");
+}
+
+
+//get the current file name
+$currentFile = $_SERVER["PHP_SELF"];
+$parts = Explode('/', $currentFile);
+//header('Content-type: text/html; charset=utf-8');
+$currentFile = $parts[count($parts) - 1];
+
+if (!extension_loaded('gettext')) {
+	function _($str) {
+		return $str;
+	}
+}
+
+if (extension_loaded('gettext')) {
+	if (isset($_COOKIE['lang'])) {
+		setlocale(LC_ALL,  $_COOKIE['lang']);
+		putenv("LANGUAGE=".$_COOKIE['lang']);
+	} else {
+		setlocale(LC_ALL,  'en_US');
+	}
+	bindtextdomain('amp','./i18n');
+	bind_textdomain_codeset('amp', 'utf8');
+	textdomain('amp');
+}
 
 if (!$quietmode) {
 ?>
@@ -30,113 +64,60 @@ if (!$quietmode) {
 <html>
 
 <head>
-    <title><?php  echo _($title) ?></title>
-    <meta http-equiv="Content-Type" content="text/html">
-    <link href="common/mainstyle.css" rel="stylesheet" type="text/css"> 
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-    <?php 
+	<title><?php  echo _($title) ?></title>
+	<meta http-equiv="Content-Type" content="text/html">
+	<link href="common/mainstyle.css" rel="stylesheet" type="text/css"> 
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+	<?php 
 	if (isset($display) && is_file("modules/{$display}/{$display}.css")) {
-	         echo "	<link href=\"modules/{$display}/{$display}.css\" rel=\"stylesheet\" type=\"text/css\">\n";
+		echo "	<link href=\"modules/{$display}/{$display}.css\" rel=\"stylesheet\" type=\"text/css\">\n";
 	}
-     ?>
-
-    <script type="text/javascript" src="common/script.js.php"></script>
-    <script type="text/javascript"> 
+	?>
+	
+	<script type="text/javascript" src="common/script.js.php"></script>
+	<script type="text/javascript"> 
 		<!--
 		// Disable browser's Back button on another pg being able to go back to this pg.
 		history.forward();
 		//-->
-    </script> 
+	</script>
 <!--[if IE]>
     <style type="text/css">div.inyourface a{position:absolute;}</style>
 <![endif]-->
 </head>
 
-<?php
-	if (extension_loaded('gettext')) {
-		if (isset($_COOKIE['lang'])) {
-			setlocale(LC_ALL,  $_COOKIE['lang']);
-			putenv("LANGUAGE=".$_COOKIE['lang']);
-		} else {
-			setlocale(LC_ALL,  'en_US');
-		}
-		bindtextdomain('amp','./i18n');
-		bind_textdomain_codeset('amp', 'utf8');
-		textdomain('amp');
-	}
-?>
-
 <body onload="setAllInfoToHideSelects();"  <?
-
 // Check if it's a RIGHT TO LEFT character set (eg, hebrew, arabic, whatever)
 //$_COOKIE['lang']="he_IL";
 if ($_COOKIE['lang']==="he_IL") 
 	echo "dir=\"rtl\"";
 ?> >
 <div id="page">
-
-<div class="header">
-
+	<div class="header">
 <?php
-if (isset($amp_conf["AMPADMINLOGO"]) && is_file($amp_conf["AMPWEBROOT"]."/admin/images/".$amp_conf["AMPADMINLOGO"]))
-{ ?>
-    <a href="index.php"><img src="images/<?php echo $amp_conf["AMPADMINLOGO"] ?>"/></a>
-<?php } else { ?>
-    <a href="index.php"><img src="images/freepbx.png"/></a>
-<?php } 
-if (!isset($_REQUEST['type'])) { $_REQUEST['type'] = 'setup'; } 
+		if (isset($amp_conf["AMPADMINLOGO"]) && is_file($amp_conf["AMPWEBROOT"]."/admin/images/".$amp_conf["AMPADMINLOGO"]))
+			echo "\t\t<a href=\"index.php\"><img src=\"images/" . $amp_conf["AMPADMINLOGO"] . "\"/></a>\n";
+		else
+			echo "\t\t<a href=\"index.php\"><img src=\"images/freepbx.png\"/></a>\n";
+		
+		print_sub_tool( _("Management"), "manage.php" , $currentFile=='manage.php' );
+		print_sub_tool( _("Setup")     , "config.php" , $currentFile=='config.php' && $_REQUEST['type']=='setup', "config.php?type=setup", false );
+		print_sub_tool( _("Tools")     , "config.php" , $currentFile=='config.php' && $_REQUEST['type']=='tool' , "config.php?type=tool", false );
+		print_sub_tool( _("Reports")   , "reports.php", $currentFile=='reports.php' );
+		print_sub_tool( _("Panel")     , "panel.php"  , $currentFile=='panel.php' );
+		print_sub_tool( _("Recordings"), "../recordings/index.php"  ,0, NULL, true );
 ?>
+	</div>
 
-<?php if (is_file("manage.php")){ ?>
-	<a id="<?php echo ($currentFile=='manage.php' ? 'current':'') ?>" href="manage.php?">
-		&#8226;
-		<li><?php echo _("Management") ?></li>
-	</a>
-<?php } ?>
-
-	<a id="<?php echo ($currentFile=='config.php' && $_REQUEST['type']=='setup' ? 'current':'') ?>" href="config.php?type=setup">
-		&#8226;
-		<li><?php echo _("Setup") ?></li>
-	</a>
-
-	<a id="<?php echo ($currentFile=='config.php' && $_REQUEST['type']=='tool' ? 'current':'') ?>" href="config.php?type=tool">
-		&#8226;
-		<li><?php echo _("Tools") ?></li>
-	</a>
-
-<?php if (is_file("cdr/cdr.php")){ ?>
-	<a id="<?php echo ($currentFile=='reports.php' ? 'current':'') ?>" href="reports.php?">
-		&#8226;
-		<li><?php echo _("Reports") ?></li>
-	</a>
-<?php } ?>
-
-<?php if (is_file("../panel/index_amp.php")){ ?>
-	<a id="<?php echo ($currentFile=='panel.php' ? 'current':'') ?>" href="panel.php?">
-		&#8226;
-		<li><?php echo _("Panel") ?></li>
-	</a>
-<?php } ?>
-
-<?php if (is_file("../recordings/index.php")){ ?>
-	<a href="../recordings/index.php" target="_blank">
-		&#8226;
-		<li><?php echo _("Recordings") ?></li>
-	</a>
-<?php } ?>
-
-</div>
-
-<div class="message">
-<?php  
+	<div class="message"><?php
 	if ( isset($_SESSION['AMP_user']) &&  $amp_conf['AUTHTYPE'] != 'none' ) {
 		echo _('Logged in: ').$_SESSION['AMP_user']->username;
 		echo ' (<a href="http'.(isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!=''?'s':'').'://';
 
-		if (!ereg('MSIE', $_SERVER['HTTP_USER_AGENT'])) { 
+		if (!ereg('MSIE', $_SERVER['HTTP_USER_AGENT'])) {
 			// use other logout for Firefox and other browsers 
-			echo 'logout:logout@'; 
-		} 
+			echo 'logout:logout@';
+		}
 
 		$pathLength = strrpos($_SERVER['PHP_SELF'],'/');
 		$logoutPath = ($pathLength === false) ? '' : substr($_SERVER['PHP_SELF'],0,$pathLength);
@@ -144,8 +125,7 @@ if (!isset($_REQUEST['type'])) { $_REQUEST['type'] = 'setup'; }
 		echo $_SERVER['HTTP_HOST'].$logoutPath.'/logout.php">Logout</a>)&nbsp;::&nbsp;';
 	}
 	echo _($message);
-?>
-</div>
+?></div>
 
 <?php
 } // End 'quietmode' check
