6cb073ff00ad4ecb250353a6c3c87a1b232c08e3
Diego Iastrubni <diego.iastrubni@xorcom.com>
2006-10-22 08:31:56 +0000

    index.php cleanups: most of the logic is not needed here, removed and forgotten. now it is a valid html 4.01, both php and html code are much cleaner.

--- amp_conf/htdocs/admin/index.php
+++ amp_conf/htdocs/admin/index.php
@@ -12,100 +12,80 @@
 //GNU General Public License for more details.
 // start session
 session_start();
-?>
 
-<?php
 $title=_("freePBX administration");
 $message=_("Administration");
 
 require_once('functions.inc.php');
 
 // get settings
-$amp_conf = parse_amportal_conf("/etc/amportal.conf");
+$amp_conf      = parse_amportal_conf("/etc/amportal.conf");
 $asterisk_conf = parse_asterisk_conf("/etc/asterisk/asterisk.conf");
 
 // connect to database
 require_once('common/db_connect.php'); //PEAR must be installed
 
 $quietmode = false;
-include 'header.php'; 
+include 'header.php';
 
-if (isset($_REQUEST['display'])) {
-	$display=$_REQUEST['display'];
-} else { 
-	$display='';
-}
 ?>
-
-<div class="nav">
-    <li><a id="<?php  echo ($display=='' ? 'current':'') ?>" href="index.php">Welcome</a></li>
+<div id="nav">
+	<ul>
+		<li>
+			<a href="index.php" id="current">Welcome</a>
+		</li>
+	</ul>
 </div>
 
+<div id="wrapper">
+	<div class="content">
 
-<div class="content">
-
-<?php
-if (isset($_REQUEST['display'])) {
-	$display=$_REQUEST['display'];
-} else { 
-	$display='';
-}
-switch($display) {
-    default:
-?>
-
-    <p align="right">
-	<?php if (extension_loaded('gettext')) {
+	<p align="right">
+<?php 
+	if (extension_loaded('gettext')) {
 		if (!isset($_COOKIE['lang'])) {
 			$_COOKIE['lang'] = "en_US";
-		} ?>
+		} 
+	}
+?>
 &nbsp;&nbsp;&nbsp;<?php echo _("Language:") ?>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select onchange="javascript:changeLang(this.value)">
-<option value="en_US" <? echo ($_COOKIE['lang']=="en_US" ? "selected" : "") ?> >English</option>
-<option value="fr_FR" <? echo ($_COOKIE['lang']=="fr_FR" ? "selected" : "") ?> >Fran&ccedil;ais</option>
-<option value="de_DE" <? echo ($_COOKIE['lang']=="de_DE" ? "selected" : "") ?> >Deutsch</option>
-<option value="it_IT" <? echo ($_COOKIE['lang']=="it_IT" ? "selected" : "") ?> >Italiano</option>
-<option value="es_ES" <? echo ($_COOKIE['lang']=="es_ES" ? "selected" : "") ?> >Espa&ntilde;ol</option>
-<option value="ru_RU" <? echo ($_COOKIE['lang']=="ru_RU" ? "selected" : "") ?> >Russki</option>
-<option value="pt_PT" <? echo ($_COOKIE['lang']=="pt_PT" ? "selected" : "") ?> >Portuguese</option>
-<option value="he_IL" <? echo ($_COOKIE['lang']=="he_IL" ? "selected" : "") ?> >Hebrew</option>
-</select>
-	<?php } ?>
-    </p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<select onchange="javascript:changeLang(this.value)">
+		<option value="en_US" <? echo ($_COOKIE['lang']=="en_US" ? "selected" : "") ?> >English</option>
+		<option value="fr_FR" <? echo ($_COOKIE['lang']=="fr_FR" ? "selected" : "") ?> >Fran&ccedil;ais</option>
+		<option value="de_DE" <? echo ($_COOKIE['lang']=="de_DE" ? "selected" : "") ?> >Deutsch</option>
+		<option value="it_IT" <? echo ($_COOKIE['lang']=="it_IT" ? "selected" : "") ?> >Italiano</option>
+		<option value="es_ES" <? echo ($_COOKIE['lang']=="es_ES" ? "selected" : "") ?> >Espa&ntilde;ol</option>
+		<option value="ru_RU" <? echo ($_COOKIE['lang']=="ru_RU" ? "selected" : "") ?> >Russki</option>
+		<option value="pt_PT" <? echo ($_COOKIE['lang']=="pt_PT" ? "selected" : "") ?> >Portuguese</option>
+		<option value="he_IL" <? echo ($_COOKIE['lang']=="he_IL" ? "selected" : "") ?> >Hebrew</option>
+		</select>
+	</p>
 
-    <h2>freePBX</h2>
-    <p>
-        <?php echo _("Welcome to the FreePBX Administration") ?> <?php $ver=getversion(); echo $ver[0][0];?>
-        <br><br><br><br><br><br>
-    </p>
-    
-<?php
-    break;
-    case '1':
-?>
-
-
-    
-<?php
-    break;
-}
-?>
-
+	<h2>freePBX</h2>
+	<p>
+	<?php echo _("Welcome to the FreePBX Administration") ?> <?php $ver=getversion(); echo $ver[0][0];?>
+	<br><br><br><br><br><br>
+	</p>
+	</div>
 </div>
 
-
-
-
-</div>
-
-<script language="javascript">
+<script type="text/javascript">
 <!--
-
 function changeLang(lang) {
 	document.cookie='lang='+lang;
 	window.location.reload();
 }
-
 //-->
 </script>
 
+<?
+include "footer.php";
+?>
+
+
+</div> <!-- /page -->
+
+</body>
+
+</html>
