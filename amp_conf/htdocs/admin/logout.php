6a284980439f4d92461d1d33830d833b0c060655
WeBRainstorm S.r.l <ask@webrainstorm.it>
2006-11-04 13:59:46 +0000

    Fix for #1227 also with HTML fixes for new UI

--- amp_conf/htdocs/admin/logout.php
+++ amp_conf/htdocs/admin/logout.php
@@ -28,12 +28,35 @@ $amp_conf = parse_amportal_conf("/etc/amportal.conf");
 // a variable 'logout' is added:
 $_SESSION = array('logout' => true);
 
+require_once('common/db_connect.php'); 
 include 'header.php'; 
+
+if (!$quietmode) {
+	// Empty navigation div
+	echo "<div id=\"nav\">\n";
+	echo "</div>\n\n";
+	
+	echo "<div id=\"wrapper\"><div id=\"background-wrapper\">\n";
+		
+	echo "<div id=\"left-corner\"></div>\n";
+	echo "<div id=\"right-corner\"></div>\n";
+		
+	echo "<div class=\"content\">\n";
+	
+	echo "<p>";
+	echo "<br><br><br><br>";
+	echo "<h2><center> ". _('You are now logged out.')."</center></h2>";
+	echo "<br><br><br><br>";
+	echo "</p>";
+	
+	echo "\t</div> <!-- /content -->\n";
+	
+	include('footer.php');
+	echo "</div></div> <!-- /background-wrapper, /wrapper -->\n";
+
+	echo "</div> <!-- /page -->\n";
+	echo "</body>\n";
+	echo "</html>\n";
+}
 ?>
 
-<p>
-	<br><br><br><br>
-	<h2><center> <?php echo _('You are now logged out.') ?> </center></h2>
-	<br><br><br><br>
-	<?php include 'footer.php'; ?>
-</p>
