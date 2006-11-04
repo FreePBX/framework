bc296fc1b091c4f528c338bc6b5f4260b5f62d97
Rob Thomas <xrobau@gmail.com>
2006-11-04 08:54:06 +0000

    Make sure it's removed in the upgrade.

--- upgrades/2.2.0beta1/ampbackup.php
+++ upgrades/2.2.0beta1/ampbackup.php
@@ -1,9 +1,17 @@
 <?
 
 global $asterisk_conf;
-outn("Cleaning up leftover backup scripts that were distributed in core...");
+out("Cleaning up leftover backup scripts that were distributed in core...");
+outn("ampbackup.pl...");
 if (file_exists($asterisk_conf['astvarlibdir']."/bin/ampbackup.pl") && !is_link($asterisk_conf['astvarlibdir']."/bin/ampbackup.pl")) {
 	unlink($asterisk_conf['astvarlibdir']."/bin/ampbackup.pl");
+	out("Done");
+} else {
+	out("Not Required");
+}
+outn("retrieve_backup_cron_from_mysql.pl...");
+if (file_exists($asterisk_conf['astvarlibdir']."/bin/retrieve_backup_cron_from_mysql.pl") && !is_link($asterisk_conf['astvarlibdir']."/bin/retrieve_backup_cron_from_mysql.pl")) {
+	unlink($asterisk_conf['astvarlibdir']."/bin/retrieve_backup_cron_from_mysql.pl");
 	out("Done");
 } else {
 	out("Not Required");
