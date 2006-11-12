3d0a4a95c0d67b9fab52f7e9e10f1b66173103ff
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2006-11-12 01:12:16 +0000

    reverting r2418 which seems to have cause #1309 and #1320

--- amp_conf/htdocs/admin/cdr/cdr.php
+++ amp_conf/htdocs/admin/cdr/cdr.php
@@ -20,10 +20,10 @@ function cdrpage_getpost_ifset($test_vars)
 	foreach($test_vars as $test_var) { 
 		if (isset($_POST[$test_var])) { 
 			global $$test_var;
-			$$test_var = mysql_real_escape_string($_POST[$test_var]); 
+			$$test_var = $_POST[$test_var]; 
 		} elseif (isset($_GET[$test_var])) {
 			global $$test_var; 
-			$$test_var = mysql_real_escape_string($_GET[$test_var]);
+			$$test_var = $_GET[$test_var];
 		}
 	}
 }
