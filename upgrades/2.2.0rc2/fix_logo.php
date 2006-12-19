871e409e5ef94c0523e7ad18ecc784f967d4a59f
Rob Thomas <xrobau@gmail.com>
2006-12-19 20:45:48 +0000

    Merge changes from 2.2 into trunk - fix for set_caller_pres, add rc2 changes.

---
+++ upgrades/2.2.0rc2/fix_logo.php
@@ -0,0 +1,6 @@
+<?php
+// As part of the new UI, make sure that the old 'amp.bmp' is changed to 'freepbx.png'
+if ($amp_conf['AMPADMINLOGO'] == "amp.bmp") {
+	$amp_conf['AMPADMINLOGO'] == "freepbx.png";
+}
+?>
