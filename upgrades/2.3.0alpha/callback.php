41e07a70e3df4c97815069986fbd490f0de71a55
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-06-19 01:28:48 +0000

    #1984: move callback agi script to module

---
+++ upgrades/2.3.0alpha/callback.php
@@ -0,0 +1,13 @@
+<?
+
+global $asterisk_conf;
+out("Cleaning up leftover callback script that was distributed in core...");
+outn("callback...");
+if (file_exists($asterisk_conf['astvarlibdir']."/bin/callback") && !is_link($asterisk_conf['astvarlibdir']."/bin/callback")) {
+	unlink($asterisk_conf['astvarlibdir']."/bin/callback");
+	out("Done");
+} else {
+	out("Not Required");
+}
+
+?>
