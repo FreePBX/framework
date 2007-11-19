60f147a93f8771055d3bda77fba1c7f8b19c978d
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-11-19 06:07:35 +0000

    script to set all the hints according to current login state of all devices. will be incorporated to rebuild hints after a reload

---
+++ amp_conf/bin/reset-hints.php
@@ -0,0 +1,101 @@
+#!/usr/bin/php -q
+<?php
+
+	$debug = -1;
+
+	if (isset($argv[1]) && is_numeric($argv[1])) {
+	 	sleep($argv[1]);
+	} else {
+		sleep(0);
+	}
+
+	$amp_conf = parse_amportal_conf_bootstrap("/etc/amportal.conf");
+
+	require_once($amp_conf['AMPWEBROOT'].'/admin/common/php-asmanager.php');
+	$astman         = new AGI_AsteriskManager();
+	if (! $res = $astman->connect("127.0.0.1", $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"])) {
+		exit;
+	}
+
+	$var = $astman->database_show('AMPUSER');
+	foreach ($var as $key => $value) {
+		$myvar = explode('/',trim($key,'/'));
+		$user_hash[$myvar[1]] = true;
+	}
+
+	foreach (array_keys($user_hash) as $user) {
+		if ($user != 'none' && $user != '') {
+			$devices = get_devices($user);
+			debug("Set hints for user: $user for devices:  ".$devices,5);
+			set_hint($user, $devices);
+		}
+	}
+
+	//---------------------------------------------------------------------
+
+	function parse_amportal_conf_bootstrap($filename) {
+		$file = file($filename);
+		foreach ($file as $line) {
+			if (preg_match("/^\s*([\w]+)\s*=\s*\"?([\w\/\:\.\*\%-]*)\"?\s*([;#].*)?/",$line,$matches)) {
+				$conf[ $matches[1] ] = $matches[2];
+			}
+		}
+		if ( !isset($conf["AMPWEBROOT"]) || ($conf["AMPWEBROOT"] == "")) {
+			$conf["AMPWEBROOT"] = "/var/www/html";
+		} else {
+			$conf["AMPWEBROOT"] = rtrim($conf["AMPWEBROOT"],'/');
+		}
+		if (!isset($conf['ASTAGIDIR']) || $conf['ASTAGIDIR'] == '') {
+			$conf['ASTAGIDIR'] = '/var/lib/asterisk/agi-bin';
+		}
+
+		return $conf;
+	}
+
+	// Set the hint for a user based on the devices in their AMPUSER object
+	//
+	function set_hint($user, $devices) {
+		debug("set_hint: user: $user, devices: $devices",8);
+		global $astman;
+
+		if ($devices) {
+			$dial_string = get_dial_string($devices);
+			debug("Setting user $user hint to $dial_string",5);
+			$response = $astman->send_request('Command',array('Command'=>"add extension {$user},hint,{$dial_string} into ext-local replace"));
+		} else {
+			debug("Removing hint for user $user",5);
+			$response = $astman->send_request('Command',array('Command'=>"remove extension {$user}@ext-local hint"));
+		}
+	}
+
+	// Get the actual technology dialstrings from the DEVICE objects (used
+	// to create proper hints)
+	//
+	function get_dial_string($devices) {
+		debug("get_dial_string: devices: $devices",8);
+		global $astman;
+
+		$device_array = explode( '&', $devices );
+		foreach ($device_array as $adevice) {
+			$dds = $astman->database_get('DEVICE',$adevice.'/dial');
+			$dialstring .= $dds.'&';
+		}
+		return trim($dialstring," &");
+	}
+
+	// Get the list of current devices for this user
+	//
+	function get_devices($user) {
+		debug("get_devices: user: $user", 8);
+		global $astman;
+
+		$devices = $astman->database_get('AMPUSER',$user.'/device');
+		return trim($devices);
+	}
+
+	function debug($string, $level=3) {
+		global $debug;
+		if ($debug >= $level) {
+			echo $string."\n";
+		}
+	}
