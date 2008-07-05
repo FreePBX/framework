2e1194371bf1f1e982573b6c728d2438d72be73f
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2008-07-05 19:34:39 +0000

    #1715 need to also create the new global vars

---
+++ upgrades/2.5.0alpha1/tables.php
@@ -0,0 +1,30 @@
+<?php
+
+outn("Checking for Global var MIXMON_FORMAT..");
+$nrows = $db->getOne("SELECT count(*) from globals where variable='MIXMON_FORMAT'");
+if (!$nrows) {
+	$db->query("insert into globals values ('MIXMON_FORMAT', 'wav')");
+	out("Created");
+} else {
+	out("Already exists!");
+}
+
+outn("Checking for Global var MIXMON_DIR..");
+$nrows = $db->getOne("SELECT count(*) from globals where variable='MIXMON_DIR'");
+if (!$nrows) {
+	$db->query("insert into globals values ('MIXMON_DIR', '1')");
+	out("Created");
+} else {
+	out("Already exists!");
+}
+
+outn("Checking for Global var MIXMON_POST..");
+$nrows = $db->getOne("SELECT count(*) from globals where variable='MIXMON_POST'");
+if (!$nrows) {
+	$db->query("insert into globals values ('MIXMON_POST', '')");
+	out("Created");
+} else {
+	out("Already exists!");
+}
+
+?>
