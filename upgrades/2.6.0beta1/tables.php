8afb067f2102f483f32728da207ed9008fbdf64b
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2009-05-20 23:44:46 +0000

    changing from sha256 to sha1 because php4 does not support hash() function re #1380

--- upgrades/2.6.0beta1/tables.php
+++ upgrades/2.6.0beta1/tables.php
@@ -3,30 +3,26 @@
 function encrypt_passwords()
 {
 	global $db;
-	$pwds = 0;
 	out("Updating passwords..");
 	$sql = "SELECT * FROM ampusers";
 	$users = $db->getAll($sql,NULL,DB_FETCHMODE_ASSOC);
-        if (DB::IsError($users)) { // Error while getting the users list to update... bad
+	if (DB::IsError($users)) { // Error while getting the users list to update... bad
 		die($users->getMessage());
 	} else {
 		outn("(".count($users)." accounts) ");	
-		foreach ($users as $index => $ufields)
-		{
-			$sql = "UPDATE ampusers SET password_sha256='".hash("sha256",$ufields['password'])."' WHERE username='".$ufields['username']."'";
+		foreach ($users as $index => $ufields) {
+			$sql = "UPDATE ampusers SET password_sha1='".sha1($ufields['password'])."' WHERE username='".$ufields['username']."'";
 			$result = $db->query($sql);
 			if (DB::IsError($result)) {
 				outn("Error while updating account: ".$ufields['username']." (".$result->getMessage.")");
-			} else {
-				$pwds++;
 			}	
 		}
 	}
 	out("Done.");
 }
 
-outn("Checking for sha256 passwords...");
-$sql = "SELECT password_sha256 FROM ampusers";
+outn("Checking for sha1 passwords..");
+$sql = "SELECT password_sha1 FROM ampusers";
 $passfield = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 if (!DB::IsError($passfield)) { // no error... Already done
 	$sql = "SELECT password FROM ampusers";
@@ -39,7 +35,7 @@ if (!DB::IsError($passfield)) { // no error... Already done
 } else {
 	if ($passfield->code == DB_ERROR_NOSUCHFIELD) {
 		outn("Updating database..");
-		$sql = "ALTER TABLE ampusers ADD password_sha256 VARCHAR ( 64 ) NOT NULL AFTER password";
+		$sql = "ALTER TABLE ampusers ADD password_sha1 VARCHAR ( 40 ) NOT NULL AFTER password";
 		$results = $db->query($sql);
 		if (DB::IsError($results)) {
 			die($results->getMessage());
@@ -50,8 +46,8 @@ if (!DB::IsError($passfield)) { // no error... Already done
 			$sql = "ALTER TABLE ampusers DROP password";
 			$results = $db->query($sql);
 			if (DB::IsError($results)) {
-        	                die($results->getMessage());
-	                } else {
+				die($results->getMessage());
+			} else {
 				out("Done.");
 			}
 		}
