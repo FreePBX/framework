4e29b0f356f740127044ae06f9ac69362a17b3cb
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-01-13 20:31:48 +0000

    revert r8560 done in error, should have been a merge from 2.6 not 2.5

--- amp_conf/agi-bin/phpagi.php
+++ amp_conf/agi-bin/phpagi.php
@@ -212,10 +212,8 @@
         if($this->audio) stream_set_blocking($this->audio, 0);
       }
 
-// Next two lines where removed 2009-08-25 due to unneccesary output when setting "core set verbose 3" or higher in Asterisk.
-//
-//    $this->conlog('PHPAGI internal configuration:');
-//    $this->conlog(print_r($this->config, true));
+      $this->conlog('PHPAGI internal configuration:');
+      $this->conlog(print_r($this->config, true));
 
 // Enable for debuggin purposes
 //foreach(explode("\n", print_r($this, true)) as $line) syslog(LOG_WARNING, $line);
