9afc272b677db89a96eb607b748b6e06eeb76028
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-02-28 06:52:36 +0000

    setup trunk as 2.8 alpha

--- setup_svn.php
+++ setup_svn.php
@@ -34,7 +34,7 @@
  *  modules sym-linked to their final place to make development easier.
  *
  */
-$VERSION = "2.7";
+$VERSION = "2.8";
 
 $NORMAL_URL =  "http://svn.freepbx.org/freepbx/branches/$VERSION/amp_conf/htdocs/admin/modules";
 $MODULE_URL  = "http://svn.freepbx.org/modules/branches/$VERSION";
