4dc61f31ddb6864aa449bcec5e8cb85333f630cb
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-03-14 15:53:42 +0000

    update manager.conf with required permissions if not already done

--- upgrades/2.8.0alpha1/tables.php
+++ upgrades/2.8.0alpha1/tables.php
@@ -2,6 +2,12 @@
 global $amp_conf;
 global $db;
 
+/*  fix manager.conf settings for older manager.conf files being upgraded as new permissions are needed for later releases of Asterisk
+ *  in english, this is limited to everything between the AMPMGRUSER section and any new section the user may have edited. It replaces
+ *  everything to the right of a 'read =' or 'write =' permission line with the full set of permissoins Asterisk offers.
+ */
+exec('sed -i.2.8.0.bak "/^\['.$amp_conf['AMPMGRUSER'].'\]/,/^\[.*\]/s/^\(\s*read\s*=\|\s*write\s*=\).*/\1 system,call,log,verbose,command,agent,user,config,command,dtmf,reporting,cdr,dialplan,originate/" '.$amp_conf['ASTETCDIR'].'/manager.conf',$outarr,$ret);
+
 $autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
 
 $outbound_routes = "
