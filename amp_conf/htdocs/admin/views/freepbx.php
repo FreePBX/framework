5c09e6d59ca504b99fc41d4f32b4c1fa922bab1b
Moshe Brevda <mbrevda@gmail.com>
2010-03-16 10:07:36 +0000

    re #1798; always load jquery library before other js

--- amp_conf/htdocs/admin/views/freepbx.php
+++ amp_conf/htdocs/admin/views/freepbx.php
@@ -107,8 +107,8 @@ if (!isset($amp_conf)) {
 <?php
 	} else {
 ?>
-	<script type="text/javascript" src="common/script.legacy.js"></script> <!-- legacy script.js.php -->
 	<script type="text/javascript" src="common/jquery-1.3.2.js"></script>
+	<script type="text/javascript" src="common/script.legacy.js"></script> <!-- legacy script.js.php -->
 	<script type="text/javascript" src="common/jquery.tabs-2.7.4.js"></script> <!-- used by setup/tools menu tab -->
 	<script type="text/javascript" src="common/jquery.dimensions.js"></script> <!-- used by reload/module admin -->
 	<script type="text/javascript" src="common/jquery.cookie.js"></script>
