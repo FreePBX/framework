4b0df586def9047ba784529821661a10f380c47c
Moshe Brevda <mbrevda@gmail.com>
2010-04-13 08:43:32 +0000

    re #4207; rebuild libfreepbx. Also, update build script to ensure proper file ordering

--- buildtools/pack_javascripts.php
+++ buildtools/pack_javascripts.php
@@ -6,16 +6,34 @@ $dir="../amp_conf/htdocs/admin/common";
 $output=array();
 
 exec("ls $dir/*.js",$output,$ret);
-$final = array();
+$final=$finalB=array();
+/*
+ * to order js files: files will be appened to the array in the order they appear
+ * in the switch statmenet below. To give a file priority, creae a case for it and
+ * add it to the $finalB array. All other files will be appended to the $final array.
+ * $finalB is then merged with $final, with $finalB being put first
+ */  
 foreach ($output as $file) {
-  if (preg_match("|$dir/jquery-\d+\.\d+\.\d+\.js|",$file,$matches)) {
-    array_unshift($final,$dir.'/script.legacy.js');
-    array_unshift($final,$file);
-  } else if ($file != $dir.'/'.$libfreepbx && $file != $dir.'/script.legacy.js') {
-    $final[] = $file;
-  }
+	switch(true){
+		case preg_match("|$dir/jquery-\d+\.\d+\.\d+\.js|",$file)://jquery
+			 $finalB[] = $file;
+		break;
+		case preg_match("|$dir/jquery-ui-.*\.js$|",$file)://jquery ui
+			$finalB[] = $file;
+		break;
+		case $file==$dir.'/script.legacy.js'://legacy script
+			$finalB[] = $file;
+		break;
+		case $file != $dir.'/'.$libfreepbx && $file != $dir.'/script.legacy.js'://default
+			$final[] = $file;
+		break;
+	}
 }
+
+$final=array_merge($finalB,$final);
+
 echo "creating $libfreepbx with:\n\n";
+print_r($final);
 echo 'cat '.implode(' ',$final)." | ./jsmin.rb >  $dir/$libfreepbx\n\n";
 
 system('cat '.implode(' ',$final)." | ./jsmin.rb >  $dir/$libfreepbx");
