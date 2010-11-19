85d5d11f01a1bd4c0cf8d863cd260d189ed25ba2
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-11-19 21:33:24 +0000

    closes #4644 make database mode authentication the defaut if not chnanged

--- amp_conf/htdocs/admin/libraries/framework_view.functions.php
+++ amp_conf/htdocs/admin/libraries/framework_view.functions.php
@@ -12,7 +12,40 @@ if (isset($_REQUEST['logout'])) {
 }
 
 switch (strtolower($amp_conf['AUTHTYPE'])) {
+	case 'webserver':
+		// handler for apache doing authentication
+		if ((!isset($_SESSION['AMP_user']) || ($_SESSION['AMP_user']->username != $_SERVER['PHP_AUTH_USER'])) && !isset($_REQUEST['logout'])) {
+			// not logged in, or username has changed;  and not trying to log out
+			
+			if (isset($_SESSION['logout']) && $_SESSION['logout']) {
+				// workaround for HTTP-auth - just tried to logout, don't allow a log in (with the same credentials)
+				unset($_SESSION['logout']);
+				// afterwards, this falls through to the !AMP_user check below, and sends 401 header, which causes the browser to re-prompt the user
+			} else {
+				$_SESSION['AMP_user'] = new ampuser($_SERVER['PHP_AUTH_USER']);
+				
+				if ($_SESSION['AMP_user']->username == $amp_conf['AMPDBUSER']) {
+					// admin user, grant full access
+					$_SESSION['AMP_user']->setAdmin();
+				}
+			}
+		}
+
+		if (!isset($_SESSION['AMP_user'])) {
+			// not logged in, send headers
+			@header('WWW-Authenticate: Basic realm="FreePBX '._('Administration').'"');
+			@header('HTTP/1.0 401 Unauthorized');
+			showview("unauthorized");
+			exit;
+		}
+	case 'none':
+		if (!isset($_SESSION['AMP_user'])) {
+			$_SESSION['AMP_user'] = new ampuser($amp_conf['AMPDBUSER']);
+			$_SESSION['AMP_user']->setAdmin();
+		}
+	break;
 	case 'database':
+	default:
 		if (!isset($_SESSION['AMP_user']) && isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']) && !isset($_REQUEST['logout'])) {
 			if (isset($_SESSION['logout']) && $_SESSION['logout']) {
 				// workaround for HTTP-auth - just tried to logout, don't allow a log in (with the same credentials)
@@ -44,39 +77,6 @@ switch (strtolower($amp_conf['AUTHTYPE'])) {
 			@header('HTTP/1.0 401 Unauthorized');
 			showview("unauthorized");
 			exit;
-		}
-	break;
-	case 'webserver':
-		// handler for apache doing authentication
-		if ((!isset($_SESSION['AMP_user']) || ($_SESSION['AMP_user']->username != $_SERVER['PHP_AUTH_USER'])) && !isset($_REQUEST['logout'])) {
-			// not logged in, or username has changed;  and not trying to log out
-			
-			if (isset($_SESSION['logout']) && $_SESSION['logout']) {
-				// workaround for HTTP-auth - just tried to logout, don't allow a log in (with the same credentials)
-				unset($_SESSION['logout']);
-				// afterwards, this falls through to the !AMP_user check below, and sends 401 header, which causes the browser to re-prompt the user
-			} else {
-				$_SESSION['AMP_user'] = new ampuser($_SERVER['PHP_AUTH_USER']);
-				
-				if ($_SESSION['AMP_user']->username == $amp_conf['AMPDBUSER']) {
-					// admin user, grant full access
-					$_SESSION['AMP_user']->setAdmin();
-				}
-			}
-		}
-
-		if (!isset($_SESSION['AMP_user'])) {
-			// not logged in, send headers
-			@header('WWW-Authenticate: Basic realm="FreePBX '._('Administration').'"');
-			@header('HTTP/1.0 401 Unauthorized');
-			showview("unauthorized");
-			exit;
-		}
-	case 'none':
-	default:
-		if (!isset($_SESSION['AMP_user'])) {
-			$_SESSION['AMP_user'] = new ampuser($amp_conf['AMPDBUSER']);
-			$_SESSION['AMP_user']->setAdmin();
 		}
 	break;
 }
