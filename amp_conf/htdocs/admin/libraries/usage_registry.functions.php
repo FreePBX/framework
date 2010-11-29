0503944cfb99fde5b95f39cca0130f250f31e91f
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-11-29 08:03:40 +0000

    revert r10623 to usage_registry.functions.php checked in error re #4667

--- amp_conf/htdocs/admin/libraries/usage_registry.functions.php
+++ amp_conf/htdocs/admin/libraries/usage_registry.functions.php
@@ -61,36 +61,6 @@ function framework_check_extension_usage($exten=true, $module_hash=false) {
 }
 
 /** check if a specific destination is being used, or get a list of all destinations that are being used
- * @param string    the old destination that is being changed
- * @param string    the new destination that is replacing the old
- * @param array     a hash of module names to search for callbacks, otherwise global $active_modules is used
- * @return integer  returns the number of records that were updated
- * @description     has each module replace their destination information with another one, used if you are
- *                  assigning a new number to something such as a conference room that may be used as a destination
- *                                               
- */
-function framework_change_destination($old_dest, $new_dest, $module_hash=false) {
-  global $db;
-	global $active_modules;
-
-  $old_dest = $db->escapeSimple($old_dest);
-  $new_dest = $db->escapeSimple($new_dest);
-
-	if (!is_array($module_hash)) {
-		$module_hash = $active_modules;
-	}
-
-  $total_updated = 0;
-	foreach(array_keys($module_hash) as $mod) {
-		$function = $mod."_change_destination";
-		if (function_exists($function)) {
-      $total_updated += $function($dest);
-		}
-	}
-	return $total_update;
-}
-
-/** check if a specific destination is being used, or get a list of all destinations that are being used
  * @param mixed     an array of destinations to check against, or if boolean true then return list of all destinations in use
  * @param array     a hash of module names to search for callbacks, otherwise global $active_modules is used
  * @return array    returns an empty array if destination not in use, or any array with usage info, or of all usage 
