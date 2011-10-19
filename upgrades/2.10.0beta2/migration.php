4846efb365b3d0909437b46ec1e51da237a9edb1
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-10-19 17:36:45 +0000

    migration should remove links as well if installed with --dev-links

--- upgrades/2.10.0beta2/migration.php
+++ upgrades/2.10.0beta2/migration.php
@@ -53,7 +53,7 @@ out("Cleaning up deprecated or moved files:");
 
 foreach ($outdated as $file) {
 	outn("Checking $file..");
-	if (file_exists($file) && !is_link($file)) {
+	if (file_exists($file) || is_link($file)) {
 		unlink($file) ? out("removed") : out("failed to remove");
 	} else {
 		out("Not Required");
