907785a0b867b10cfbaa33dfb02c08daf46181fc
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2012-02-28 00:23:12 +0000

    fixes #5628 we think don't mutilate channel variables passed in

--- amp_conf/htdocs/admin/libraries/extensions.class.php
+++ amp_conf/htdocs/admin/libraries/extensions.class.php
@@ -1024,12 +1024,37 @@ class ext_meetme {
 		switch ($amp_conf['ASTCONFAPP']) {
 			case 'app_confbridge':
 				$this->app = 'ConfBridge';
+			
 				//remove invalid options
-				$this->options = str_replace(array('b', 'C', 'd', 'D', 
-													'e', 'E', 'F', 'i', 
-													'I', 'l', 'o', 'P', 
-													'r', 's', 't', 'T', 
-													'x', 'X'), '', $this->options);
+				$meetme_only = array('b', 'C', 'd', 'D', 
+									'e', 'E', 'F', 'i', 
+									'I', 'l', 'o', 'P', 
+									'r', 's', 't', 'T', 
+									'x', 'X');
+									
+				//find asterisk variables in $this->options, if any
+				if (preg_match_all('/\$|}/', $this->options, $matches, PREG_OFFSET_CAPTURE)) {
+					$matches = $matches[0];
+					//build a range of start and endpoints of any asterisk variables
+					for($i = 0; $i < count($matches); $i += 2) {
+						if ($matches[$i][0] == '$') {
+							$range[] = array( $matches[$i][1], $matches[$i + 1][1]);
+						}
+					}
+					
+					//loop through each charachter in $this->options. If its not in the
+					//range of asterisk variables $range, replace its charachter it its in $meetme_only
+					$str_array = str_split($this->options);
+					for ($i = 0; $i < count($str_array); $i++) {
+						if (!$this->in_ast_var_range($i, $range)) {
+							$str_array[$i] = str_replace($meetme_only, '', $stra[$i]);
+						}
+					}
+					$this->options = implode($str_array);
+				} else {
+					$this->options = str_replace($meetme_only, '', $this->options);
+				}
+				
 				$this->options = preg_replace('/[GpSL]\(.*\)/', '', $this->options);
 				$this->options = preg_replace('/w\(.*\)/', 'w', $this->options);
 				break;
@@ -1042,6 +1067,26 @@ class ext_meetme {
 
 	function output() {
 		return $this->app . "(".$this->confno.",".$this->options.",".$this->pin.")";
+	}
+	
+	/**
+	 * @pram int
+	 * @pram array - multi dimensional with ranges
+	 * i.e. array(
+	 *	array(4 => 6),
+	 *  array(8 => 9)
+	 * )
+	 *
+	 * @return true if $pos is not in range
+	 */
+	function in_ast_var_range($pos, $range) {
+		foreach ($range as $r) {
+			if ($pos >= $r[0] && $pos <= $r[1]) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
 
