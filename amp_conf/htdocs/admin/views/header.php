351d7980ef2fc7dcc5490cad1466248676d2e161
Moshe Brevda <mbrevda@gmail.com>
2013-01-28 16:04:11 +0000

    remove duplicate header; adjust to #page_body to allow child floats to grow it. This may have unforeseen repercussions, keep your eyes open for view related side effects

--- amp_conf/htdocs/admin/views/header.php
+++ amp_conf/htdocs/admin/views/header.php
@@ -18,7 +18,6 @@ $html .= '<title>'
 		. '</title>';
 
 $html .= '<meta http-equiv="Content-Type" content="text/html;charset=utf-8">'
-		. '<meta http-equiv="Content-Type" content="text/html;charset=utf-8">'
 		. '<meta http-equiv="X-UA-Compatible" content="chrome=1">'
 		. '<meta name="robots" content="noindex" />'
 		. '<link rel="shortcut icon" href="images/favicon.ico">';
