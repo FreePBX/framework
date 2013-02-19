9a57b1967b68c5af67bd8f34c073c6a2f3561a89
Andrew Nagy <andrew.nagy@the159.com>
2013-02-19 23:18:53 +0000

    Fixed recording link

--- amp_conf/htdocs/admin/views/login.php
+++ amp_conf/htdocs/admin/views/login.php
@@ -24,7 +24,7 @@ $html .= form_close();
 $html .= '</div>';
 $html .= '<div id="login_icon_holder">';
 $html .= '<a href="#" class="login_item" id="login_admin"/>&nbsp</a>';
-$html .= '<a href="recordings/" '
+$html .= '<a href="/recordings" '
 		. 'class="login_item" id="login_ari"/>&nbsp</a>';
 if ($panel) {
     $html .= '<a href="' . $panel . '" '
