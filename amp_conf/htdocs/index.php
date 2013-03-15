ffd6c39a962c7af7e50f1ebabd1f4d3ac7fcee9d
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2013-03-15 22:45:46 +0000

    change the index_custom.php redirect so it stays as index.php in the browser

--- amp_conf/htdocs/index.php
+++ amp_conf/htdocs/index.php
@@ -1,5 +1,6 @@
 <?php
 if (file_exists(dirname(__FILE__) . 'index_custom.php')) {
-	header('Location: index_custom.php');
+	  include_once(dirname(__FILE__) . 'index_custom.php');
+} else {
+	  header('Location: /admin');
 }
-header('Location: /admin');
