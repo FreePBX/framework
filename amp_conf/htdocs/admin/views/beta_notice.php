1e3c684c7b37d969734883b73ed4060b221ccf1f
Bryan Walters <secretop@gmail.com>
2013-04-30 14:08:59 +0000

    update wording from This Module Page to This page

--- amp_conf/htdocs/admin/views/beta_notice.php
+++ amp_conf/htdocs/admin/views/beta_notice.php
@@ -5,7 +5,7 @@
  *       pages can be beta vs. the whole module.
  */
 $html = '<div id="menuitem_beta">';
-$html .= _("-This Module Page is currently BETA-");
+$html .= _("-This page is currently BETA-");
 $html .= '</div>';
 
 echo $html;
