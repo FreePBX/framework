1addbf1b77254fc8c23cf36f28357c2680ecc328
Andrew Nagy <andrew.nagy@the159.com>
2013-07-26 18:01:04 +0000

    Fixes #6656

--- amp_conf/htdocs/admin/assets/js/script.legacy.js
+++ amp_conf/htdocs/admin/assets/js/script.legacy.js
@@ -438,14 +438,15 @@ function isInside(s, c)
 // ** Check if valid email address                  **
 // ***************************************************
 function isEmail (s) {
-  if (isEmpty(s)) {
+	if (isEmpty(s)) {
 		if (isEmail.arguments.length == 1) {
 			return defaultEmptyOK;
 		} else {
 			return (isEmail.arguments[1] == true)
 		}
 	}
-	var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+	var pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 
 	return pattern.test(s)
 }
