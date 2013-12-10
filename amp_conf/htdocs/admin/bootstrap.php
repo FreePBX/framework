5bc76b30a196f23e8c8fdcddf604f0b7e572c731
Andrew Nagy <andrew.nagy@the159.com>
2013-12-10 04:18:12 +0100

    FREEPBX-6484 added separate css compress class to help prevent errors of css compression in the future

--- amp_conf/htdocs/admin/bootstrap.php
+++ amp_conf/htdocs/admin/bootstrap.php
@@ -79,6 +79,7 @@ $restrict_mods = isset($restrict_mods) ? $restrict_mods : false;
 
 	 	 
 // include base functions
+require_once($dirname . '/libraries/compress.class.php');
 require_once($dirname . '/libraries/utility.functions.php'); 
 $bootstrap_settings['framework_functions_included'] = false; 
 require_once($dirname . '/functions.inc.php'); 
