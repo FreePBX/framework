d0c4bac8f3c1c9d42920a5ed4f693685cac92c7f
Andrew Nagy <andrew.nagy@the159.com>
2013-12-16 19:51:53 +0100

    Add font awesome fonts for bootstrap layer

--- libfreepbx.install.php
+++ libfreepbx.install.php
@@ -354,10 +354,10 @@ function recursive_copy($dirsourceparent, $dirdest, &$md5sums, $dirsource = "") 
 					$overwrite = true;
 				}
 
-        // These are modified by apply_conf.sh, there may be others that fit in this category also. This keeps these from
-        // being symlinked and then developers inadvertently checking in the changes when they should not have.
-        //
-        $never_symlink = array("cdr_mysql.conf", "manager.conf", "vm_email.inc");
+				// These are modified by apply_conf.sh, there may be others that fit in this category also. This keeps these from
+				// being symlinked and then developers inadvertently checking in the changes when they should not have.
+				//
+				$never_symlink = array("cdr_mysql.conf", "manager.conf", "vm_email.inc");
 
 				$num_files++;
 				if ($overwrite) {
