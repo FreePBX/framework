b378fde11938eab523985410ea7bc27b385d18d8
Rob Thomas <xrobau@gmail.com>
2013-12-28 09:37:05 +1000

    More work on giving some intelligence to Hooks

--- amp_conf/htdocs/admin/BMO/Hooks.class.php
+++ amp_conf/htdocs/admin/BMO/Hooks.class.php
@@ -3,6 +3,8 @@
 
 class Hooks {
 
+	private $hooks;
+
 	public function __construct($freepbx = null) {
 		if ($freepbx == null)
 			throw new Exception("Need to be instantiated with a FreePBX Object");
@@ -10,8 +12,17 @@ class Hooks {
 		$this->FreePBX = $freepbx;
 	}
 
-	public function getBMOHooks() {
-		// Find all BMO Modules, query them for GUI and Dialplan hooks.
+	public function getAllHooks() {
+		// TODO: Cache this. The only time 'updateBMOHooks' should be run
+		// is in retrieve_conf.
+		if (!isset($this->hooks))
+			$this->updateBMOHooks();
+
+		return $this->hooks;
+	}
+
+	public function updateBMOHooks() {
+		// Find all BMO Modules, query them for GUI, Dialplan, and configpageinit hooks.
 		
 		$this->preloadBMOModules();
 		$classes = get_declared_classes();
@@ -20,9 +31,38 @@ class Hooks {
 		foreach ($classes as $class) {
 			$implements = class_implements($class);
 			if (isset($implements['BMO']))
-				$retarr[] = $class;
+				$bmomodules[] = $class;
 		}
-		return $retarr;
+
+		$allhooks = array();
+
+		foreach ($bmomodules as $mod) {
+			// Find GUI Hooks
+			if (method_exists($mod, "myGuiHooks")) {
+				$allhooks['GuiHooks'][$mod] = $mod::myGuiHooks();
+			}
+
+			// Find Dialplan hooks (eg, called when retrieve_conf is run)
+			if (method_exists($mod, "myDialplanHooks")) {
+				$allhooks['DialplanHooks'][$mod] = $mod::myDialplanHooks();
+			}
+
+			// Find ConfigPageInit hooks (called before the page is displayed,
+			// and often used to catch 'submit' POST/GETs, or as an alternative
+			// to guihooks.
+			if (method_exists($mod, "myConfigPageInit")) {
+				$allhooks['ConfigPageInits'][$mod] = $mod::myConfigPageInit();
+			}
+
+			// Discover if the module wants to write to any other files, which
+			// is done with getConfig/writeConfig
+			if (method_exists($mod, "writeConfig")) {
+				$allhooks['ConfigFiles'][] = $mod;
+			}
+		}
+
+		$this->hooks = $allhooks;
+		return $allhooks;
 	}
 
 	private function preloadBMOModules() {
