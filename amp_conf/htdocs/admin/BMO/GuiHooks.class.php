e3430f53876d718db5e993f8d712fd7d978fe5ba
Rob Thomas <xrobau@gmail.com>
2013-12-29 13:56:50 +1000

    Configpageinit sorta kinda works.

--- amp_conf/htdocs/admin/BMO/GuiHooks.class.php
+++ amp_conf/htdocs/admin/BMO/GuiHooks.class.php
@@ -46,6 +46,8 @@ class GuiHooks {
 					$retarr['hooks'][] = $module;
 			}
 		}
+
+		$retarr['oldhooks'] = $this->getOldConfigPageInit();
 		return $retarr;
 	}
 
@@ -113,4 +115,96 @@ class GuiHooks {
 		return $output;
 	}
 
+	private function getOldConfigPageInit($onlymodule = null) {
+
+		$active_modules = $this->FreePBX->Modules->active_modules;
+
+		foreach($active_modules as $key => $module) {
+			// If we've been handed a modulename, only return the
+			// hooks for that specific module.
+			if ($onlymodule == null || $onlymodule == $key) {
+				// Does this module have a _configpageinit function?
+				$initfuncname = $key . '_configpageinit';
+				if (function_exists($initfuncname) ) {
+					$configpageinits[] = $initfuncname;
+				}
+
+				// Does the module have multiple items?
+				if (is_array($module['items'])) {
+					foreach($module['items'] as $itemKey => $itemName) {
+						// Each item may have a configpageinit, too.
+						$initfuncname = $key . '_' . $itemKey . '_configpageinit';
+						if (function_exists($initfuncname)) {
+							$configpageinits[] = $initfuncname;
+						}
+					}
+				}
+			}
+		}
+
+		if (isset($configpageinits))
+			return $configpageinits;
+
+		// None? This will be awesome, because it means all the old stuff 
+		// has been rewritten. However, until then, we'll throw an exception
+		// here because that SHOULDN'T BE HAPPENING. 
+		//
+		// Rob will buy you a beer if you get to remove these lines because it's
+		// legitimately being triggered.
+		if ($onlymodule == null)
+			throw new Exception("No configpageinit's found. This is amazingly unlikely");
+
+		return array();
+	}
+
+	public function doConfigPageInits($display = null) {
+		if ($display == null)
+			throw new Exception("Hooking into the main page is currently not supported. Sorry");
+
+		$bmoHooks = $this->FreePBX->Hooks->getAllHooks();
+
+		if (isset($bmoHooks['ConfigPageInits'])) {
+			$class = $this->FreePBX->Modules->getClassName($display);
+			$myHooks = $bmoHooks['ConfigPageInits'];
+		} else {
+			$myHooks = array();
+			$class = false;
+		}
+
+		$myOldHooks = $this->getOldConfigPageInit();
+
+		// Before we run any others, we want to make sure that THIS MODULE'S hooks are run first.
+		// Firstly, do the OLD Hooks.
+		$preOldHooks = $this->getOldConfigPageInit($display);
+		foreach ($preOldHooks as $hook) {
+			// Remove the hook from the ones we're going to run later
+			unset($myOldHooks[$hook]);
+			// Run it.
+			$hook($display);
+		}
+
+		// New style module? Here, have your data..
+		if ($class) {
+			$this->doBMOConfigPage($class, $display);
+			unset($myHooks[$class]);
+		}
+
+		// Now we've run the modules own stuff, now we can hand the request off to any other
+		// modules that want it.
+
+		// Firstly, old style hooks
+		foreach ($myOldHooks as $hook) {
+			$hook($display);
+		}
+
+		// And now the new-style module hooks.
+		foreach ($myHooks as $mod => $arr) {
+			if (in_array($display, $arr))
+				$this->doBMOConfigPage($mod, $display);
+		}
+	}
+
+	private function doBMOConfigPage($class, $display) {
+		$this->FreePBX->$class->doConfigPageInit($display);
+	}
 }
