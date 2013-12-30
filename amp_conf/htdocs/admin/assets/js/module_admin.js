10a717a75470ed4985ff06c46646d00478807dc8
Andrew Nagy <andrew.nagy@the159.com>
2013-12-29 18:08:52 -0800

    Cleanup javascripts, add XMLHttpRequest pollyfill

--- amp_conf/htdocs/admin/assets/js/module_admin.js
+++ amp_conf/htdocs/admin/assets/js/module_admin.js
@@ -217,29 +217,7 @@ function showhide_upgrades() {
 	}
 
 }
-if ('window' in this && 'document' in this) {
-	//----------------------------------------------------------------------
-	//
-	// Web Standards Polyfills
-	//
-	//----------------------------------------------------------------------
 
-	//
-	// XMLHttpRequest (http://www.w3.org/TR/XMLHttpRequest/)
-	//
-	window.XMLHttpRequest = window.XMLHttpRequest || function () {
-		/*global ActiveXObject*/
-		try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch (e1) { }
-		try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch (e2) { }
-		try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e3) { }
-		throw new Error("This browser does not support XMLHttpRequest.");
-	};
-	XMLHttpRequest.UNSENT = 0;
-	XMLHttpRequest.OPENED = 1;
-	XMLHttpRequest.HEADERS_RECEIVED = 2;
-	XMLHttpRequest.LOADING = 3;
-	XMLHttpRequest.DONE = 4;
-}
 var box;
 function process_module_actions(modules) {
 	var urlStr = '';
