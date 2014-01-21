7f5fd2d59b721740cff3bcb975ab7edf119c2850
Bryan Walters <secretop@gmail.com>
2014-01-21 17:54:14 -0500

    FREEPBX-7086 #qa fix issues with module_runscript_includes and add the includes before calling the module_install_check_callback as modules being installed may want to perform the same check

--- amp_conf/htdocs/admin/libraries/module.functions.php
+++ amp_conf/htdocs/admin/libraries/module.functions.php
@@ -1144,6 +1144,12 @@ function module_install($modulename, $force = false) {
 	// indicating why the installation must fail
 	//
 	$rejects = array();
+
+	//We need to include developer files before the callback happens during an install
+	if (!_module_runscripts_include($modules, 'install')) {
+		return array(_("Failed to run installation scripts"));
+	}
+
 	foreach (mod_func_iterator('module_install_check_callback', $modules) as $mod => $res) {
 		if ($res !== true) {
 			$rejects[] = $res;
@@ -1501,12 +1507,22 @@ function _modules_setversion($modname, $vers) {
  * @return boolean   If the action was successful, currently TRUE so we don't prevent the install
  */
 function _module_runscripts_include($modulexml, $type) {
+	global $amp_conf;
+
 	foreach($modulexml as $modulename => $items) {
+		$moduledir = $amp_conf["AMPWEBROOT"]."/admin/modules/".$modulename;
 		if (isset($items['fileinclude'][$type]) && !empty($items['fileinclude'][$type])) {
-			foreach($items['fileinclude'][$type] as $key => $filename) {
-				$ret = _modules_doinclude($moduledir.'/'.$filename, $modulename);
+			if(!is_array($items['fileinclude'][$type])) {
+				$ret = _modules_doinclude($moduledir.'/'.$items['fileinclude'][$type], $modulename);
 				if (!$ret) {
-					freepbx_log(FPBX_LOG_WARNING,sprintf(_("failed to include %s during %s of the %s module."),$filename,$type,$modulename));		
+					freepbx_log(FPBX_LOG_WARNING,sprintf(_("failed to include %s during %s of the %s module."),$items['fileinclude'][$type],$type,$modulename));
+				}
+			} else {
+				foreach($items['fileinclude'][$type] as $key => $filename) {
+					$ret = _modules_doinclude($moduledir.'/'.$filename, $modulename);
+					if (!$ret) {
+						freepbx_log(FPBX_LOG_WARNING,sprintf(_("failed to include %s during %s of the %s module."),$filename,$type,$modulename));
+					}
 				}
 			}
 		}
@@ -1784,7 +1800,6 @@ function _module_distro_id() {
   if (isset($pbx_type)) {
     return array('pbx_type' => $pbx_type, 'pbx_version' => $pbx_version);
   }
-  
 
   exec('lscpu 2>&1',$out,$ret);
   $cpu_arch = '';
