da7d7ab7dd425adc890e0f327ee285d5c3a01dfc
Rob Thomas <xrobau@gmail.com>
2014-04-15 06:44:20 +1000

    Default ajax connections to have access to astman.

--- amp_conf/htdocs/admin/ajax.php
+++ amp_conf/htdocs/admin/ajax.php
@@ -18,8 +18,12 @@ if (isset($_REQUEST['command'])) {
 	$command = "unset";
 }
 
-// No astman connection
-$bootstrap_settings['skip_astman'] = true;
+// I think we'll default to having astman connected,
+// it adds a REALLY minor startup penalty, and saves
+// work in the modules. Feel free to revisit later and
+// yell at me if you disagree.
+//
+// $bootstrap_settings['skip_astman'] = true;
 
 // No auth - we'll do that later.
 $bootstrap_settings['freepbx_auth'] = false;
