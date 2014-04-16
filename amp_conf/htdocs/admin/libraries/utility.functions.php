3892979bad6f6f28b51f4716c4c9d6087c565ef4
Rob Thomas <xrobau@gmail.com>
2014-04-17 06:19:45 +1000

    Add missing error code picked up in unit tests.

--- amp_conf/htdocs/admin/libraries/utility.functions.php
+++ amp_conf/htdocs/admin/libraries/utility.functions.php
@@ -318,6 +318,7 @@ function freepbx_error_handler($errno, $errstr, $errfile, $errline,  $errcontext
                 E_CORE_WARNING                  => 'CORE_WARNING',
                 E_COMPILE_ERROR                 => 'COMPILE_ERROR',
                 E_COMPILE_WARNING               => 'COMPILE_WARNING',
+		E_DEPRECATED			=> 'DEPRECATION_WARNING',
                 E_USER_ERROR                    => 'USER_ERROR',
                 E_USER_WARNING                  => 'USER_WARNING',
                 E_USER_NOTICE                   => 'USER_NOTICE',
