8325b1ecd53bb602c8b5f15b023791295fa6e45d
Andrew Nagy <andrew.nagy@the159.com>
2014-08-01 14:21:12 -0400

    be nicer about this

--- amp_conf/htdocs/admin/libraries/extensions.class.php
+++ amp_conf/htdocs/admin/libraries/extensions.class.php
@@ -1395,13 +1395,25 @@ class ext_stopplaytones extends extension {
 }
 class ext_zapbarge extends extension {
 	function output() {
-		$trace = debug_backtrace();
-		$function = $trace[1]['function'];
-		$line = $trace[1]['line'];
-		$file = $trace[1]['file'];
-		freepbx_log(FPBX_LOG_WARNING,'Depreciated Asterisk Function '.$function.' detected in '.$file.' on line '.$line);
-		$n = new ext_noop('test1');
-		return $n->output();
+		global $version;
+		if(version_compare($version,"12.5","<")) {
+			global $chan_dahdi;
+			if($chan_dahdi) {
+				$command = 'DAHDIBarge';
+			} else {
+				$command = 'ZapBarge';
+			}
+			return "$command(".$this->data.")";
+		} else {
+			$trace = debug_backtrace();
+			$function = $trace[1]['function'];
+			$line = $trace[1]['line'];
+			$file = $trace[1]['file'];
+			freepbx_log(FPBX_LOG_WARNING,'Depreciated Asterisk Function '.$function.' detected in '.$file.' on line '.$line);
+			$n = new ext_noop('Using zapbarge or DAHDibarge has been removed');
+			$n->data('Using zapbarge or DAHDibarge has been removed');
+			return $n->output();
+		}
 	}
 }
 class ext_sayalpha extends extension {
