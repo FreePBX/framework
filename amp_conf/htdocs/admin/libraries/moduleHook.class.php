fd5e40564a45a29e5a0bbae76c12cc506de33ee6
Rob Thomas <xrobau@gmail.com>
2014-09-14 05:37:30 +1000

    Whitespace changes only

--- amp_conf/htdocs/admin/libraries/moduleHook.class.php
+++ amp_conf/htdocs/admin/libraries/moduleHook.class.php
@@ -7,25 +7,25 @@ class moduleHook {
 	function install_hooks($viewing_itemid,$target_module,$target_menuid = '') {
 		global $active_modules;
 
-    /*  Loop though all active modules and find which ones have hooks.
-     *  Then process those hooks. Note we split this into two loops
-     *  because of #4057, if drawselects() is called from within a hook
-     *  it's interaction with the same $active_modules array renders the
-     *  foreach loop done after that module and execution ends.
-     */
-    $our_hooks = array();
+		/*  Loop though all active modules and find which ones have hooks.
+		 *  Then process those hooks. Note we split this into two loops
+		 *  because of #4057, if drawselects() is called from within a hook
+		 *  it's interaction with the same $active_modules array renders the
+		 *  foreach loop done after that module and execution ends.
+		 */
+		$our_hooks = array();
 		foreach($active_modules as $this_module) {
 			// look for requested hooks for $module
 			// ie: findme_hook_extensions()
 			$funct = $this_module['rawname'] . '_hook_' . $target_module;
 			if( function_exists( $funct ) ) {
-			  // remember who installed hooks
-			  // we need to know this for processing form vars
-			  $this->arrHooks[] = $this_module['rawname'];
-        $our_hooks[$this_module['rawname']] = $funct;
-      }
-    }
-    foreach($our_hooks as $thismod => $funct) {
+				// remember who installed hooks
+				// we need to know this for processing form vars
+				$this->arrHooks[] = $this_module['rawname'];
+				$our_hooks[$this_module['rawname']] = $funct;
+			}
+		}
+		foreach($our_hooks as $thismod => $funct) {
 			modgettext::push_textdomain($thismod);
 			if ($hookReturn = $funct($target_menuid, $viewing_itemid)) {
 				$this->hookHtml .= $hookReturn;
