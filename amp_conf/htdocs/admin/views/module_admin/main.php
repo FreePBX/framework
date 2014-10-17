2a33b6fd250a0b09314a5612b02d9d4c3f828f94
Bryan Walters <secretop@gmail.com>
2014-10-17 13:11:52 -0400

    Only show the Buy/Upgrade/or Free buttons if we can determine you don't already own it

--- amp_conf/htdocs/admin/views/module_admin/main.php
+++ amp_conf/htdocs/admin/views/module_admin/main.php
@@ -1,7 +1,5 @@
 <?php
 // vim: :set filetype=php tabstop=4 shiftwidth=4 autoindent smartindent:
-?>
-<?php
 if($online) { ?>
 	<?php if(!empty($announcements)) {?>
 		<div class='announcements'><?php echo $announcements?></div>
@@ -61,6 +59,37 @@ if($online) { ?>
 							<span class="modulepublisher"><?php echo $module['publisher']?></span>
 							<span class="modulelicense"><?php echo (!empty($module['licenselink'])) ? '<a href="'.$module['licenselink'].'" target="_moduleLicenseLink">'.$module['license'].'</a>' : $module['license']?></span>
 							<span class="modulestatus">
+									<?php if($module['commercial']['status']) {?>
+										<?php if (function_exists('sysadmin_is_module_licensed') && !sysadmin_is_module_licensed($module['name'])) { ?>
+											<?php if($module['commercial']['sysadmin'] || $module['name'] == 'sysadmin' && $module['status'] == MODULE_STATUS_ENABLED) {?>
+												<?php if(!$module['commercial']['licensed']) { ?>
+													<?php switch($module['commercial']['type']) {
+															case 'upgradeable':?>
+																<button class="fpbx-buy" data-rawname="<?php echo $module['name']?>">
+																	<i class="fa fa-money"></i><?php echo _("Upgrade")?>
+																</button>
+														<?php break;?>
+														<?php case 'free':?>
+															<span class="buy">
+																<button class="fpbx-buy" data-rawname="<?php echo $module['name']?>">
+																	<i class="fa fa-money"></i><?php echo _("Buy")?>
+																</button>
+															</span>
+														<?php break;?>
+														<?php case 'paid':?>
+															<span class="buy">
+																<button class="fpbx-buy" data-rawname="<?php echo $module['name']?>">
+																	<i class="fa fa-money"></i><?php echo _("Buy")?>
+																</button>
+															</span>
+														<?php break;?>
+														<?php default:?>
+														<?php break;?>
+													<?php } ?>
+												<?php } ?>
+											<?php } ?>
+										<?php } ?>
+									<?php } ?>
 									<?php switch ($module['status']) {
 										case MODULE_STATUS_NOTINSTALLED:
 											if (!empty($module['raw']['online'])) { ?>
