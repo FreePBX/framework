acdb791c7aa94fe673c7796262e75a7b0d25fd00
Bryan Walters <secretop@gmail.com>
2014-10-22 13:31:55 -0700

    FREEI-221 - use an anchor instead of a button

--- amp_conf/htdocs/admin/views/module_admin/main.php
+++ amp_conf/htdocs/admin/views/module_admin/main.php
@@ -64,28 +64,20 @@ if($online) { ?>
 											<?php if($module['commercial']['sysadmin'] || $module['name'] == 'sysadmin' && $module['status'] == MODULE_STATUS_ENABLED) {?>
 												<?php if(!$module['commercial']['licensed']) { ?>
 													<?php switch($module['commercial']['type']) {
-															case 'upgradeable':?>
-																<button class="fpbx-buy" data-rawname="<?php echo $module['name']?>">
-																	<i class="fa fa-money"></i><?php echo _("Upgrade")?>
-																</button>
-														<?php break;?>
-														<?php case 'free':?>
-															<span class="buy">
-																<button class="fpbx-buy" data-rawname="<?php echo $module['name']?>">
-																	<i class="fa fa-money"></i><?php echo _("Buy")?>
-																</button>
-															</span>
-														<?php break;?>
-														<?php case 'paid':?>
-															<span class="buy">
-																<button class="fpbx-buy" data-rawname="<?php echo $module['name']?>">
-																	<i class="fa fa-money"></i><?php echo _("Buy")?>
-																</button>
-															</span>
-														<?php break;?>
-														<?php default:?>
-														<?php break;?>
-													<?php } ?>
+															case 'upgradeable':
+															case 'free':
+																$buyText = _("Upgrade");
+															break;
+															case 'paid':
+															default:
+																$buyText = _("Buy");
+															break;
+													} ?>
+													<span class="buy">
+														<a class="btn fpbx-buy" data-rawname="<?php echo $module['name']?>" href="<?php echo $module['commercial']['purchaselink']?>" target="_new">
+															<i class="fa fa-money"></i><?php echo $buyText?>
+														</a>
+													</span>
 												<?php } ?>
 											<?php } ?>
 										<?php } ?>
