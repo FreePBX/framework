d363079a1b6b0b04de54a5f924357ace14a80ff0
Andrew Nagy <andrew.nagy@the159.com>
2014-10-29 17:59:05 -0400

    inital work on extensions redub

--- amp_conf/htdocs/admin/functions.inc.php
+++ amp_conf/htdocs/admin/functions.inc.php
@@ -426,17 +426,17 @@ function do_reload($passthru=false) {
 
 // draw list for users and devices with paging
 // $skip has been deprecated, used to be used to page-enate
-function drawListMenu($results, $skip, $type, $dispnum, $extdisplay, $description=false) {
+function drawListMenu($results, $skip=null, $type=null, $dispnum, $extdisplay, $description=false) {
 
 	$index = 0;
 	echo "<ul>\n";
 	if ($description !== false) {
- 		echo "\t<li><a ".($extdisplay=='' ? 'class="current"':'')." href=\"config.php?type=".$type."&display=".$dispnum."\">"._("Add")." ".$description."</a></li>\n";
+ 		echo "\t<li><a ".($extdisplay=='' ? 'class="current"':'')." href=\"config.php?display=".$dispnum."\">"._("Add")." ".$description."</a></li>\n";
 	}
 	if (isset($results)) {
 		foreach ($results as $key=>$result) {
 			$index= $index + 1;
-			echo "\t<li><a".($extdisplay==$result[0] ? ' class="current"':''). " href=\"config.php?type=".$type."&display=".$dispnum."&extdisplay={$result[0]}\">{$result[1]} &lt;{$result[0]}&gt;</a></li>\n";
+			echo "\t<li><a".($extdisplay==$result[0] ? ' class="current"':''). " href=\"config.php?display=".$dispnum."&extdisplay={$result[0]}\">{$result[1]} &lt;{$result[0]}&gt;</a></li>\n";
 		}
 	}
 	echo "</ul>\n";
