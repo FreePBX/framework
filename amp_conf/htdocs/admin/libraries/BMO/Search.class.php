28f35749c34bc67f3bc6a8af1a07d17d5c8357fd
Rob Thomas <xrobau@gmail.com>
2014-10-30 06:33:02 +1000

    Add skeleton of Search class

---
+++ amp_conf/htdocs/admin/libraries/BMO/Search.class.php
@@ -0,0 +1,43 @@
+<?php
+// vim: set ai ts=4 sw=4 ft=php:
+/**
+ * This is the FreePBX Big Module Object.
+ *
+ * License for all code of this FreePBX module can be found in the license file inside the module directory
+ * Copyright 2006-2014 Schmooze Com Inc.
+ */
+
+
+class Search extends FreePBX_Helpers {
+
+	public function globalSearch($str) {
+		$retarr = array(
+			array("text" => "Search Result 1", "type" => "get", "dest" => "config.php?display=one"),
+			array("text" => "Second Search Result", "type" => "get", "dest" => "config.php?display=two"),
+			array("text" => "Third Result", "type" => "get", "dest" => "config.php?display=three"),
+			);
+		return $retarr;
+	}
+
+	public function moduleSearch($module, $str) {
+		$module = htmlentities($module);
+		// Lets see if the module exists
+		try {
+			$mod = $this->FreePBX->$module;
+			if(!method_exists($mod, 'search')) {
+				throw new Exception("No Search Method");
+			}
+		} catch (Exception $e) {
+			return array(
+				array("text" => "Module $module doesn't implement search", "type" => "text", "details" => $e->getMessage())
+			);
+		}
+		$res = $mod->search($str);
+		if (!is_array($res)) {
+			return array(
+				array("text" => "Search Error", "type" => "text", "details" => $res)
+			);
+		}
+		return $res;
+	}
+}
