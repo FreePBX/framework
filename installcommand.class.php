7e160727fc4414cdc540118d43fd83e015820079
Jason Parker <jason.parker@schmoozecom.com>
2014-10-29 16:46:10 -0500

    More progress, files copied.

--- installcommand.class.php
+++ installcommand.class.php
@@ -38,6 +38,8 @@ class FreePBXInstallCommand extends Command {
 		),
 	);
 
+	protected $amp_conf;
+
 	protected function configure() {
 		$this
 			->setName('install')
@@ -78,29 +80,30 @@ class FreePBXInstallCommand extends Command {
 
 		// Copy default amportal.conf
 		if (!file_exists(AMP_CONF)) {
-			$amp_conf = $this->amportal_conf_read(dirname(__FILE__) . "/amportal.conf");
+			$newinstall = true;
+			$this->amp_conf = $this->amportal_conf_read(dirname(__FILE__) . "/amportal.conf");
 		} else {
-			$amp_conf = $this->amportal_conf_read(AMP_CONF);
+			$this->amp_conf = $this->amportal_conf_read(AMP_CONF);
 		}
 
 		if (isset($answers['dbengine'])) {
-			$amp_conf['AMPDBENGINE'] = $answers['dbengine'];
+			$this->amp_conf['AMPDBENGINE'] = $answers['dbengine'];
 		}
 		if (isset($answers['webroot'])) {
-			$amp_conf['AMPWEBROOT'] = $answers['webroot'];
+			$this->amp_conf['AMPWEBROOT'] = $answers['webroot'];
 		}
 		if (isset($answers['user'])) {
-			$amp_conf['AMPASTERISKUSER'] = $answers['user'];
-			$amp_conf['AMPASTERISKWEBUSER'] = $answers['user'];
-			$amp_conf['AMPDEVUSER'] = $answers['user'];
+			$this->amp_conf['AMPASTERISKUSER'] = $answers['user'];
+			$this->amp_conf['AMPASTERISKWEBUSER'] = $answers['user'];
+			$this->amp_conf['AMPDEVUSER'] = $answers['user'];
 		}
 		if (isset($answers['group'])) {
-			$amp_conf['AMPASTERISKGROUP'] = $answers['group'];
-			$amp_conf['AMPASTERISKWEBGROUP'] = $answers['group'];
-			$amp_conf['AMPDEVGROUP'] = $answers['group'];
+			$this->amp_conf['AMPASTERISKGROUP'] = $answers['group'];
+			$this->amp_conf['AMPASTERISKWEBGROUP'] = $answers['group'];
+			$this->amp_conf['AMPDEVGROUP'] = $answers['group'];
 		}
-		if (!isset($amp_conf['AMPMANAGERHOST'])) {
-			$amp_conf['AMPMANAGERHOST'] = 'localhost';
+		if (!isset($this->amp_conf['AMPMANAGERHOST'])) {
+			$this->amp_conf['AMPMANAGERHOST'] = 'localhost';
 		}
 
 		// ... and then write amportal.conf?
@@ -132,30 +135,30 @@ class FreePBXInstallCommand extends Command {
 		}
 
 		if (isset($asterisk_conf['astetcdir'])) {
-			$amp_conf['ASTETCDIR'] = $asterisk_conf['astetcdir'];
+			$this->amp_conf['ASTETCDIR'] = $asterisk_conf['astetcdir'];
 		}
 		if (isset($asterisk_conf['astmoddir'])) {
-			$amp_conf['ASTMODDIR'] = $asterisk_conf['astmoddir'];
+			$this->amp_conf['ASTMODDIR'] = $asterisk_conf['astmoddir'];
 		}
 		if (isset($asterisk_conf['astvarlibdir'])) {
-			$amp_conf['ASTVARLIBDIR'] = $asterisk_conf['astvarlibdir'];
+			$this->amp_conf['ASTVARLIBDIR'] = $asterisk_conf['astvarlibdir'];
 		}
 		if (isset($asterisk_conf['astagidir'])) {
-			$amp_conf['ASTAGIDIR'] = $asterisk_conf['astagidir'];
+			$this->amp_conf['ASTAGIDIR'] = $asterisk_conf['astagidir'];
 		}
 		if (isset($asterisk_conf['astspooldir'])) {
-			$amp_conf['ASTSPOOLDIR'] = $asterisk_conf['astspooldir'];
+			$this->amp_conf['ASTSPOOLDIR'] = $asterisk_conf['astspooldir'];
 		}
 		if (isset($asterisk_conf['astrundir'])) {
-			$amp_conf['ASTRUNDIR'] = $asterisk_conf['astrundir'];
+			$this->amp_conf['ASTRUNDIR'] = $asterisk_conf['astrundir'];
 		}
 		if (isset($asterisk_conf['astlogdir'])) {
-			$amp_conf['ASTLOGDIR'] = $asterisk_conf['astlogdir'];
+			$this->amp_conf['ASTLOGDIR'] = $asterisk_conf['astlogdir'];
 		}
 
 		// Read/parse asterisk.conf
 		// ... and then write amportal.conf, again?!
-		$this->amportal_conf_write(dirname(__FILE__) . "/amportal_new.conf", $amp_conf);
+		$this->amportal_conf_write(dirname(__FILE__) . "/amportal_new.conf");
 
 		// Write /etc/asterisk/version ?
 		exec("asterisk -V", $tmpout, $ret);
@@ -166,7 +169,7 @@ class FreePBXInstallCommand extends Command {
 		$astver = $tmpout[0];
 		unset($tmpout);
 
-		file_put_contents($amp_conf['ASTETCDIR'] . '/version', $astver);
+		file_put_contents($this->amp_conf['ASTETCDIR'] . '/version', $astver);
 
 		// Parse Asterisk version.
 		if (preg_match('/^Asterisk (?:SVN-)?(\d+(\.\d+)*)(-?(.*))$/', $astver, $matches)) {
@@ -191,17 +194,17 @@ class FreePBXInstallCommand extends Command {
 
 		// Create database(s).
 		/* CDR first, so we can keep the correct database handler around. */
-		$dsn = $amp_conf['AMPDBENGINE'] . ":host=" . $amp_conf['AMPDBHOST'] . ";dbname=asteriskcdrdb";
-		$db = new \Database($amp_conf, $dsn);
+		$dsn = $this->amp_conf['AMPDBENGINE'] . ":host=" . $this->amp_conf['AMPDBHOST'] . ";dbname=asteriskcdrdb";
+		$db = new \Database($this->amp_conf, $dsn);
 		$sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'asteriskcdrdb';";
 		if (!$db->getOne($sql)) {
 			$this->installampimport_mysql_dump('cdr_mysql_table.sql', $db);
 		}
 		unset($db);
 
-		$dsn = $amp_conf['AMPDBENGINE'] . ":host=" . $amp_conf['AMPDBHOST'] . ";dbname=" . $amp_conf['AMPDBNAME'];
-		$db = new \Database($amp_conf, $dsn);
-		$sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '" . $amp_conf['AMPDBNAME'] . "';";
+		$dsn = $this->amp_conf['AMPDBENGINE'] . ":host=" . $this->amp_conf['AMPDBHOST'] . ";dbname=" . $this->amp_conf['AMPDBNAME'];
+		$db = new \Database($this->amp_conf, $dsn);
+		$sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '" . $this->amp_conf['AMPDBNAME'] . "';";
 		if (!$db->getOne($sql)) {
 			$this->installampimport_mysql_dump('newinstall.sql', $db);
 		}
@@ -210,36 +213,36 @@ class FreePBXInstallCommand extends Command {
 		$version = $this->get_version($db);
 
 		// Copy amp_conf/
-//		$this->recursive_copy("amp_conf", "");
+		$this->recursive_copy("amp_conf", "", "", $newinstall, true);
 
 		// Create dirs
 		// 	/var/www/html/admin/modules/framework/
 		// 	/var/www/html/admin/modules/_cache/
 		//	./amp_conf/htdocs/admin/modules/_cache/
-		mkdir($amp_conf['AMPWEBROOT'] . "/admin/modules/_cache", 0777, true);
+		mkdir($this->amp_conf['AMPWEBROOT'] . "/admin/modules/_cache", 0777, true);
 
 		// Copy /var/www/html/admin/modules/framework/module.xml
-		copy(dirname(__FILE__) . "/module.xml", $amp_conf['AMPWEBROOT'] . "/admin/modules/framework/module.xml");
+		copy(dirname(__FILE__) . "/module.xml", $this->amp_conf['AMPWEBROOT'] . "/admin/modules/framework/module.xml");
 
 		// Create dirs
 		//	/var/spool/asterisk/voicemail/device/
-		mkdir($amp_conf['AMPSPOOLDIR'] . "/voicemail/device", 0755, true);
+		mkdir($this->amp_conf['AMPSPOOLDIR'] . "/voicemail/device", 0755, true);
 		// Copy /etc/asterisk/voicemail.conf.template
 		// ... to /etc/asterisk/voicemail.conf
 
 		//	/var/spool/asterisk/fax/
-		mkdir($amp_conf['AMPSPOOLDIR'] . "/fax", 0766, true);
+		mkdir($this->amp_conf['AMPSPOOLDIR'] . "/fax", 0766, true);
 
 		//	/var/spool/asterisk/monitor/
-		mkdir($amp_conf['AMPSPOOLDIR'] . "/monitor", 0766, true);
+		mkdir($this->amp_conf['AMPSPOOLDIR'] . "/monitor", 0766, true);
 
 		//	/var/www/html/recordings/themes/js/
-		mkdir($amp_conf['AMPWEBROOT'] . "/recordings/themes/js", 0755, true);
+		mkdir($this->amp_conf['AMPWEBROOT'] . "/recordings/themes/js", 0755, true);
 
 		// Link /var/www/html/admin/common/libfreepbx.javascripts.js
 		// ... to /var/www/html/recordings/themes/js/
-		$js = $amp_conf['AMPWEBROOT'] . "/admin/common/libfreepbx.javascripts.js";
-		$js_link = $amp_conf['AMPWEBROOT'] . "/recordings/themes/js/libfreepbx.javascripts.js";
+		$js = $this->amp_conf['AMPWEBROOT'] . "/admin/common/libfreepbx.javascripts.js";
+		$js_link = $this->amp_conf['AMPWEBROOT'] . "/recordings/themes/js/libfreepbx.javascripts.js";
 		if (file_exists($js) && !file_exists($js_link)) {
 			link($js, $js_link);
 		}
@@ -254,7 +257,7 @@ class FreePBXInstallCommand extends Command {
 		//	ASTMANAGERHOST - should this be localhost?  Yes.
 
 		// Create missing #include files.
-		exec("grep -h '^#include' " . $amp_conf['ASTETCDIR'] . "/*.conf | sed 's/\s*;.*//;s/#include\s*//'", $tmpout, $ret);
+		exec("grep -h '^#include' " . $this->amp_conf['ASTETCDIR'] . "/*.conf | sed 's/\s*;.*//;s/#include\s*//'", $tmpout, $ret);
 		if ($ret != 0) {
 			$output->writeln("Error finding #include files.");
 			die();
@@ -262,7 +265,7 @@ class FreePBXInstallCommand extends Command {
 
 		foreach ($tmpout as $file) {
 			if ($file[0] != "/") {
-				$file = $amp_conf['ASTETCDIR'] . "/" . $file;
+				$file = $this->amp_conf['ASTETCDIR'] . "/" . $file;
 			}
 			if (!file_exists($file)) {
 				touch($file);
@@ -284,7 +287,7 @@ class FreePBXInstallCommand extends Command {
 		// needreload();
 	}
 
-	function amportal_conf_read($filename) {
+	private function amportal_conf_read($filename) {
 		$file = file($filename);
 
 		foreach ($file as $line) {
@@ -296,15 +299,15 @@ class FreePBXInstallCommand extends Command {
 		return $conf;
 	}
 
-	function amportal_conf_write($filename, $conf) {
-		foreach ($conf as $key => $value) {
+	private function amportal_conf_write($filename) {
+		foreach ($this->amp_conf as $key => $value) {
 			$file[] = $key . "=" . $value;
 		}
 
 		file_put_contents($filename, implode("\n", $file) . "\n");
 	}
 
-	function asterisk_conf_read($filename) {
+	private function asterisk_conf_read($filename) {
 		$file = file($filename);
 
 		foreach ($file as $line) {
@@ -316,7 +319,7 @@ class FreePBXInstallCommand extends Command {
 		return $conf;
 	}
 
-	function asterisk_conf_write($filename, $conf) {
+	private function asterisk_conf_write($filename, $conf) {
 		foreach ($conf as $key => $value) {
 			$file[] = $key . "=>" . $value;
 		}
@@ -332,7 +335,7 @@ class FreePBXInstallCommand extends Command {
 	private function set_version($db, $version) {
 	}
 
-	function installampimport_mysql_dump($file, $db) {
+	private function installampimport_mysql_dump($file, $db) {
 		$dir = dirname(__FILE__);
 		if (!file_exists($dir.'/SQL/'.$file)) {
 			return false;
@@ -362,6 +365,167 @@ class FreePBXInstallCommand extends Command {
 				$templine = '';
 			}
 		}
+	}
+
+	/**
+	 * Recursive Read Links
+	 *
+	 * This function is used to recursively read symlink until we reach a real directory
+	 *
+	 * @params string $source - The original file we are replacing
+	 * @returns array of the original source we read in and the real directory for it
+	 */
+	private function recursive_readlink($source){
+		$dir = dirname($source);
+		$links = array();
+		$ldir = null;
+
+		while (!in_array($dir,array('.','..','','/')) && strpos('.git',$dir) == false) {
+			if ($dir == $ldir) {
+				break;
+			}
+			if (is_link($dir)) {
+				$ldir = readlink($dir);
+				$file = str_replace($dir, $ldir, $source);
+				if (!is_link($ldir) && file_exists($file)) {
+					$links[$source] = $file;
+				}
+			} else {
+				if (file_exists($source) && !is_link(dirname($source))) {
+					break;
+				}
+				$ldir = dirname($dir);
+				$file = str_replace($dir, $ldir, $source);
+				if (!is_link($ldir) && file_exists($file)) {
+					$links[$source] = $file;
+				}
+			}
+			$ldir = $dir;
+			$dir = dirname($dir);
+		}
+
+		return $links;
+	}
+
+	/**
+	 * Substitute Read Links
+	 *
+	 * This function is used to substitute symlinks, to real directories where information is stored
+	 *
+	 * @params string $source - The original file we are replacing
+	 * @params array $links - A list of possible replacements
+	 * @return string of the real file path to the given source
+	 */
+	private function substitute_readlinks($source,$links) {
+		foreach ($links as $key => $value) {
+			if (strpos($source, $key) !== false) {
+				$source = str_replace($key, $value, $source);
+				return $source;
+			}
+		}
+	}
+
+	private function recursive_copy($dirsourceparent, $dirdest, $dirsource = "", $newinstall = true, $make_links = false) {
+		$moh_subdir = isset($this->amp_conf['MOHDIR']) ? trim(trim($this->amp_conf['MOHDIR']),'/') : 'moh';
+
+		// total # files, # actually copied
+		$num_files = $num_copied = 0;
+
+		if ($dirsource && ($dirsource[0] != "/")) $dirsource = "/".$dirsource;
+
+		if (is_dir($dirsourceparent.$dirsource)) $dir_handle = opendir($dirsourceparent.$dirsource);
+
+		while (isset($dir_handle) && ($file = readdir($dir_handle))) {
+			if (($file==".") || ($file=="..") || ($file == "CVS") || ($file == ".svn") || ($file == ".git")) {
+				continue;
+			}
+
+			$source = $dirsourceparent.$dirsource."/".$file;
+			$destination =  $dirdest.$dirsource."/".$file;
+
+			if ($dirsource == "" && $file == "moh" && !$newinstall) {
+				// skip to the next dir
+				continue;
+			}
+
+			// configurable in amportal.conf
+			$destination=preg_replace("/^\/htdocs/",trim($this->amp_conf["AMPWEBROOT"])."/",$destination);
+
+			$destination=str_replace("/astetc",trim($this->amp_conf["ASTETCDIR"]),$destination);
+			$destination=str_replace("/moh",trim($this->amp_conf["ASTVARLIBDIR"])."/$moh_subdir",$destination);
+			$destination=str_replace("/astvarlib",trim($this->amp_conf["ASTVARLIBDIR"]),$destination);
+			if(strpos($dirsource, 'modules') === false) {
+				$destination=str_replace("/agi-bin",trim($this->amp_conf["ASTAGIDIR"]),$destination);
+				$destination=str_replace("/sounds",trim($this->amp_conf["ASTVARLIBDIR"])."/sounds",$destination);
+				$destination=str_replace("/bin",trim($this->amp_conf["AMPBIN"]),$destination);
+			}
+			$destination=str_replace("/sbin",trim($this->amp_conf["AMPSBIN"]),$destination);
+
+			if (!is_dir($source)) {
+				// These are modified by apply_conf.sh, there may be others that fit in this category also. This keeps these from
+				// being symlinked and then developers inadvertently checking in the changes when they should not have.
+				//
+				$never_symlink = array("cdr_mysql.conf", "manager.conf", "vm_email.inc", "modules.conf");
+
+				$num_files++;
+				if ($make_links && !in_array(basename($source),$never_symlink)) {
+					// symlink, unlike copy, doesn't overwrite - have to delete first
+					if (is_link($destination) || file_exists($destination)) {
+						unlink($destination);
+					}
+
+					$links = $this->recursive_readlink($source);
+					if (!empty($links)) {
+						@symlink($this->substitute_readlinks($source,$links), $destination);
+					} else {
+						if(file_exists(dirname(__FILE__)."/".$source)) {
+							@symlink(dirname(__FILE__)."/".$source, $destination);
+						}
+					}
+				} else {
+					$ow = false;
+					if(file_exists($destination)) {
+						if ($this->check_diff($source, $destination) && !$make_links) {
+							$ow = ask_overwrite($source, $destination);
+						}
+					} else {
+						$ow = true;
+					}
+					//ask_overwrite
+					if($ow) {
+						//Copy will not overwrite a symlink, phpnesssss
+						if(file_exists($destination) && is_link($destination)) {
+							unlink($destination);
+						}
+						copy($source, $destination);
+					} else {
+						continue;
+					}
+				}
+				$num_copied++;
+			} else {
+				// if this is a directory, ensure destination exists
+				if (!file_exists($destination)) {
+					if ($destination != "") {
+						mkdir($destination, "0750", true);
+					}
+				}
+
+				list($tmp_num_files, $tmp_num_copied) = $this->recursive_copy($dirsourceparent, $dirdest, $dirsource."/".$file, $newinstall, $make_links);
+				$num_files += $tmp_num_files;
+				$num_copied += $tmp_num_copied;
+			}
+		}
+
+		if (isset($dir_handle)) closedir($dir_handle);
+
+		return array($num_files, $num_copied);
+	}
+
+	private function check_diff($file1, $file2) {
+		// diff, ignore whitespace and be quiet
+		exec("diff -wq ".escapeshellarg($file2)." ".escapeshellarg($file1), $output, $retVal);
+		return ($retVal != 0);
 	}
 }
 
