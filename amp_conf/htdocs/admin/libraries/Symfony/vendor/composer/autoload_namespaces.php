45c0be8fe0d698eba202a4c1177c6b0c6751b01c
Andrew Nagy <andrew.nagy@the159.com>
2014-10-31 10:18:56 -0400

    more symfony classes

--- amp_conf/htdocs/admin/libraries/Symfony/vendor/composer/autoload_namespaces.php
+++ amp_conf/htdocs/admin/libraries/Symfony/vendor/composer/autoload_namespaces.php
@@ -6,5 +6,12 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Security\\' => array($vendorDir . '/symfony/security'),
+    'Symfony\\Component\\Process\\' => array($vendorDir . '/symfony/process'),
+    'Symfony\\Component\\HttpKernel\\' => array($vendorDir . '/symfony/http-kernel'),
+    'Symfony\\Component\\HttpFoundation\\' => array($vendorDir . '/symfony/http-foundation'),
+    'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
+    'Symfony\\Component\\Debug\\' => array($vendorDir . '/symfony/debug'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
 );
