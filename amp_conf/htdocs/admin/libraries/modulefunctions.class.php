92e3674f731cd33ec0dabe1944677f597d136d8e
Andrew Nagy <andrew.nagy@the159.com>
2014-12-01 19:43:59 -0500

    small tracking change

--- amp_conf/htdocs/admin/libraries/modulefunctions.class.php
+++ amp_conf/htdocs/admin/libraries/modulefunctions.class.php
@@ -2713,6 +2713,7 @@ class module_functions {
 					$options['modules'][$m]['version'] = $mod['version'];
 					$options['modules'][$m]['status'] = $mod['status'];
 					$options['modules'][$m]['rawname'] = $mod['rawname'];
+					$options['modules'][$m]['license'] = !empty($mod['license']) ? $mod['license'] : "unknown";
 				}
 			}
 
