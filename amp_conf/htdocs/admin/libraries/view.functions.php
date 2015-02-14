4fd32f0f6ed02ff854942e986b961897e4415215
Andrew Nagy <andrew.nagy@the159.com>
2015-02-13 19:54:16 -0500

    better localization awareness

--- amp_conf/htdocs/admin/libraries/view.functions.php
+++ amp_conf/htdocs/admin/libraries/view.functions.php
@@ -37,13 +37,19 @@ function frameworkPasswordCheck() {
 // setup locale
 function set_language() {
 	if (extension_loaded('gettext')) {
-		if (empty($_COOKIE['lang']) || !preg_match('/^[\w\._@-]+$/', $_COOKIE['lang'])) {
-			$_COOKIE['lang'] = 'en_US';
+		if (empty($_COOKIE['lang']) || !preg_match('/^[\w\._@-]+$/', $_COOKIE['lang'], $matches)) {
+			$lang = $amp_conf['UIDEFAULTLANG']?$amp_conf['UIDEFAULTLANG']:'en_US';
+			$_COOKIE['lang'] = $lang;
+		} else {
+			preg_match('/^([\w\._@-]+)$/', $_COOKIE['lang'], $matches);
+			$lang = !empty($matches[1])?$matches[1]:'en_US';
 		}
-		setlocale(LC_ALL,  $_COOKIE['lang']);
-		putenv("LANGUAGE=".$_COOKIE['lang']);
+		putenv('LC_ALL='.$lang);
+		putenv('LANG='.$lang);
+		putenv('LANGUAGE='.$lang);
+		setlocale(LC_ALL, $lang);
 
-        bindtextdomain('amp','./i18n');
+		bindtextdomain('amp',$amp_conf['AMPWEBROOT'].'/admin/i18n');
 		bind_textdomain_codeset('amp', 'utf8');
 		textdomain('amp');
 	}
