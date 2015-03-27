ceca386d5f586be99857457a0742235f51f2413a
Andrew Nagy <andrew.nagy@the159.com>
2015-03-27 02:49:10 +0100

    FREEPBX-8361 update hooks during install and uninstall

--- amp_conf/htdocs/admin/libraries/modulefunctions.class.php
+++ amp_conf/htdocs/admin/libraries/modulefunctions.class.php
@@ -1916,6 +1916,12 @@ class module_functions {
 			}
 		}catch(\Exception $e) {}
 		out(_("Done"));
+
+		outn(_("Updating Hooks..."));
+		try {
+			\FreePBX::Hooks()->updateBMOHooks();
+		}catch(\Exception $e) {}
+		out(_("Done"));
 		return true;
 	}
 
@@ -2020,6 +2026,13 @@ class module_functions {
 			$sth = FreePBX::Database()->prepare("DELETE FROM `kvstore` WHERE `module` = :mod");
 			$sth->execute(array(":mod" => $modulename));
 		} catch(\Exception $e) {}
+
+		outn(_("Updating Hooks..."));
+		try {
+			\FreePBX::Hooks()->updateBMOHooks();
+		}catch(\Exception $e) {}
+		out(_("Done"));
+
 		return true;
 	}
 
