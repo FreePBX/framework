26c82843f1535307e70be7245ecbc36d61f88f0c
Andrew Nagy <andrew.nagy@the159.com>
2015-05-06 10:18:06 -0400

    first fully functional web test

--- utests/WebTest.php
+++ utests/WebTest.php
@@ -5,6 +5,8 @@
 * @backupStaticAttributes disabled
 */
 use Guzzle\Http\Client;
+use Guzzle\Plugin\Cookie\CookiePlugin;
+use Guzzle\Plugin\Cookie\CookieJar\ArrayCookieJar;
 use Symfony\Component\DomCrawler\Crawler;
 class WebTest extends PHPUnit_Framework_TestCase {
 
@@ -16,7 +18,9 @@ class WebTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function testLogin() {
+		$cookiePlugin = new CookiePlugin(new ArrayCookieJar());
 		$client = new Client('http://127.0.0.1/admin/');
+		$client->addSubscriber($cookiePlugin);
 		$request = $client->get('config.php');
 		$response = $request->send();
 		$crawler = new Crawler($response->getBody(true));
@@ -24,6 +28,24 @@ class WebTest extends PHPUnit_Framework_TestCase {
 			0,
 			$crawler->filter('html:contains("FreePBX Administration")')->count(),
 			"The Login Page Seems Incorrect. Missing FreePBX Administration"
+		);
+
+		$this->assertGreaterThan(
+			0,
+			$crawler->filter('#key')->count(),
+			"Could not find login token"
+		);
+
+		$key = $crawler->filter('#key')->text();
+		$key = trim($key);
+		exec('fwconsole u '.$key);
+
+		$response = $request->send();
+		$crawler = new Crawler($response->getBody(true));
+		$this->assertEquals(
+			0,
+			$crawler->filter('#key')->count(),
+			"The Session unlock process did not work"
 		);
 	}
 }
