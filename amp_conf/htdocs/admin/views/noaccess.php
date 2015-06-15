f8805280b60df5b394c6b6ee63eb9402a089c169
James Finstrom <jfinstrom@sangoma.com>
2015-06-15 19:53:39 -0400

    Add some help text to noaccess.php

--- amp_conf/htdocs/admin/views/noaccess.php
+++ amp_conf/htdocs/admin/views/noaccess.php
@@ -1,8 +1,13 @@
-<?php
-$html = '';
-$html .= '<h2>' . _('Not found') .'</h2>';
-$html .= _('The section you requested does not exist or you do not have access '
-	. 'to it.');
-//show_view($amp_conf['VIEW_FREEPBX'], $template);
-echo $html;
-?>
+<div class="container-fluid">
+	<h1><?php echo _('Not Found')?></h1>
+	<div class="alert alert-warning">
+		<?php echo _('The section you requested does not exist or you do not have access to it.')?>
+	</div>
+<h2><?php echo _("Additional Information")?></h2>
+<ul class='list-group'>
+	<li class="list-group-item"><a href="#"><?php echo _("Make sure you entered the address correctly.")?></a></li>
+	<li class="list-group-item"><a href="?display=ampusers"><?php echo _("Make sure you have access to this item.")?></a></li>
+	<li class="list-group-item"><a href="?display=modules"><?php echo _("Make sure the module is installed and enabled.")?></a></li>
+</ul>
+
+</div>
