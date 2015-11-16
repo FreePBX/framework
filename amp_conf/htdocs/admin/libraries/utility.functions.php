aa692f48ba938c254b077ea14e97d2cc29bda2e4
Andrew Nagy <andrew.nagy@the159.com>
2015-11-16 10:00:32 -0800

    add non-existant function

--- amp_conf/htdocs/admin/libraries/utility.functions.php
+++ amp_conf/htdocs/admin/libraries/utility.functions.php
@@ -32,6 +32,24 @@ function SPLAutoloadBroken() {
 	}
 	return true;
 }
+
+/** In PHP 5.5 **/
+if (!function_exists('json_last_error_msg')) {
+	function json_last_error_msg() {
+		static $ERRORS = array(
+			JSON_ERROR_NONE => 'No error',
+			JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+			JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
+			JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+			JSON_ERROR_SYNTAX => 'Syntax error',
+			JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+		);
+
+		$error = json_last_error();
+		return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
+	}
+}
+
 /**
  * Log a message to the freepbx security file
  * @param  {string} $message the message
