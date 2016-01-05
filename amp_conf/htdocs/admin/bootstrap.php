2c05765caae9cd2e981e4166caa9165da6254528
Andrew Nagy <andrew.nagy@the159.com>
2016-01-05 12:14:42 -0800

    timezone set in bootstrap

--- amp_conf/htdocs/admin/bootstrap.php
+++ amp_conf/htdocs/admin/bootstrap.php
@@ -159,6 +159,10 @@ $db = new DB();
 
 // get settings
 $freepbx_conf = $bmo->Freepbx_conf();
+$phptimezone = $freepbx_conf->get('PHPTIMEZONE');
+if(!empty($phptimezone)) {
+	date_default_timezone_set($phptimezone);
+}
 
 // passing by reference, this means that the $amp_conf available to everyone is the same one as present
 // within the class, which is probably a direction we want to go to use the class.
