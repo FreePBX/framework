200ca26236adaf9bbe1a1a7792082faef085e646
Andrew Nagy <andrew.nagy@the159.com>
2016-02-18 17:15:57 -0800

    comments and cleanup

--- amp_conf/htdocs/admin/helpers/freepbx_helpers.php
+++ amp_conf/htdocs/admin/helpers/freepbx_helpers.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * TODO: This needs to go away in 14. Remove useless functions
+ * move drawselects to utilities or view (pref view)
+ */
 
 /**
  * short FreePBX Label generator
@@ -11,7 +15,6 @@
  * @return string
  * @todo change format to take advantage of html's data attribute. No need for spans!
  *
- * {@source }
  */
 function fpbx_label($text, $help = '') {
 	if ($help) {
@@ -34,7 +37,6 @@ function fpbx_label($text, $help = '') {
  * @author Philippe Lindheimer
  * @param string $text
  * @return string
- * {@source }
  */
 function fpbx_section_header($text) {
 	return '<br><h5>' . $text . '<hr></h5>';
@@ -288,24 +290,31 @@ function drawselects($goto, $i, $show_custom=false, $table=true, $nodest_msg='',
 	return $output_array ? $ds_array : $html;
 }
 
-// This function will get the MySQL field size of the specified fieldname
-// It's useful for finding out the limit of certain fields in MySQL so that
-// we can do validation checks on strings to make sure they aren't too long.
-// This will help prevent MySQL from needing to do auto chopping on lengthy strings
-// which causes problems with multibyte characters getting cut off abruptly.
-// The third argument defaultsize is just to futureproof in case someone decides
-// to change things in MySQL in the future that would otherwise just pass null back
-// and cause a bug.
+
+/**
+ * This function will get the MySQL field size of the specified fieldname
+ * It's useful for finding out the limit of certain fields in MySQL so that
+ * we can do validation checks on strings to make sure they aren't too long.
+ * This will help prevent MySQL from needing to do auto chopping on lengthy strings
+ * which causes problems with multibyte characters getting cut off abruptly.
+ * The third argument defaultsize is just to futureproof in case someone decides
+ * to change things in MySQL in the future that would otherwise just pass null back
+ * and cause a bug.
+ * @param  string $tablename   The table name
+ * @param  string $fieldname   The fieldname
+ * @param  string $defaultsize Default Size of the field
+ * @return string              The default size
+ */
 function module_get_field_size($tablename, $fieldname, $defaultsize) {
-        global $db;
+	global $db;
 
-        $sql = "SELECT character_maximum_length FROM information_schema.columns WHERE table_name = ? AND column_name = ?";
+	$sql = "SELECT character_maximum_length FROM information_schema.columns WHERE table_name = ? AND column_name = ?";
 
-        $results = $db->getAll($sql, array($tablename, $fieldname));
+	$results = $db->getAll($sql, array($tablename, $fieldname));
 
-        if(DB::IsError($results)) {
-                $results = null;
-        }
+	if(DB::IsError($results)) {
+		$results = null;
+	}
 
-        return isset($results)?$results[0][0]:$defaultsize;
+	return isset($results)?$results[0][0]:$defaultsize;
 }
