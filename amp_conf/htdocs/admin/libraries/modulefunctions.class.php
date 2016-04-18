6efbc4f9c844c697febac4ba5287fc37bd6f2e57
Andrew Nagy <andrew.nagy@the159.com>
2016-04-18 14:37:56 -0700

    FREEPBX-12084 only show edge if version is higher than whats online

--- amp_conf/htdocs/admin/libraries/modulefunctions.class.php
+++ amp_conf/htdocs/admin/libraries/modulefunctions.class.php
@@ -195,7 +195,7 @@ class module_functions {
 			if ($module != false) {
 				foreach ($modules as $mod) {
 					if ($module == $mod['rawname']) {
-						if(!empty($edge[$module]) && (isset($amp_conf['MODULEADMINEDGE']) && $amp_conf['MODULEADMINEDGE'])) {
+						if(!empty($edge[$module]) && (isset($amp_conf['MODULEADMINEDGE']) && $amp_conf['MODULEADMINEDGE']) && version_compare_freepbx($mod['version'],$edge[$module]['version'],'<')) {
 							$mod = $edge[$module];
 						}
 						$releases = !empty($previous[$module]['releases']['module']) ? $previous[$module]['releases']['module'] : array();
@@ -223,7 +223,7 @@ class module_functions {
 			} else {
 				$final = array();
 				foreach ($modules as $mod) {
-					if(!empty($edge[$mod['rawname']]) && (isset($amp_conf['MODULEADMINEDGE']) && $amp_conf['MODULEADMINEDGE'])) {
+					if(!empty($edge[$mod['rawname']]) && (isset($amp_conf['MODULEADMINEDGE']) && $amp_conf['MODULEADMINEDGE']) && version_compare_freepbx($mod['version'],$edge[$mod['rawname']]['version'],'<')) {
 						$mod = $edge[$mod['rawname']];
 					}
 					$final[$mod['rawname']] = $mod;
