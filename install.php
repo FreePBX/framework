0992672a4723ec1dd45ab08f82c56f3195a5c948
Rob Thomas <xrobau@gmail.com>
2016-05-11 12:18:33 +1000

    Fix chmod error on install

--- install.php
+++ install.php
@@ -98,7 +98,7 @@ if (is_link("$wr/admin/images/notify_security.png")) {
 		framework_print_errors($bin_source, $bin_dest, $out);
 		out(_("done, see errors below"));
 	} else {
-		exec("chmod +x $bin_source/*");
+		exec("chmod +x $bin_dest/*");
 		out(_("done"));
 	}
 
@@ -109,7 +109,7 @@ if (is_link("$wr/admin/images/notify_security.png")) {
 		framework_print_errors($agibin_source, $agibin_dest, $out);
 		out(_("done, see errors below"));
 	} else {
-		exec("chmod +x $agibin_source/*");
+		exec("chmod +x $agibin_dest/*");
 		out(_("done"));
 	}
 
