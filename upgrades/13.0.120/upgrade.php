565a1ecb1b9582249093cf9b004d323752f06fb3
Andrew Nagy <andrew.nagy@the159.com>
2016-05-13 16:05:34 -0700

    needs to be false not true

--- upgrades/13.0.120/upgrade.php
+++ upgrades/13.0.120/upgrade.php
@@ -6,9 +6,9 @@ $freepbx_conf =& freepbx_conf::create();
 //Depreciated advanced settings that no longer apply FREEPBX-12312
 $set = array();
 $set['hidden'] = 1;
-$set['value'] = true;
-$set['defaultval'] = true;
+$set['value'] = false;
+$set['defaultval'] = false;
 if ($freepbx_conf->conf_setting_exists('DYNAMICHINTS')) {
-	$freepbx_conf->set_conf_values(array('DYNAMICHINTS' => true), true);
+	$freepbx_conf->set_conf_values(array('DYNAMICHINTS' => false), true);
 	$freepbx_conf->define_conf_setting('DYNAMICHINTS',$set,true);
 }
