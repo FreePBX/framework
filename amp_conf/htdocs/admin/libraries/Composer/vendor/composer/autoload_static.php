f24ffb3c4ac36af6cf2e41ec98129b3e83fbed87
James Finstrom <jfinstrom@sangoma.com>
2016-05-20 18:43:03 -0500

    Add swiftmailer TODO: Write wrapper

--- amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_static.php
+++ amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_static.php
@@ -14,6 +14,7 @@ class ComposerStaticIniteed34d9d8733e39baaa15f2a55f59cd6
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
     );
 
     public static $prefixLengthsPsr4 = array (
