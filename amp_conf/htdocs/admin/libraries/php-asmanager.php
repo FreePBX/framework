bc466e7ab6e00c3f2087e622d434266adbff0233
Andrew Nagy <andrew.nagy@the159.com>
2016-08-31 16:04:31 -0700

    code review changes re: bryan, make sure to get first line

--- amp_conf/htdocs/admin/libraries/php-asmanager.php
+++ amp_conf/htdocs/admin/libraries/php-asmanager.php
@@ -310,15 +310,12 @@ class AGI_AsteriskManager {
 					} elseif(count($parameters) == 2) {
 						if($parameters['Response'] == "Success" && isset($parameters['Message']) && $parameters['Message'] == 'Command output follows') {
 							// A 'Command output follows' response means there is a muiltiline field that follows.
-							$parameters['data'] = '';
+							$parameters['data'] = preg_replace("/^Output:\s*/","",$buffer);
 							$buff = fgets($this->socket, 4096);
 							while($buff !== "\r\n") {
 								$buff = preg_replace("/^Output:\s*/","",$buff);
 								$parameters['data'] .= $buff;
 								$buff = fgets($this->socket, 4096);
-							}
-							if(empty($parameters['data'])) {
-								$parameters['data'] = preg_replace("/^Output:\s*/","",$buffer);
 							}
 							break;
 						}
