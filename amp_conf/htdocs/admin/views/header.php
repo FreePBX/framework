45192ab40af24377cc5a60524d7f08eb56d9cbfd
Andrew Nagy <andrew.nagy@the159.com>
2017-01-04 17:23:26 -0800

    make this a real grown up js file

--- amp_conf/htdocs/admin/views/header.php
+++ amp_conf/htdocs/admin/views/header.php
@@ -87,38 +87,7 @@ if(isset($module_name) && !empty($module_name)) {
 	$html .= '<script>textdomain("' . $module_name . '")</script>';
 }
 
-// Add global variables to be used later
-$html .= "<script type='text/javascript'>
-	var fpbx = Class.extend({
-		params: {},
-		init: function() {
-			var self = this;
-			var path = window.location.pathname.toString().split('/');
-
-			path[path.length - 1] = 'ajax.php';
-			if (typeof(window.location.origin) == 'undefined') {
-				// Oh look, IE. Hur Dur, I'm a bwowsah.
-				window.location.origin = window.location.protocol+'//'+window.location.host;
-				if (window.location.port.length != 0) {
-					window.location.origin = window.location.origin+':'+window.location.port;
-				}
-			}
-			this.ajaxurl = window.location.origin + path.join('/');
-			if (window.location.search.length) {
-				var params = window.location.search.split(/\?|&/);
-				// NOT using jquery here. This is a bit more annoying, yes, but it means we
-				// can move it out of the way later. Note we break compat with IE8 and below
-				// here.
-				params.forEach(function(v) {
-					if (res = v.match(/(.+)=(.+)/)) {
-						self.params[res[1]] = res[2];
-					}
-				});
-			}
-		}
-	});
-	window.FreePBX = new fpbx();
-</script>";
+$html .= '<script type="text/javascript" src="assets/js/FreePBX.js' . $version_tag . '"></script>';
 
 $html .= '<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1">
 <meta name="apple-mobile-web-app-capable" content="yes" />
