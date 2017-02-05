879a14ee3511bc650fcd1822cddb0e16688ff44f
Rob Thomas <xrobau@gmail.com>
2017-02-06 09:06:03 +1100

    Auto-detect admin port, if possible
    Conflicts:
    	utests/WebTest.php

--- utests/WebTest.php
+++ utests/WebTest.php
@@ -14,11 +14,43 @@ class WebTest extends PHPUnit_Framework_TestCase {
 	public static function setUpBeforeClass() {
 		self::$f = FreePBX::create();
 	}
+	private function getHttpPort($name = "acp") {
+		// This is only usable if the machine the tests are run on
+		// is a FreePBX Distro machine, and that the ports have been
+		// configured by sysadmin.  If the ports are not discoverable,
+		// it will return false.
+		$conffile = false;
+		if (file_exists("/etc/httpd/conf.d/schmoozecom.conf")) {
+			$conffile = "/etc/httpd/conf.d/schmoozecom.conf";
+		} elseif (file_exists("/etc/httpd/conf.d/sangoma.conf")) {
+			$conffile = "/etc/httpd/conf.d/sangoma.conf";
+		} else {
+			// Unable to detect, return false.
+			return false;
+		}
+		$filearr = file($conffile, FILE_IGNORE_NEW_LINES);
+		// Try to json_decode the second line, which should
+		// contain the configuration of the system
+		if (!isset($filearr[1])) {
+			return false;
+		}
+		$confarr = @json_decode(substr($filearr[1], 2), true);
+		if (isset($confarr[$name])) {
+			return $confarr[$name]['port'];
+		} else {
+			return false;
+		}
+	}
 
 	public function testLogin() {
+		$port = $this->getHttpPort("acp");
+		if (!$port) {
+			// Not a FreePBX Distro machine. Assume port 80
+			$port = 80;
+		}
 		$jar = new CookieJar();
 		$client = new Client();
-		$res = $client->request('GET', 'http://127.0.0.1/admin/', ['cookies' => $jar]);
+		$res = $client->request('GET', "http://127.0.0.1:$port/admin/", ['cookies' => $jar]);
 		$body = $res->getBody();
 		$body = (string)$body;
 
@@ -39,7 +71,7 @@ class WebTest extends PHPUnit_Framework_TestCase {
 		$key = trim($key);
 		exec('fwconsole unlock '.$key);
 
-                $res = $client->request('GET', 'http://127.0.0.1/admin/', ['cookies' => $jar]);
+                $res = $client->request('GET', "http://127.0.0.1:$port/admin/", ['cookies' => $jar]);
                 $body = $res->getBody();
 		$body = (string)$body;
 
