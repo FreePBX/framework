ddc743ad07a224fa87d79189e3ec8226cc68e45f
Rob Thomas <xrobau@gmail.com>
2017-06-07 05:09:17 +0000

    Fix legacy PHP that I accidentally copied and pasted.

--- upgrades/14.0.1rc1.20/upgrade.php
+++ upgrades/14.0.1rc1.20/upgrade.php
@@ -3,7 +3,7 @@ global $amp_conf;
 if(!class_exists('freepbx_conf')) {
 	include_once ($amp_conf['AMPWEBROOT'].'/admin/libraries/freepbx_conf.class.php');
 }
-$freepbx_conf =& freepbx_conf::create();
+$freepbx_conf = freepbx_conf::create();
 
 if ($freepbx_conf->conf_setting_exists('EXPOSE_ALL_FEATURE_CODES')) {
 	$freepbx_conf->remove_conf_settings('EXPOSE_ALL_FEATURE_CODES');
