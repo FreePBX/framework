e8a7bcb19c15362b5d6d38bb6a2cd0255c67a1be
Andrew Nagy <andrew.nagy@the159.com>
2017-07-27 08:47:51 -0700

    Remove res_digium_phones from noload even if pjsip is disabled

--- upgrades/13.0.192.16/upgrade.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config = FreePBX::ModulesConf()->ProcessedConfig;
-if(in_array('res_digium_phones.so',$config['modules']['noload'])) {
-	FreePBX::ModulesConf()->removenoload('res_digium_phones.so');
-}
