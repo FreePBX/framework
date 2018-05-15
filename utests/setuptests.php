931235e0d10271eca621b024cd5853a1d893e2df
Andrew Nagy <andrew.nagy@the159.com>
2018-05-15 19:44:22 -0400

    cant run these tests anymore

--- utests/setuptests.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $amp_conf, $db;
-$restrict_mods = true;
-if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
-	include_once('/etc/asterisk/freepbx.conf');
-}
-restore_error_handler();
-error_reporting(-1);
