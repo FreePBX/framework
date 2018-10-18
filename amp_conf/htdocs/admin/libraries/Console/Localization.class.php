0e54a645f6cba5f48894ae0b7c1e765e896f7cfc
Andrew Nagy <andrew.nagy@the159.com>
2018-10-17 20:16:00 -0400

    remove merge files and fix localization cli

--- amp_conf/htdocs/admin/libraries/Console/Localization.class.php
+++ amp_conf/htdocs/admin/libraries/Console/Localization.class.php
@@ -38,9 +38,10 @@ class Localization extends Command {
 			->setDefinition(array(
 				new InputOption('authorization', null, InputOption::VALUE_REQUIRED, sprintf(_('Authorization Token. Requests are limited to 100/day, without setting this. See %s for more information'),self::BASE_URL.'/accounts/profile/#api')),
 				new InputOption('list', null, InputOption::VALUE_NONE, _('List Modules with localizations')),
-				new InputOption('update', null, InputOption::VALUE_NONE, _('List Modules with localizations')),
-				new InputOption('module', null, InputOption::VALUE_REQUIRED, _('List Modules with localizations')),
-				new InputOption('language', null, InputOption::VALUE_REQUIRED, _('List Modules with localizations')),
+				new InputOption('update', null, InputOption::VALUE_NONE, _('Update localizations, optionally provide --module')),
+				new InputOption('module', null, InputOption::VALUE_REQUIRED, _('Module to work against, if not provided all modules are assumed')),
+				new InputOption('language', null, InputOption::VALUE_REQUIRED, _('The language code to work against, if not provided all languages are assumed')),
+				new InputOption('ignorechange', null, InputOption::VALUE_NONE, _('Ignore last change date, process regardless')),
 			));
 	}
 	protected function execute(InputInterface $input, OutputInterface $output){
@@ -64,19 +65,25 @@ class Localization extends Command {
 
 	private function update(InputInterface $input, OutputInterface $output) {
 		$module = $input->getOption('module');
+		$language = $input->getOption('language');
+		$ignorechange = !empty($input->getOption('ignorechange'));
 		if(empty($module)) {
 			foreach($this->getModuleLanuageList() as $module) {
 				if(!$module['hasLangs']) {
+					$output->writeln("<error>".$module['rawname']." has no languages</error>");
 					continue;
 				}
 				try {
-					$this->updateModulei18n($output, $module['rawname']);
+					$this->updateModulei18n($output, $module['rawname'], $ignorechange, $language);
 				} catch(\Exception $e) {
 					$output->writeln("<error>".$e->getMessage()."</error>");
+					if($e->getCode() === 403) {
+						exit(-1);
+					}
 				}
 			}
 		} else {
-			$translations = $this->updateModulei18n($output, $input->getOption('module'));
+			$translations = $this->updateModulei18n($output, $input->getOption('module'), $ignorechange, $language);
 		}
 	}
 
@@ -96,52 +103,79 @@ class Localization extends Command {
 		$table->render($output);
 	}
 
-	private function updateModulei18n(OutputInterface $output, $module,$language=null) {
+	private function updateModulei18n(OutputInterface $output, $module,$ignorechange=false,$language=null) {
 		$path = \FreePBX::Config()->get('AMPWEBROOT');
 		$translations = $this->getTranslations($module);
+		$output->writeln("Processing $module");
 		foreach($translations['results'] as $translation) {
-			if(empty($translation['last_change'])) {
+			if(!empty($language) && $translation['language_code'] !== $language) {
+				//$output->writeln($module." '".$translation['language_code']."' is not '".$language."' skipping");
 				continue;
 			}
-			//seconds are not inputed into the po files so remove them for comparison
-			$translation['last_change'] = preg_replace("/:\d{2}\.\d*/", ":00", $translation['last_change']);
 			$mo = $module;
 			$filename = $path."/admin/modules/".$module."/i18n/".$translation['language_code']."/LC_MESSAGES/".$module.".po";
 			if($module === 'framework') {
 				$mo = 'amp';
 				$filename = $path."/admin/i18n/".$translation['language_code']."/LC_MESSAGES/amp.po";
 			}
+
 			if(file_exists($filename)) {
+
+				if(empty($translation['last_change'])) {
+					if(!$ignorechange) {
+						$output->writeln("\t".$module."[".$translation['language_code']."] last change date unknown unable to determine last change. Skipping (Use --ignorechange to force)");
+						continue;
+					} else {
+						$output->writeln("\t".$module."[".$translation['language_code']."] has never been modified on weblate, forcing last change date to now");
+						$now = new \DateTime("now", new \DateTimeZone("UTC"));
+						$translation['last_change'] = $now->format('Y-m-d\TH:i:s\Z');
+					}
+				}
+
 				$fileHandler = new POFS($filename);
 				$poParser = new POP($fileHandler);
 				$catalog  = $poParser->parse();
-				$revision = '';
+				$then = new \DateTime("@0", new \DateTimeZone("UTC"));
+				$revision = $then->format('Y-m-d\TH:i:s\Z');
 				foreach($catalog->getHeaders() as $header) {
 					if(preg_match('/^PO-Revision-Date/i',$header)) {
 						$revision = trim(explode(":",$header,2)[1]);
 					}
 				}
-				$previous = Carbon::parse($revision);
-				$last = Carbon::parse($translation['last_change']);
-				if($previous->getTimestamp() < $last->getTimestamp()) {
-					$output->writeln($module."[".$translation['language_code']."] needs an update (".$revision." < ".$translation['last_change'].")");
-					try {
-						$filedata = $this->getRequest()->get($translation['file_url'],$this->headers);
-					} catch(\Exception $e) {
-						$output->writeln("<error>Failed to download: ".$translation['file_url']."</error>");
-						exit(-1);
-					}
 
-					file_put_contents($filename, $filedata);
-					$process = new Process('msgfmt '.$filename.' -o '.dirname($filename).'/'.$mo.'.mo');
-					$process->mustRun();
-				} else {
-					$output->writeln($module."[".$translation['language_code']."] is already up to date");
+				//seconds are not inputed into the po files so remove them for comparison
+				//Set to :00 because we don't actually know the seconds
+				$translation['last_change'] = preg_replace("/:\d{2}\.\d*/", ":00", $translation['last_change']);
+
+				$previous = Carbon::parse($revision);
+				$previous->setTime($previous->format("H"), $previous->format("i"), 0); //set seconds to 0 because we dont know them accurately
+				$last = Carbon::parse($translation['last_change']);
+				$last->setTime($previous->format("H"), $previous->format("i"), 0); //set seconds to 0 because we dont know them accurately
+				if($previous->getTimestamp() >= $last->getTimestamp()) {
+					$output->writeln("\t".$module."[".$translation['language_code']."] is already up to date");
+					continue;
 				}
+				$output->writeln("\t".$module."[".$translation['language_code']."] will be updated (".$previous->format('Y-m-d\TH:i:s\Z')." < ".$last->format('Y-m-d\TH:i:s\Z').")");
 			} else {
-				$output->writeln($module."[".$translation['language_code']."] does not exist [".$filename."]");
+				$output->writeln("\t".$module."[".$translation['language_code']."] will be added");
+				mkdir(dirname($filename),0777,true);
+				touch($filename);
 			}
+			try {
+				$filedata = $this->getRequest()->get($translation['file_url'],$this->headers);
+			} catch(\Exception $e) {
+				$output->writeln("\t"."<error>Failed to download: ".$translation['file_url']."</error>");
+				continue;
+			}
+			if($filedata->status_code !== 200) {
+				$output->writeln("\t"."<error>Failed to download: ".$translation['file_url']."</error>");
+				continue;
+			}
+			file_put_contents($filename, $filedata->body);
+			$process = new Process('msgfmt '.$filename.' -o '.dirname($filename).'/'.$mo.'.mo');
+			$process->mustRun();
 		}
+		$output->writeln("Finished Processing $module");
 	}
 
 	private function getModuleLanuageList() {
@@ -158,6 +192,7 @@ class Localization extends Command {
 				$v = basename($v);
 			});
 		}
+
 		$final = [];
 		$final['framework'] = [
 			'name' => 'Framework',
@@ -166,9 +201,10 @@ class Localization extends Command {
 			'hasLangs' => !empty($langs)
 		];
 
-		return $final;
-
 		foreach($modules as $value) {
+			if($value['rawname'] === 'framework') {
+				continue;
+			}
 			$langs = [];
 			if(file_exists($path."/admin/modules/".$value['rawname']."/i18n")) {
 				$langs = glob($path."/admin/modules/".$value['rawname']."/i18n/*",GLOB_ONLYDIR);
@@ -196,14 +232,14 @@ class Localization extends Command {
 			}
 		}
 		if(empty($project)) {
-			throw new \Exception("Unable to find $component");
+			throw new \Exception("Unable to find $component on weblate");
 		}
 		$translations = $this->getRequest()->get(str_replace(['%project%','%component%'],[$project,$component],self::BASE_API_URL.self::TRANSLATIONS_URL).'?limit=200',$this->headers);
 		if($translations->status_code === 200) {
 			return json_decode($translations->body,true);
 		}
 		if($translations->status_code === 429) {
-			throw new \Exception("Too many requests to weblate. Consider using an Authorization Token. See --authorization for more information");
+			throw new \Exception("Too many requests to weblate. Consider using an Authorization Token. See --authorization for more information", 403);
 		}
 		throw new \Exception("Unable to find $component on weblate");
 	}
