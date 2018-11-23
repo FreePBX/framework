aa77dfdbaff65c385fd79152d080a40b4e99c643
Andrew Nagy <andrew.nagy@the159.com>
2018-11-23 16:19:24 -0500

    add fallbacks just in case

--- amp_conf/htdocs/admin/libraries/modulelist.class.php
+++ amp_conf/htdocs/admin/libraries/modulelist.class.php
@@ -3,4 +3,13 @@ class modulelist{
 	public static function create($db) {
 		return \FreePBX::create()->Modulelist;
 	}
+	public function is_loaded() {
+		return \FreePBX::Modulelist()->is_loaded();
+	}
+	public function initialize($module_list) {
+		return \FreePBX::Modulelist()->initialize($module_list);
+	}
+	public function invalidate() {
+		return \FreePBX::Modulelist()->invalidate();
+	}
 }
