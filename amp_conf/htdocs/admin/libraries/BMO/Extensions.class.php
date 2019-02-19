3d626a4fa6d4c4e6991b13d0d7c26b45a97bc0e4
Andrew Nagy <andrew.nagy@the159.com>
2019-02-19 11:58:59 -0500

    FREEPBX-19494

--- amp_conf/htdocs/admin/libraries/BMO/Extensions.class.php
+++ amp_conf/htdocs/admin/libraries/BMO/Extensions.class.php
@@ -86,8 +86,7 @@ class Extensions {
 	public function getExtmap($force = false) {
 		// If aggresive mode, we get it each time
 		if (!$this->FreePBX->Config->get('AGGRESSIVE_DUPLICATE_CHECK') && !$force) {
-			$sth = $this->FreePBX->Database->query("SELECT `data` FROM `module_xml` WHERE `id` = 'extmap_serialized'", \PDO::FETCH_COLUMN , 0);
-			$extmap_serialized = $sth->fetch();
+			$extmap_serialized = $this->FreePBX->Database->query("SELECT `data` FROM `module_xml` WHERE `id` = 'extmap_serialized'")->fetch(\PDO::FETCH_COLUMN);
 			// Now make sure there was something there
 			if ($extmap_serialized) {
 				$this->extmap = unserialize($extmap_serialized);
