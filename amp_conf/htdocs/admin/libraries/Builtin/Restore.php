d9a3a6112ef1b67a95788abe7c89c22a96b90b28
kapil gupta <kgupta@sangoma.com>
2019-07-25 15:55:03 +0530

    Fixing FREEPBX-20291 for FreePBX15 restore case also

--- amp_conf/htdocs/admin/libraries/Builtin/Restore.php
+++ amp_conf/htdocs/admin/libraries/Builtin/Restore.php
@@ -7,6 +7,10 @@ class Restore Extends Base\RestoreBase{
 		$sql = "UPDATE IGNORE freepbx_settings SET `value` = :value WHERE `keyword` = :keyword AND `module` = ''";
 		$sth = $this->FreePBX->Database->prepare($sql);
 		foreach($configs['settings'] as $keyword => $value) {
+			if ($data['keyword'] === 'AMPMGRPASS') {
+				$this->log(sprintf(_("Ignorning restore of AMPMGRPASS Advanced Settings from %s"), $module));
+				continue;
+			}
 			$sth->execute([
 				":keyword" => $keyword,
 				":value" => $value
