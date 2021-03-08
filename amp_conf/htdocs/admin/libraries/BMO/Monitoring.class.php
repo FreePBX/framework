d3e0f4d77bde22a3cb75e3b8ca6aa49103f076c1
akbar khan <akhan@sangoma.com>
2021-03-08 05:39:31 +0000

    FREEI-2911 new apis for status fetch of asterisk DB GUI mode

--- amp_conf/htdocs/admin/libraries/BMO/Monitoring.class.php
+++ amp_conf/htdocs/admin/libraries/BMO/Monitoring.class.php
@@ -48,5 +48,82 @@ class Monitoring {
 
 		exit($level);
 	}
+	
+	/**
+	 * asteriskInfo
+	 *
+	 * @return void
+	 */
+	public function asteriskInfo(){
+		return engine_getinfo();
+	}
+	
+	/**
+	 * asteriskRunning
+	 *
+	 * @return void
+	 */
+	public function asteriskRunning(){
+		if(file_exists('/var/run/asterisk/asterisk.ctl')){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * astmanInfo
+	 *
+	 * @return void
+	 */
+	public function astmanInfo($freepbx){
+		$ami = $freepbx->astman->connected();
+		if($ami){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * dbStatus
+	 *
+	 * @return void
+	 */
+	public function dbStatus(){
+		if(DB_OK){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * GUIMode
+	 *
+	 * @param  mixed $freepbx
+	 * @return void
+	 */
+	public function GUIMode($freepbx){
+		return $freepbx->Config()->get('FPBXOPMODE');
+	}
+	
+	/**
+	 * setupWizardDetails
+	 *
+	 * @param  mixed $freepbx
+	 * @return void
+	 */
+	public function setupWizardDetails($freepbx){
+		$sql = $freepbx->database->prepare("SELECT val FROM `kvstore_OOBE` where `key` like ?");
+		$sql->execute(array('completed'));
+		return $sql->fetchAll(\PDO::FETCH_ASSOC);
+	}
+	
+	/**
+	 * autoUpdateDetails
+	 *
+	 * @return void
+	 */
+	public function autoUpdateDetails(){
+		$um = new \FreePBX\Builtin\UpdateManager();
+		return $um->getCurrentUpdateSettings();
+	}
 }
-
