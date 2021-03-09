b18510d02ee77c67018ce891cbb5ef7a2d0c8b43
kapil gupta <kgupta@sangoma.com>
2021-03-09 06:47:05 +0000

    [Module Tag script: framework 16.0.2]

--- amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_real.php
+++ amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInitpbxframework
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
