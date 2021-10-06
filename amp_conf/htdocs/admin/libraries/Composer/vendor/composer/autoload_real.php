c0d9561d096b33f958d2930e8ac22647b8318e90
Matt Brooks <mbrooks@sangoma.com>
2021-10-06 21:12:09 +0000

    FREEPBX-22910 Upgrade rmccue request library to latest version
    The rmccue library distributes its own CA certificates bundle, so
    when the Let's encrypt DST Root CA X3 root expired, this library
    stopped working with sites that were signed with Let's Encrypt.
    Upgrading the rmccue request library to the latest version seems to
    have fixed the issue.

--- amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_real.php
+++ amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInitpbxframework
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitpbxframework', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitpbxframework', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitpbxframework::getInitializer($loader));
         } else {
