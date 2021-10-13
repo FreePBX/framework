b65afe332308032988fa761bec96989f0a27e032
kapil gupta <kgupta@sangoma.com>
2021-10-13 14:20:18 +0000

    Updating composer files

--- amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_real.php
+++ amp_conf/htdocs/admin/libraries/Composer/vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInitpbxframework
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitpbxframework', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitpbxframework', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitpbxframework::getInitializer($loader));
         } else {
