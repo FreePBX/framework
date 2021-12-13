1375494ced1eedf5aa675953b1dcd0e27d700cfe
kapil gupta <kgupta@sangoma.com>
2021-12-13 06:27:27 +0000

    Fixing FREEI-4291 Adding Asterisk 19 support

--- amp_conf/htdocs/admin/libraries/utility.functions.php
+++ amp_conf/htdocs/admin/libraries/utility.functions.php
@@ -1419,11 +1419,11 @@ function freepbxGetSanitizedRequest($definition = FILTER_SANITIZE_STRING, $add_e
  */
 function IsAsteriskSupported($version){
 	$min 				= "11";
-	$max 				= "19";
+	$max 				= "20";
 	$result["status"] 	= true;
 	if(version_compare($version, $min, "lt") || version_compare($version, $max, "ge")) {
 		$result["status"] 	= false;
-		$result["message"] 	= _("Supported Asterisk versions:")." 11, 12, 13, 14, 15, 16, 17, 18.";
+		$result["message"] 	= _("Supported Asterisk versions:")." 11, 12, 13, 14, 15, 16, 17, 18, 19.";
 	}
 	return $result;	
 }
