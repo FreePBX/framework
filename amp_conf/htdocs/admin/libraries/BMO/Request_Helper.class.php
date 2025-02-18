40281a912c53a4ae475448cf7d19454e78c9e3bb
kapil gupta <kgupta@sangoma.com>
2025-02-18 05:57:59 -0500

    Revert "issue-630 removed getSanitizedRequest() method from Request_helper"
    This reverts commit 81d1c678704e4682185ae086c0de182d6c758509.

--- amp_conf/htdocs/admin/libraries/BMO/Request_Helper.class.php
+++ amp_conf/htdocs/admin/libraries/BMO/Request_Helper.class.php
@@ -196,4 +196,42 @@ class Request_Helper extends Self_Helper {
 		}
 		return;
 	}
+
+	/**
+	 * Get $_REQUEST sanitized through filter_input_array
+	 *
+	 * @method getSanitizedRequest
+	 * @return array              $_REQUEST, sanitized
+	 */
+	public function getSanitizedRequest($definition = FILTER_SANITIZE_FULL_SPECIAL_CHARS, $add_empty = true)
+	{
+		$order = ini_get('request_order');
+		$order = !empty($order) ? $order : ini_get('variables_order');
+		$total = strlen($order);
+		$request = array();
+		for($i = 0; $i < $total; $i++) {
+			switch($order[$i]) {
+				case 'G':
+					$GET = filter_input_array(INPUT_GET, $definition, $add_empty);
+					if(is_array($GET)) {
+						$request = array_merge($request,$GET);
+					}
+				break;
+				case 'P':
+					$POST = filter_input_array(INPUT_POST, $definition, $add_empty);
+					if(is_array($POST)) {
+						$request = array_merge($request,$POST);
+					}
+				break;
+				case 'C':
+					$COOKIE = filter_input_array(INPUT_COOKIE, $definition, $add_empty);
+					if(is_array($COOKIE)) {
+						$request = array_merge($request,$COOKIE);
+					}
+				break;
+			}
+		}
+
+		return $request;
+	}
 }
