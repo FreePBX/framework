a3918ac487c8eeab63af9696794af6e4c9cbb8d2
kapil gupta <kgupta@sangoma.com>
2025-07-23 09:35:21 +0000

    updating js

--- amp_conf/htdocs/admin/assets/js/script.legacy.js
+++ amp_conf/htdocs/admin/assets/js/script.legacy.js
@@ -1872,6 +1872,12 @@ $(document).ready(function() {
 												</div>
 											</form>
 										`);
+										// Re-attach Enter key handler to new inputs
+										dialog.find("input").off("keyup.enter").on("keyup.enter", function(event) {
+											if (event.keyCode === 13) {
+												dialog.closest(".ui-dialog").find(".ui-dialog-buttonpane button:first").click();
+											}
+										});
 									}
 								});
 							}
@@ -2275,3 +2281,11 @@ $(document).on('click','.clicktoedit',function(){
 		}
 	}, 130000);
 })();
+
+$(document).on('submit', '#loginform', function(e) {
+	const dialog = $(this).closest(".ui-dialog");
+	const hasPassword = dialog.find("input[type='password']").length > 0;
+    if (!hasPassword) {
+        e.preventDefault();
+    }
+});
